965eeae37b1afb2192edc4e87357d756cf6453df
bianjing <bianjing@baidu.com>
2015-01-08 18:55:52 +0800

    Added Bilibili part

---
+++ BilibiliAssGenerator/Bilibili/BilibiliChat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BiliBiliAssGenerator.Bilibili
+{
+    public class BilibiliChat
+    {
+        List<BilibiliComment> comments;
+
+        public string Cid { get; private set; }
+        public TimeSpan ChatOffset { get; set; } = TimeSpan.Zero;
+        public IEnumerable<BilibiliComment> Comments
+            => comments.Select(c => c.WithOffset(ChatOffset));
+        public string Title { get; set; }
+
+        public BilibiliChat(string cid, string title)
+        {
+            Cid = cid;
+            Title = title;
+            HttpWebRequest request = WebRequest.CreateHttp("http://comment.bilibili.com/\{cid}.xml");
+            var document = new XmlDocument();
+            using (var s = request.GetResponse().GetResponseStream())
+            {
+                document.Load(s);
+            }
+
+            comments = new List<BilibiliComment>();
+            foreach (XmlNode comment in document.SelectNodes("//d"))
+            {
+                comments.Add(new BilibiliComment(comment.Attributes["p"].Value, comment.InnerText));
+            }
+        }
+    }
+}
