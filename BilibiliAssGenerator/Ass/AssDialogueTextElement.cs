3ea8fbe553f2c717ab02dcee4d1b5be07edb5ea2
bianjing <bianjing@baidu.com>
2015-01-16 18:53:56 +0800

    WIP

--- BilibiliAssGenerator/Ass/AssDialogueTextElement.cs
+++ BilibiliAssGenerator/Ass/AssDialogueTextElement.cs
@@ -6,9 +6,46 @@ using System.Threading.Tasks;
 
 namespace BilibiliAssGenerator.Ass
 {
-    public abstract class AssDialogueTextElement : AssElement
+    public class AssDialogueTextElement : AssElement
     {
+        public string Content { get; set; }
+
+        public bool? Bold { get; set; }
+
+        public bool? Italic { get; set; }
+
+        public bool? StrikeOut { get; set; }
+
+        public int? Border { get; set; }
+
+        public int? Shadow { get; set; }
+
+        public bool? BlurEdges { get; set; }
+
+        public string FontName { get; set; }
+
+        public int FontSize { get; set; }
+
+        public int? FontSizePercentX { get; set; }
+
+        public int? FontSizePercentY { get; set; }
+
+        public AssDialogueTextElement()
+        {
+            Content = "";
+        }
+
+        public AssDialogueTextElement(string s)
+        {
+            Content = s;
+        }
+
         public static implicit operator AssDialogueTextElement(string s)
-            => new AssDialogueTextNormalElement(s);
+            => new AssDialogueTextElement(s);
+
+        public override string GenerateAssText()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
