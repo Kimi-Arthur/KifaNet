6ee735604835cf18edc865fa826f27f9bc5ea044
Kimi@Desk <kimi.ribery@gmail.com>
2015-04-06 13:40:49 +0800

    Added missing file.

---
+++ Utilities/FileUtility/ByteArrayExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pimix.Utilities
+{
+    static class ByteArrayExtensions
+    {
+        public static string Dump(this byte[] input)
+            => BitConverter.ToString(input).Replace("-", "");
+    }
+}
