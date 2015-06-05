c5c1f0df892bff9ce54167e062a4f06961edcaed
Kimi Arthur <kimi.ribery@gmail.com>
2015-06-05 09:54:55 -0700

    Update FileInformationGenerator to support other type of addresses.

--- FileInformationGenerator/Program.cs
+++ FileInformationGenerator/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Pimix.Cloud.Baidu;
 using Pimix.Service;
 using Pimix.Storage;
 
@@ -16,17 +17,25 @@ namespace FileInformationGenerator
         {
             string site = ConfigurationManager.AppSettings["Site"];
             string pathPrefix = ConfigurationManager.AppSettings["PathPrefix"];
+
+            DataModel.PimixServerApiAddress = ConfigurationManager.AppSettings["PimixServerApiAddress"];
+            DataModel.PimixServerCredential = ConfigurationManager.AppSettings["PimixServerCredential"];
+
             foreach (var arg in args)
             {
                 foreach (var path in arg.Split('\n'))
                 {
-                    var info = FileUtility.GetInformation($"{pathPrefix}/{path}", FileProperties.All ^ FileProperties.Path);
-                    info.Id = path;
-                    info.Path = path;
-                    info.Locations = new List<string> { $"{site}:{path}" };
-                    DataModel.PimixServerApiAddress = ConfigurationManager.AppSettings["PimixServerApiAddress"];
-                    DataModel.PimixServerCredential = ConfigurationManager.AppSettings["PimixServerCredential"];
-                    DataModel.Patch(info);
+                    Uri uri = new Uri(path);
+                    //var info = FileUtility.GetInformation($"{pathPrefix}/{path}", FileProperties.All ^ FileProperties.Path);
+                    var client = new StorageClient() { AccountId = uri.UserInfo };
+                    using (var s = client.GetDownloadStream(uri.AbsolutePath))
+                    {
+                        var info = FileUtility.GetInformation(s, FileProperties.All ^ FileProperties.Path);
+                        info.Id = path;
+                        info.Path = path;
+                        info.Locations = new List<string> { $"{site}:{path}" };
+                        DataModel.Patch(info);
+                    }
                 }
             }
         }
