47219c8636d279c52ccccb4ffb015a48cc9bc543
Kimi Arthur <kimi.ribery@gmail.com>
2015-06-12 20:45:21 -0700

    PimixFileFormatV0 decode done. Needs testing.

---
+++ Pimix.IO.FileFormats/PimixFileFormat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pimix.IO.FileFormats
+{
+    public abstract class PimixFileFormat
+    {
+        public abstract Stream GetEncodeStream(Stream rawStream, FileInformation fileInformation = null);
+
+        public abstract Stream GetDecodeStream(Stream encodedStream, FileInformation fileInformation = null);
+    }
+}
---
+++ Pimix.IO.FileFormats/PimixFileFormatV0.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Pimix.Cryptography;
+
+namespace Pimix.IO.FileFormats
+{
+    /// <summary>
+    /// Legacy file format with encryption and information header.
+    /// 
+    /// There is a verbose header part, 0x1225 bytes long and starts with "0x01231225".
+    /// Information wise, it only contains SHA256 and file size.
+    /// SHA256 starts at 0x0e90 (3728) and is in hex string format (thus occupies 64 bytes)
+    /// File size starts at 0x073e (1854) and will end with space.
+    /// 
+    /// </summary>
+    public class PimixFileFormatV0 : PimixFileFormat
+    {
+        public override Stream GetDecodeStream(Stream encodedStream, FileInformation fileInformation = null)
+        {
+            fileInformation = fileInformation ?? new FileInformation();
+            if (fileInformation.EncryptionKey == null)
+            {
+                // We have to get the data from server...
+
+                if (fileInformation.Id == null)
+                {
+                    // We need to get the secondary id from the stream as ":SHA256".
+                    encodedStream.Seek(3728, SeekOrigin.Begin);
+                    byte[] sha256Bytes = new byte[64];
+                    encodedStream.Read(sha256Bytes, 0, 64);
+                    fileInformation.Id = ":" + Encoding.UTF8.GetString(sha256Bytes, 0, 64);
+                }
+
+                fileInformation = FileInformation.Get<FileInformation>(fileInformation.Id);
+            }
+
+            if (fileInformation.Size == null)
+            {
+                // If we only miss the file size, we can get from the header.
+                encodedStream.Seek(1854, SeekOrigin.Begin);
+                byte[] sizeBytes = new byte[92];
+                fileInformation.Size = long.Parse(Encoding.UTF8.GetString(sizeBytes, 0, 92));
+            }
+
+            Aes aesAlgorithm = new AesCryptoServiceProvider
+            {
+                Padding = PaddingMode.ANSIX923,
+                Key = fileInformation.EncryptionKey.ParseHexString(),
+                Mode = CipherMode.ECB
+            };
+
+            return new PimixCryptoStream(new PartialStream(encodedStream, 0x1225), aesAlgorithm.CreateDecryptor(), fileInformation.Size.Value, true);
+        }
+
+        public override Stream GetEncodeStream(Stream rawStream, FileInformation fileInformation = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
