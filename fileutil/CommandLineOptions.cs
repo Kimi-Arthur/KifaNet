5bddb2de3a1dc1ab50786f84cd4581c9c12b7d24
Kimi Arthur <kimi.ribery@gmail.com>
2015-06-19 08:49:27 -0700

    Adding command line options.

---
+++ fileutil/CommandLineOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace fileutil
+{
+    class CommandLineOptions
+    {
+        public CopyCommandOptions CopyCommand { get; set; }
+    }
+
+    [Verb("cp", HelpText = "Copy file from SOURCE to DEST.")]
+    class CopyCommandOptions
+    {
+        [Value(0, Required = true)]
+        public string SourceUri { get; set; }
+
+        [Value(1, Required = true)]
+        public string DestinationUri { get; set; }
+    }
+}
