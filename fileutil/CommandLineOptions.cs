63ab0832c41df55bcb64255f16ba182637a5b6f0
Kimi@Desk <kimi.ribery@gmail.com>
2015-06-21 14:33:48 +0800

    Incorporated CommandLine to handle arguments parsing.

--- fileutil/CommandLineOptions.cs
+++ fileutil/CommandLineOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,6 @@ using CommandLine;
 
 namespace fileutil
 {
-    class CommandLineOptions
-    {
-        public CopyCommandOptions CopyCommand { get; set; }
-    }
 
     [Verb("cp", HelpText = "Copy file from SOURCE to DEST.")]
     class CopyCommandOptions
@@ -20,5 +17,28 @@ namespace fileutil
 
         [Value(1, Required = true)]
         public string DestinationUri { get; set; }
+
+        [Option('c', "chunk-size", HelpText = "The chunk size used to copy data")]
+        public string ChunkSize { get; set; } = ConfigurationManager.AppSettings["BufferSize"];
+    }
+
+    [Verb("upload", HelpText = "Upload file baidu cloud.")]
+    class UploadCommandOptions
+    {
+        [Value(0, Required = true)]
+        public string SourceUri { get; set; }
+
+        [Value(1, Required = true)]
+        public string DestinationUri { get; set; }
+    }
+
+    [Verb("info", HelpText = "Generate information of the specified file.")]
+    class InfoCommandOptions
+    {
+        [Value(0, Required = true)]
+        public string FileUri { get; set; }
+
+        [Option]
+        public bool Dryrun { get; set; }
     }
 }
--- fileutil/Program.cs
+++ fileutil/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 using Newtonsoft.Json;
 using Pimix;
 using Pimix.Cloud.BaiduCloud;
@@ -16,100 +17,108 @@ namespace fileutil
     {
         static void Main(string[] args)
         {
-            int bufferSize = (int)ConfigurationManager.AppSettings["BufferSize"].ParseSizeString();
-            BaiduCloudConfig.PimixServerApiAddress = ConfigurationManager.AppSettings["PimixServerApiAddress"];
-
-            switch (args[0])
+            var commands = new Dictionary<Type, Action<object>>()
             {
-                case "cp":
-                    {
-                        Uri input = new Uri(args[1]);
-                        //Uri output = new Uri(args[2]);
-                        BaiduCloudStorageClient.Config = BaiduCloudConfig.Get("baidu_cloud");
-                        var client = new BaiduCloudStorageClient() { AccountId = input.UserInfo };
-                        using (var stream = client.GetDownloadStream(input.LocalPath))
-                        {
-                            using (FileStream fs = new FileStream(args[2], FileMode.Create))
-                            {
-                                stream.CopyTo(fs, bufferSize);
-                            }
-                        }
+                [typeof(InfoCommandOptions)] = (x) => GetInfo(x as InfoCommandOptions),
+                [typeof(CopyCommandOptions)] = (x) => CopyFile(x as CopyCommandOptions),
+                [typeof(UploadCommandOptions)] = (x) => UploadFile(x as UploadCommandOptions)
+            };
 
-                        break;
-                    }
-                case "upload":
-                    {
-                        Uri uploadTo = new Uri(args[2]);
-                        BaiduCloudStorageClient.Config = BaiduCloudConfig.Get("baidu_cloud");
-                        var client = new BaiduCloudStorageClient() { AccountId = uploadTo.UserInfo };
-                        using (var stream = File.OpenRead(args[1]))
-                        {
-                            client.UploadStream(uploadTo.LocalPath, stream);
-                        }
+            var result = Parser.Default.ParseArguments<CopyCommandOptions, InfoCommandOptions, UploadCommandOptions>(args);
+            if (!result.Errors.Any())
+            {
+                Initialize();
 
-                        break;
-                    }
-                case "info":
-                    {
-                        BaiduCloudStorageClient.Config = BaiduCloudConfig.Get("baidu_cloud");
-                        string path = args[1];
-                        bool dryRun = args.Length > 2 && args[2] == "--dryrun";
-                        Uri uri = new Uri(path);
-                        Stream downloadStream = null;
-                        var schemes = uri.Scheme.Split('+').ToList();
-                        if (schemes[0] != "pimix")
-                            break;
-
-                        if (schemes.Contains("cloud"))
-                        {
-                            switch (uri.Host)
-                            {
-                                case "pan.baidu.com":
-                                    {
-                                        var client = new BaiduCloudStorageClient
-                                        {
-                                            AccountId = uri.UserInfo
-                                        };
-                                        downloadStream = client.GetDownloadStream(uri.LocalPath);
-                                        break;
-                                    }
-                                default:
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            downloadStream = File.OpenRead($"/allfiles/{uri.Host}{uri.LocalPath}");
-                            // Use ftp stream first.
-                            //FtpWebRequest request = WebRequest.Create($"ftp://{uri.Host}/files{uri.LocalPath}") as FtpWebRequest;
-                            //request.Credentials = new NetworkCredential("pimix", "P2015apr");
-                            //downloadStream = request.GetResponse().GetResponseStream();
-                        }
-
-                        using (var s = downloadStream)
-                        {
-                            long len = downloadStream.Length;
-                            var info = FileInformation.Get(uri.LocalPath).AddProperties(s, FileProperties.All ^ FileProperties.Path);
-                            info.Path = uri.LocalPath;
-                            if (info.Locations == null)
-                                info.Locations = new Dictionary<string, string>();
-                            info.Locations[uri.GetLeftPart(UriPartial.Authority)] = path;
-                            if (len == info.Size)
-                            {
-                                if (dryRun)
-                                {
-                                    Console.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
-                                }
-                                else
-                                {
-                                    FileInformation.Patch(info);
-                                }
-                            }
-                        }
-
-                        break;
-                    }
+                commands[result.Value.GetType()](result.Value);
             }
+        }
+
+        static void Initialize()
+        {
+            BaiduCloudConfig.PimixServerApiAddress = ConfigurationManager.AppSettings["PimixServerApiAddress"];
+        }
+
+        static void CopyFile(CopyCommandOptions options)
+        {
+            Uri input = new Uri(options.SourceUri);
+            BaiduCloudStorageClient.Config = BaiduCloudConfig.Get("baidu_cloud");
+            var client = new BaiduCloudStorageClient() { AccountId = input.UserInfo };
+            using (var stream = client.GetDownloadStream(input.LocalPath))
+            {
+                using (FileStream fs = new FileStream(options.DestinationUri, FileMode.Create))
+                {
+                    stream.CopyTo(fs, (int)options.ChunkSize.ParseSizeString());
+                }
+            }
+        }
+
+        static void UploadFile(UploadCommandOptions options)
+        {
+            Uri uploadTo = new Uri(options.DestinationUri);
+            BaiduCloudStorageClient.Config = BaiduCloudConfig.Get("baidu_cloud");
+            var client = new BaiduCloudStorageClient() { AccountId = uploadTo.UserInfo };
+            using (var stream = File.OpenRead(options.SourceUri))
+            {
+                client.UploadStream(uploadTo.LocalPath, stream);
+            }
+        }
+
+        static void GetInfo(InfoCommandOptions options)
+        {
+            BaiduCloudStorageClient.Config = BaiduCloudConfig.Get("baidu_cloud");
+            Uri uri = new Uri(options.FileUri);
+            Stream downloadStream = null;
+            var schemes = uri.Scheme.Split('+').ToList();
+            if (schemes[0] != "pimix")
+                return;
+
+            if (schemes.Contains("cloud"))
+            {
+                switch (uri.Host)
+                {
+                    case "pan.baidu.com":
+                        {
+                            var client = new BaiduCloudStorageClient
+                            {
+                                AccountId = uri.UserInfo
+                            };
+                            downloadStream = client.GetDownloadStream(uri.LocalPath);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                downloadStream = File.OpenRead($"/allfiles/{uri.Host}{uri.LocalPath}");
+                // Use ftp stream first.
+                //FtpWebRequest request = WebRequest.Create($"ftp://{uri.Host}/files{uri.LocalPath}") as FtpWebRequest;
+                //request.Credentials = new NetworkCredential("pimix", "P2015apr");
+                //downloadStream = request.GetResponse().GetResponseStream();
+            }
+
+            using (var s = downloadStream)
+            {
+                long len = downloadStream.Length;
+                var info = FileInformation.Get(uri.LocalPath).AddProperties(s, FileProperties.All ^ FileProperties.Path);
+                info.Path = uri.LocalPath;
+                if (info.Locations == null)
+                    info.Locations = new Dictionary<string, string>();
+                info.Locations[uri.GetLeftPart(UriPartial.Authority)] = options.FileUri;
+                if (len == info.Size)
+                {
+                    if (options.Dryrun)
+                    {
+                        Console.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
+                    }
+                    else
+                    {
+                        FileInformation.Patch(info);
+                    }
+                }
+            }
+
         }
     }
 }
