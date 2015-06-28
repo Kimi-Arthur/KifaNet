49a2b5a7bcb591df95582163c5e40ed34a588a48
Kimi@Desk <kimi.ribery@gmail.com>
2015-06-28 18:58:03 +0800

    Fixed tests.

--- PimixTest.IO.FileFormats/PimixFileV1Tests.cs
+++ PimixTest.IO.FileFormats/PimixFileV1Tests.cs
@@ -14,30 +14,32 @@ namespace PimixTest.IO.FileFormats
         [TestMethod]
         public void RandomDataTest()
         {
+            string EncryptionKey ="C7C37D56DD70FD6258BDD01AED083C88432EC27536DF9328D6329382183DB795";
+
             byte[] data = new byte[(64 << 20) + 100];
             new Random().NextBytes(data);
-            FileInformation fs = new FileInformation { EncryptionKey = "C7C37D56DD70FD6258BDD01AED083C88432EC27536DF9328D6329382183DB795" };
 
             var rangeList = new List<Tuple<int, int>>
             {
+                new Tuple<int, int>(0, 100),
                 new Tuple<int, int>(0, (64 << 20) + 100),
                 new Tuple<int, int>(0, 64 << 20),
                 new Tuple<int, int>(0, (64 << 20) - 16),
                 new Tuple<int, int>(0, (64 << 20) + 16),
                 new Tuple<int, int>(0, (64 << 20) - 8)
             };
+
             foreach (var item in rangeList)
             {
-                using (MemoryStream ms = new MemoryStream(data))
+                using (MemoryStream ms = new MemoryStream(data, item.Item1, item.Item2))
                 {
                     using (MemoryStream encrypted = new MemoryStream())
                     {
-                        using (Stream encryptionStream = new PimixFileV1 { Info = fs }.GetEncodeStream(ms))
+                        using (Stream encryptionStream = new PimixFileV1 { Info = new FileInformation { EncryptionKey = EncryptionKey } }.GetEncodeStream(ms))
                         {
                             encryptionStream.CopyTo(encrypted);
-                            using (Stream output = new PimixFileV1 { Info = fs }.GetDecodeStream(encrypted))
+                            using (Stream output = new PimixFileV1 { Info = new FileInformation { EncryptionKey = EncryptionKey } }.GetDecodeStream(encrypted))
                             {
-                                ms.Position = 0;
                                 var fs1 = FileInformation.GetInformation(ms, FileProperties.Size | FileProperties.SHA256);
                                 var fs2 = FileInformation.GetInformation(output, FileProperties.Size | FileProperties.SHA256);
                                 Assert.AreEqual(fs1.Size, fs2.Size);
