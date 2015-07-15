e71768879daccc8ef0b41d2803dd821285d2e7b0
Kimi@Desk <kimi.ribery@gmail.com>
2015-07-16 01:29:35 +0800

    Added jobutil.

---
+++ jobutil/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace jobutil
+{
+    abstract class Command
+    {
+        [Option('s', "pimix-server-api-address", HelpText = "Uri for pimix api server address")]
+        public string PimixServerAddress { get; set; } = ConfigurationManager.AppSettings["PimixServerApiAddress"];
+
+        public abstract int Execute();
+    }
+}
---
+++ jobutil/RunJobCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace jobutil
+{
+    class RunJobCommand : Command
+    {
+        [Value(0, Required = true)]
+        public string JobId { get; set; }
+
+        public override int Execute()
+        {
+            return -1;
+        }
+    }
+}
