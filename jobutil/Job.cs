d3edff68f1c9bff6363a3134c5fc1710bdc53e6d
Kimi@Desk <kimi.ribery@gmail.com>
2015-07-17 01:07:22 +0800

    Added experimental code for jobutil.

---
+++ jobutil/Job.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Pimix.Service;
+
+namespace jobutil
+{
+    [DataModel("jobs")]
+    class Job
+    {
+        [JsonProperty("$id")]
+        public string Id { get; set; }
+
+        [JsonProperty("command")]
+        public string Command { get; set; }
+
+        #region PimixService Wrappers
+
+        public static string PimixServerApiAddress
+        {
+            get
+            {
+                return PimixService.PimixServerApiAddress;
+            }
+            set
+            {
+                PimixService.PimixServerApiAddress = value;
+            }
+        }
+
+        public static string PimixServerCredential
+        {
+            get
+            {
+                return PimixService.PimixServerCredential;
+            }
+            set
+            {
+                PimixService.PimixServerCredential = value;
+            }
+        }
+
+        public static bool Patch(Job data, string id = null)
+            => PimixService.Patch<Job>(data, id);
+
+        public static Job Get(string id)
+            => PimixService.Get<Job>(id);
+
+        #endregion
+
+        public static Job StartJob(string id)
+        {
+            return PimixService.Call<Job, Job>("start_job", methodType: "POST", parameters: new Dictionary<string, string> {["id"] = id });
+        }
+
+        public static Job FinishJob(string id, bool failed = false)
+        {
+            return PimixService.Call<Job, Job>("finish_job", methodType: "POST", parameters: new Dictionary<string, string> {["id"] = id, ["failed"] = failed.ToString() });
+        }
+
+        public static Job AddInfo(string id, Dictionary<string, object> information)
+        {
+            return PimixService.Call<Job, Job>("add_info", methodType: "POST", parameters: new Dictionary<string, string> {["id"] = id }, body: information);
+        }
+    }
+}
--- jobutil/RunJobCommand.cs
+++ jobutil/RunJobCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,18 @@ namespace jobutil
 
         public override int Execute()
         {
-            return -1;
+            string cmd = Job.StartJob(JobId).Command;
+            Process proc = new Process();
+            proc.StartInfo.FileName = cmd;
+            proc.StartInfo.Arguments = cmd;
+            proc.Start();
+            proc.WaitForExit();
+            string stdout = proc.StandardOutput.ReadToEnd();
+            string stderr = proc.StandardError.ReadToEnd();
+            Job.AddInfo(JobId, new Dictionary<string, object> { ["stdout"] = stdout, ["stderr"] = stderr, ["exit_code"] = proc.ExitCode.ToString() });
+            Job.FinishJob(JobId, proc.ExitCode != 0);
+            // Even if the job fails, the runner is ok.
+            return 0;
         }
     }
 }
