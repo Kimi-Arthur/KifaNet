50e5b0f9428b732ba906015ab882caf2b6641015
Kimi@Desk <kimi.ribery@gmail.com>
2015-07-19 01:42:02 +0800

    Added configs.

--- jobutil/Program.cs
+++ jobutil/Program.cs
@@ -12,7 +12,35 @@ namespace jobutil
         static int Main(string[] args)
             => Parser.Default.ParseArguments<RunJobCommand>(args)
             .Return(
-                x => x.Execute(),
-                x => 1);
+                x => ExecuteCommand(x),
+                HandleParseFail);
+
+        static int ExecuteCommand(Command command)
+        {
+            Initialize(command);
+
+            try
+            {
+                return command.Execute();
+            }
+            catch (Exception ex)
+            {
+                while (ex != null)
+                {
+                    Console.WriteLine("Caused by:");
+                    Console.WriteLine(ex);
+                    ex = ex.InnerException;
+                }
+
+                return 1;
+            }
+        }
+
+        static int HandleParseFail(IEnumerable<Error> errors) => 2;
+
+        static void Initialize(Command options)
+        {
+            Job.PimixServerApiAddress = options.PimixServerAddress;
+        }
     }
 }
