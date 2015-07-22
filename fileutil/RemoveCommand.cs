616f73102bf7a14a60f5dbe3c8ee2486a2a48f4c
Kimi@Desk <kimi.ribery@gmail.com>
2015-07-23 00:28:32 +0800

    Added dummy RemoveCommand.

---
+++ fileutil/RemoveCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace fileutil
+{
+    [Verb("rm", HelpText = "Remove the FILE. Can be either logic path like: pimix:///Software/... or real path like: pimix://xxx@xxx/....")]
+    class RemoveCommand : Command
+    {
+        [Value(0, MetaName = "FILE", MetaValue = "STRING", Required = true, HelpText = "File to be removed.")]
+        public string FileUri { get; set; }
+
+        public override int Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
