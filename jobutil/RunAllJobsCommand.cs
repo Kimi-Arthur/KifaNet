e45c710b8df17c0159b787e60c93835846d03dca
Kimi Mac <kimi.ribery@gmail.com>
2015-09-02 13:06:13 +0800

    Do not use sleep in getting jobs. Use it in running job.
    There will be fewer pulled but not run jobs.

--- jobutil/RunAllJobsCommand.cs
+++ jobutil/RunAllJobsCommand.cs
@@ -19,21 +19,26 @@ namespace jobutil
 
         public override int Execute()
         {
-            Parallel.ForEach(
-                GetJobs(),
-                new ParallelOptions { MaxDegreeOfParallelism = ThreadCount },
-                j =>
-                {
-                    var c = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Error.WriteLine($"Started job ({j.Id}) at {DateTime.Now}.");
-                    Console.ForegroundColor = c;
-                    j.Execute();
-                    c = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Error.WriteLine($"Finished job ({j.Id}) at {DateTime.Now}.");
-                    Console.ForegroundColor = c;
-                });
+            while (true)
+            {
+                Parallel.ForEach(
+                    GetJobs(),
+                    new ParallelOptions { MaxDegreeOfParallelism = ThreadCount },
+                    j =>
+                    {
+                        var c = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Error.WriteLine($"Started job ({j.Id}) at {DateTime.Now}.");
+                        Console.ForegroundColor = c;
+                        j.Execute();
+                        c = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Error.WriteLine($"Finished job ({j.Id}) at {DateTime.Now}.");
+                        Console.ForegroundColor = c;
+                    });
+                Console.Error.WriteLine("No jobs now. Sleep 2 minutes");
+                Thread.Sleep(TimeSpan.FromMinutes(2));
+            }
 
             return 0;
         }
@@ -53,8 +58,7 @@ namespace jobutil
                 }
                 catch (Exception ex)
                 {
-                    Console.Error.WriteLine("No jobs now. Sleep 2 minutes");
-                    Thread.Sleep(TimeSpan.FromMinutes(2));
+                    yield break;
                 }
 
                 if (j != null)
