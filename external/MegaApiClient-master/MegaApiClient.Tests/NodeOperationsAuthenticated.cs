bc3a54f3f9206058d00f7617d607354d7ace48ce
Kimi Mac <kimi.ribery@gmail.com>
2015-09-18 13:36:29 +0800

    Add MegaApiClient's source code to external for future incorporation.

---
+++ external/MegaApiClient-master/MegaApiClient.Tests/NodeOperationsAuthenticated.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CG.Web.MegaApiClient.Tests
+{
+    [TestFixture]
+    public class NodeOperationsAuthenticated : NodeOperations
+    {
+        public NodeOperationsAuthenticated()
+            : base(Options.LoginAuthenticated | Options.Clean)
+        {
+        }
+
+        [TestCase("SsRDGA4Y", "bsxVBKLL", NodeType.Directory, "SharedFolder", 0, "2015-05-19T09:38:13.0000000+08:00")]
+        [TestCase("u95ATDYA", "SsRDGA4Y", NodeType.Directory, "SharedSubFolder", 0, "2015-07-14T18:24:31.0000000+08:00")]
+        [TestCase("KshlkSIK", "SsRDGA4Y", NodeType.File, "SharedFile.jpg", 523265, "2015-05-19T09:40:47.0000000+08:00")]
+        [TestCase("b0I0QDhA", "u4IgDb5K", NodeType.Directory, "SharedRemoteFolder", 0, "2015-05-21T02:35:22.0000000+08:00")]
+        [TestCase("e5wjkSJB", "b0I0QDhA", NodeType.File, "SharedRemoteFile.jpg", 523265, "2015-05-21T02:36:06.0000000+08:00")]
+        [TestCase("KhZSWI7C", "b0I0QDhA", NodeType.Directory, "SharedRemoteSubFolder", 0, "2015-07-14T17:05:03.0000000+08:00")]
+        [TestCase("HtonzYYY", "KhZSWI7C", NodeType.File, "SharedRemoteSubFile.jpg", 523265, "2015-07-14T18:06:27.0000000+08:00")]
+        [TestCase("z1YCibCT", "KhZSWI7C", NodeType.Directory, "SharedRemoteSubSubFolder", 0, "2015-07-14T18:01:56.0000000+08:00")]
+        public void Validate_PermanentNodes_Succeeds(
+            string id,
+            string expectedParent,
+            NodeType expectedNodeType,
+            string expectedName,
+            long expectedSize,
+            string expectedModificationDate
+            )
+        {
+            var node = this.Client.GetNodes().SingleOrDefault(x => x.Id == id);
+
+            Assert.That(node, Is.Not.Null
+                .And.Property<INode>(x => x.Type).EqualTo(expectedNodeType)
+                .And.Property<INode>(x => x.ParentId).EqualTo(expectedParent)
+                .And.Property<INode>(x => x.Name).EqualTo(expectedName)
+                .And.Property<INode>(x => x.Size).EqualTo(expectedSize)
+                .And.Property<INode>(x => x.LastModificationDate).EqualTo(DateTime.Parse(expectedModificationDate)));
+        }
+    }
+}
---
+++ external/MegaApiClient-master/MegaApiClient/WebClient.cs
@@ -0,0 +1,105 @@
+﻿#region License
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Gregoire Pailler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#endregion
+
+using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace CG.Web.MegaApiClient
+{
+    public class WebClient : IWebClient
+    {
+        private static readonly string UserAgent;
+        private const uint BufferSize = 8192;
+
+        static WebClient()
+        {
+            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
+            UserAgent = string.Format("{0} v{1}", assemblyName.Name, assemblyName.Version.ToString(2));
+        }
+
+        public string PostRequestJson(Uri url, string jsonData)
+        {
+            using (MemoryStream jsonStream = new MemoryStream(jsonData.ToBytes()))
+            {
+                return this.PostRequest(url, jsonStream, "application/json");
+            }
+        }
+        
+        public string PostRequestRaw(Uri url, Stream dataStream)
+        {
+            return this.PostRequest(url, dataStream, "application/octet-stream");
+        }
+
+        public Stream GetRequestRaw(Uri url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Timeout = -1;
+            request.UserAgent = UserAgent;
+
+            return request.GetResponse().GetResponseStream();
+        }
+
+        private string PostRequest(Uri url, Stream dataStream, string contentType)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentLength = dataStream.Length;
+            request.Method = "POST";
+            request.Timeout = -1;
+            request.UserAgent = UserAgent;
+            request.ContentType = contentType;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                dataStream.Position = 0;
+
+                int length = (int)Math.Min(BufferSize, dataStream.Length);
+                byte[] buffer = new byte[length];
+                int bytesRead;
+
+                while ((bytesRead = dataStream.Read(buffer, 0, length)) > 0)
+                {
+                    requestStream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
