84f6b4c15adecec8b81e2b89ba7fcac8db5a981d
Kimi@Desk <kimi.ribery@gmail.com>
2015-09-21 23:33:23 +0800

    Added a basic test case.

---
+++ PimixTest.Cloud.MegaNz/MegaApiClientTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using CG.Web.MegaApiClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PimixTest.Cloud.MegaNz
+{
+    [TestClass]
+    public class MegaApiClientTests
+    {
+        [TestMethod]
+        public void MegaApiClientBasicTest()
+        {
+            MegaApiClient client = new MegaApiClient();
+
+            client.Login("pimixserver+test@gmail.com", "Pny3YQzV");
+            var nodes = client.GetNodes();
+
+            INode root = nodes.Single(n => n.Type == NodeType.Root);
+            INode myFolder = client.CreateFolder("Upload", root);
+
+            INode myFile = client.Upload("data.bin", myFolder);
+
+            Uri downloadUrl = client.GetDownloadLink(myFile);
+            Assert.IsNotNull(downloadUrl);
+        }
+    }
+}
--- PimixTest.Cloud.MegaNz/Properties/AssemblyInfo.cs
+++ PimixTest.Cloud.MegaNz/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4dcb440b-ff1c-487a-afad-122b38491109")]
+[assembly: Guid("3dcb620c-d096-45f4-9512-6f453c5a0def")]
 
 // Version information for an assembly consists of the following four values:
 //
