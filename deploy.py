667e426404b070aaec968c65a0c5df881c62bcb7
Kimi@Desk <kimi.ribery@gmail.com>
2015-10-17 01:43:32 +0800

    Add deploy.py for Windows deployment.

---
+++ deploy.py
@@ -0,0 +1,42 @@
+﻿import os
+import re
+import shutil
+
+destination = 'C:/Software/Pimix/'
+
+apps = [
+    'fileutil',
+    'jobutil'
+]
+
+include_patterns = [
+    r'\.exe',
+    r'\.exe\.config',
+    r'\.dll'
+]
+
+exclude_patterns = [
+    'FSharp',
+    'vshost'
+]
+
+
+os.makedirs(destination, exist_ok=True)
+
+for app in apps:
+    for entry in os.scandir('src/{}/bin/Release/'.format(app)):
+        print(entry.path)
+        to_copy = False
+        for p in include_patterns:
+            if re.search(p, entry.path):
+                print(':{}'.format(entry.path))
+                to_copy = True
+                break
+        if not to_copy:
+            continue
+        for p in exclude_patterns:
+            if re.search(p, entry.path):
+                to_copy = False
+                break
+        if to_copy:
+            shutil.copyfile(entry.path, '{}{}'.format(destination, entry.name))
