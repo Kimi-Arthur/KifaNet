d291cff048cb8c5220c15071b97665818330fe87
Kimi@Desk <kimi.ribery@gmail.com>
2015-11-27 00:58:09 +0800

    Check in WIP storage manager.

---
+++ Pimix.Apps.AzureUtil/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Pimix.Apps.AzureUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new CloudPageBlob(new Uri("blob"), new StorageCredentials("user", "pass"));
+            b.FetchAttributes();
+            b.ClearPages(0, b.Properties.Length);
+        }
+    }
+}
