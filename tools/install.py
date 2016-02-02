5facabe22df9d85b10b7ca4035f474b0c998017f
Kimi@Desk <kimi.ribery@gmail.com>
2016-02-03 01:18:15 +0800

    Fix install script for Windows.

--- tools/install.py
+++ tools/install.py
@@ -4,6 +4,7 @@ import json
 import os
 import platform
 import pystache
+import shutil
 import subprocess
 import sys
 from colorama import init, Fore
@@ -29,6 +30,15 @@ def download_packages(data):
                 (['-Prerelease'] if prerelease else []))
 
 
+def copy_binaries(data):
+    bin_path = '{}/{}'.format(data['bin_prefix'], data['name'])
+    os.makedirs(bin_path, exist_ok=True)
+
+    for p in data['packages']:
+        for entry in os.scandir("{lib_prefix}/{name}.{version}/lib/{framework}/".format(lib_prefix=data['lib_prefix'], **p)):
+            shutil.copy(entry.path, "{bin_path}/{entry_name}".format(bin_path=bin_path, entry_name=entry.name, **p))
+
+
 def write_runner(data):
     tmpl = codecs.open("runner.sh.mustache", "r", "utf-8").read()
     path = '{}/{}'.format(data['bin_prefix'], data['name'])
@@ -44,19 +54,27 @@ def prepare_data(data):
     data['lib_prefix'] = data['install_prefix'] + '/lib/pimix'
     data['bin_prefix'] = data['install_prefix'] + '/bin'
 
+    os.makedirs(data['lib_prefix'], exist_ok=True)
+    os.makedirs(data['bin_prefix'], exist_ok=True)
+
     for p in data['packages']:
         if 'binary' in p:
             data['binary'] = p
             break
     for k, v in data['defaults'].items():
-        data[k] = v
+        for p in data['packages']:
+            if k not in p:
+                p[k] = v
     return data
 
 
 def install(name):
     data = prepare_data(json.load(codecs.open(name + '.json', 'r', 'utf-8')))
     download_packages(data)
-    write_runner(data)
+    if is_windows:
+        copy_binaries(data)
+    else:
+        write_runner(data)
 
 
 def main():
