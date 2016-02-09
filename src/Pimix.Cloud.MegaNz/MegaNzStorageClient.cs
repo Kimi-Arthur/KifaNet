2ca874fd533b84c06eecea4ac0718812e239c81e
Kimi@Desk <kimi.ribery@gmail.com>
2016-02-09 16:33:09 +0800

    Add MegaNzStorageClient.

--- src/Pimix.Cloud.MegaNz/MegaNzStorageClient.cs
+++ src/Pimix.Cloud.MegaNz/MegaNzStorageClient.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pimix.IO;
 
 namespace Pimix.Cloud.MegaNz
 {
-    public class MegaNzStorageClient
+    public class MegaNzStorageClient : StorageClient
     {
+        public override void Copy(string sourcePath, string destinationPath)
+        {
+            throw new NotImplementedException();
+        }
 
+        public override void Delete(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Exists(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Stream OpenRead(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string path, Stream stream = null, FileInformation fileInformation = null, bool match = true)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
