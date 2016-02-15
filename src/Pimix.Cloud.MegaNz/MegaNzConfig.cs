6c24b6df66d2ed79731815f9e4446abb029e4843
Kimi@Desk <kimi.ribery@gmail.com>
2016-02-15 23:37:52 +0800

    Add MegaNzConfig class.

--- src/Pimix.Cloud.MegaNz/MegaNzConfig.cs
+++ src/Pimix.Cloud.MegaNz/MegaNzConfig.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Pimix.Service;
 
 namespace Pimix.Cloud.MegaNz
 {
-    class MegaNzConfig
+    [DataModel("configs")]
+    public partial class MegaNzConfig
     {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("accounts")]
+        public Dictionary<string, AccountInfo> Accounts { get; private set; }
+    }
+
+    public class AccountInfo
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
     }
 }
