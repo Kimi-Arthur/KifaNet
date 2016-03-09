859890680878d3b79f198c924579e62ca2f404f6
Kimi@Desk <kimi.ribery@gmail.com>
2016-03-09 23:09:14 +0800

    Naive implementation of MegaNzStorageClient.OpenRead.

--- src/Pimix.Cloud.MegaNz/MegaNzStorageClient.cs
+++ src/Pimix.Cloud.MegaNz/MegaNzStorageClient.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CG.Web.MegaApiClient;
 using Pimix.IO;
 
 namespace Pimix.Cloud.MegaNz
 {
     public class MegaNzStorageClient : StorageClient
     {
+
+        string accountId;
+        public string AccountId
+        {
+            get
+            {
+                return accountId;
+            }
+            set
+            {
+                accountId = value;
+                Account = Config.Accounts[accountId];
+            }
+        }
+
+        public AccountInfo Account { get; private set; }
+
         public static MegaNzConfig Config { get; set; }
 
         public override void Copy(string sourcePath, string destinationPath)
@@ -29,7 +44,22 @@ namespace Pimix.Cloud.MegaNz
 
         public override Stream OpenRead(string path)
         {
-            throw new NotImplementedException();
+            MegaApiClient client = new MegaApiClient();
+
+            client.Login(Account.Username, Account.Password);
+
+            var nodes = client.GetNodes();
+
+            INode node = nodes.Single(n => n.Type == NodeType.Root);
+            foreach (var p in path.Split('/'))
+            {
+                Console.WriteLine(node.Name);
+                node = client.GetNodes(node).Single(n => n.Name == p);
+            }
+
+            MemoryStream memoryStream = new MemoryStream();
+            client.Download(node).CopyTo(memoryStream);
+            return memoryStream;
         }
 
         public override void Write(string path, Stream stream = null, FileInformation fileInformation = null, bool match = true)
