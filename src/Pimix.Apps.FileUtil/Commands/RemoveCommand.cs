308c9728026817e27866f49c11665f1bf08edc79
Jing <jingbian@google.com>
2017-07-19 15:04:52 +0800

    Standardize RemoveCommand to use FileId + new FileUri format.

--- src/Pimix.Apps.FileUtil/Commands/RemoveCommand.cs
+++ src/Pimix.Apps.FileUtil/Commands/RemoveCommand.cs
@@ -5,97 +5,61 @@ using Pimix.IO;
 
 namespace Pimix.Apps.FileUtil.Commands
 {
-    [Verb("rm", HelpText = "Remove the FILE. Can be either logic path like: pimix:///Software/... or real path like: pimix://xxx@xxx/....")]
+    [Verb("rm", HelpText = "Remove the FILE. Can be either logic path like: /Software/... or real path like: local:desk/Software....")]
     class RemoveCommand : FileUtilCommand
     {
-        [Value(0, MetaName = "FILE", MetaValue = "STRING", Required = true, HelpText = "File to be removed.")]
+        [Value(0, MetaName = "FILE", MetaValue = "STRING", HelpText = "File to be removed.")]
         public string FileUri { get; set; }
+
+        [Option('i', "id", HelpText = "ID for the uri.")]
+        public string FileId { get; set; }
 
         [Option('l', "link", HelpText = "Remove link only.")]
         public bool RemoveLinkOnly { get; set; }
 
         public override int Execute()
         {
-            Uri uri;
-            if (Uri.TryCreate(FileUri, UriKind.Absolute, out uri) && uri.Scheme.StartsWith("pimix"))
-            {
-                var info = FileInformation.Get(uri.LocalPath);
+            if (String.IsNullOrEmpty(FileUri)) {
+                var info = FileInformation.Get(FileId);
 
-                if (string.IsNullOrEmpty(uri.Host))
+                // Remove logical file.
+                if (!RemoveLinkOnly && info.Locations != null)
                 {
-                    // Remove logical file.
-                    if (!RemoveLinkOnly && info.Locations != null)
+                    // Remove real files.
+                    foreach (var location in info.Locations)
                     {
-                        // Remove real files.
-                        foreach (var location in info.Locations)
-                        {
-                            if (!RemoveRealFile(location))
-                            {
-                                Console.Error.WriteLine($"Removal of real file for {location} failed.");
-                                return 1;
-                            }
-                        }
+                        var file = new PimixFile(location, FileId);
+                        file.Delete();
                     }
 
-                    // Logical removal.
-                    FileInformation.Delete(info.Id);
-                    return 0;
-                }
-                else
-                {
-                    if (!info.Locations.Contains(FileUri))
-                    {
-                        // Not found the entry. Then it's already done somehow.
-                        Console.Error.WriteLine($"File {FileUri} already removed!");
-                        return 0;
-                    }
-
-                    // Remove specific location item.
-                    if (!RemoveLinkOnly)
-                    {
-                        // Remove real files.
-                        if (!RemoveRealFile(FileUri))
-                        {
-                            Console.Error.WriteLine($"Removal of real file for {FileUri} failed.");
-                            return 1;
-                        }
-                    }
-
-                    // Logical removal.
-                    info.Locations.Remove(FileUri);
+                    info.Locations.Clear();
                     FileInformation.Patch(info);
-                    return 0;
                 }
+
+                // Logical removal.
+                FileInformation.Delete(info.Id);
+                return 0;
             }
             else
             {
-                // Since it's local path, it won't have logical path.
+                var file = new PimixFile(FileUri, FileId);
+                if (!file.FileInfo.Locations.Contains(FileUri))
+                {
+                    // Not found the entry. Then it's already done somehow.
+                    Console.Error.WriteLine($"File {FileUri} already removed!");
+                    return 0;
+                }
+
+                // Remove specific location item.
                 if (!RemoveLinkOnly)
                 {
                     // Remove real files.
-                    if (!RemoveRealFile(FileUri))
-                    {
-                        Console.Error.WriteLine($"Removal of real file for {FileUri} failed.");
-                        return 1;
-                    }
+                    file.Delete();
                 }
 
-                return 0;
-            }
-        }
+                FileInformation.RemoveLocation(file.Id, FileUri);
 
-        bool RemoveRealFile(string location)
-        {
-            try
-            {
-                new PimixFile(location).Delete();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"StorageClient.DeleteFile failed for path {location}");
-                Console.Error.WriteLine(ex);
-                return false;
+                return 0;
             }
         }
     }
