5cb0683854c663e193c2061a7b535b150eb7c3ea
Kimi @ win <kimi.ribery@gmail.com>
2017-08-28 00:29:34 +0800

    Add JobService

---
+++ src/Pimix.WinServices.JobService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceProcess;
+
+namespace Pimix.WinServices.JobService {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main() {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] {
+                new JobService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
