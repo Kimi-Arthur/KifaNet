e7990d1fd123d18401874120c8d719b01233065e
Kimi @ win <kimi.ribery@gmail.com>
2018-03-13 03:53:58 +0800

    Read credential from config file.

--- src/Pimix.Apps.FileUtil/Commands/FileUtilCommand.cs
+++ src/Pimix.Apps.FileUtil/Commands/FileUtilCommand.cs
@@ -1,23 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
+using System.Text;
 using CommandLine;
 using NLog;
 using NLog.Layouts;
 using NLog.Targets;
-using Pimix.Cloud.BaiduCloud;
+using Pimix.Service;
 
 namespace Pimix.Apps.FileUtil.Commands {
     abstract class FileUtilCommand {
         [Option('s', "pimix-server-api-address", HelpText = "Uri for pimix api server address")]
         public string PimixServerAddress { get; set; } = ConfigurationManager.AppSettings["PimixServerApiAddress"];
 
+        [Option("pimix-server-credential", HelpText = "Credential to access api server, with form: username:password")]
+        public string PimixServerCredential { get; set; } = ConfigurationManager.AppSettings["PimixServerCredential"];
+
         [Option("job-id", HelpText = "Job ID to report log as.")]
         public string JobId { get; set; } = null;
 
         public virtual void Initialize() {
-            BaiduCloudConfig.PimixServerApiAddress = PimixServerAddress;
+            PimixService.PimixServerApiAddress = PimixServerAddress;
+            PimixService.PimixServerCredential = Convert.ToBase64String(Encoding.UTF8.GetBytes(PimixServerCredential));
             var config = LogManager.Configuration;
             if (JobId != null) {
                 config.RemoveTarget("console");
@@ -25,8 +29,10 @@ namespace Pimix.Apps.FileUtil.Commands {
             }
 
             CredentialCache.DefaultNetworkCredentials.Domain = ConfigurationManager.AppSettings["DefaultNetworkDomain"];
-            CredentialCache.DefaultNetworkCredentials.UserName = ConfigurationManager.AppSettings["DefaultNetworkUserName"];
-            CredentialCache.DefaultNetworkCredentials.Password = ConfigurationManager.AppSettings["DefaultNetworkPassword"];
+            CredentialCache.DefaultNetworkCredentials.UserName =
+                ConfigurationManager.AppSettings["DefaultNetworkUserName"];
+            CredentialCache.DefaultNetworkCredentials.Password =
+                ConfigurationManager.AppSettings["DefaultNetworkPassword"];
         }
 
         public abstract int Execute();
@@ -34,10 +40,11 @@ namespace Pimix.Apps.FileUtil.Commands {
         Target ServiceTarget {
             get {
                 var target = new NetworkTarget();
-                target.Address = Layout.FromString($"{PimixServerAddress}/jobs/$log?id={JobId}&level=${{pad:padding=1:fixedLength=true:inner=${{level}}}}");
+                target.Address =
+                    Layout.FromString(
+                        $"{PimixServerAddress}/jobs/$log?id={JobId}&level=${{pad:padding=1:fixedLength=true:inner=${{level}}}}");
                 return target;
             }
         }
     }
-
 }
--- src/Pimix.Apps.JobUtil/JobUtilCommand.cs
+++ src/Pimix.Apps.JobUtil/JobUtilCommand.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Configuration;
+using System.Text;
 using CommandLine;
+using Pimix.Service;
 
-namespace Pimix.Apps.JobUtil
-{
-    abstract class JobUtilCommand
-    {
+namespace Pimix.Apps.JobUtil {
+    abstract class JobUtilCommand {
         [Option('s', "pimix-server-api-address", HelpText = "Uri for pimix api server address.")]
         public string PimixServerAddress { get; set; } = ConfigurationManager.AppSettings["PimixServerApiAddress"];
+
+        [Option("pimix-server-credential", HelpText = "Credential to access api server, with form: username:password")]
+        public string PimixServerCredential { get; set; } = ConfigurationManager.AppSettings["PimixServerCredential"];
 
         [Option('c', "client-name", HelpText = "Client name used to distinguish running jobutil instances.")]
         public string ClientName { get; set; } = ConfigurationManager.AppSettings["ClientName"];
 
-        public TimeSpan HeartbeatInterval { get; set; } = ConfigurationManager.AppSettings["HeartbeatInterval"].ParseTimeSpanString();
+        public TimeSpan HeartbeatInterval { get; set; } =
+            ConfigurationManager.AppSettings["HeartbeatInterval"].ParseTimeSpanString();
 
         [Option('b', "fire-heartbeat", HelpText = "Whether to fire heartbeat during job execution.")]
         public bool FireHeartbeat { get; set; } = false;
 
-        public virtual void Initialize()
-        {
-            Job.PimixServerApiAddress = PimixServerAddress;
+        public virtual void Initialize() {
+            PimixService.PimixServerApiAddress = PimixServerAddress;
+            PimixService.PimixServerCredential = Convert.ToBase64String(Encoding.UTF8.GetBytes(PimixServerCredential));
         }
 
         public abstract int Execute();
