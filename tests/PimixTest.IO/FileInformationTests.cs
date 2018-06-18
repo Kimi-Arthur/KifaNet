c354f2d958fac78f2172c5657c663be9d850d72a
Kimi@mac3 <kimi.ribery@gmail.com>
2018-06-17 18:11:14 -0700

    Upgraded some packages and fixed tests.

--- tests/PimixTest.IO/FileInformationTests.cs
+++ tests/PimixTest.IO/FileInformationTests.cs
@@ -20,7 +20,7 @@ namespace PimixTest.Storage {
             Assert.AreEqual("99EA7BF70F6E69AD71659995677B43F8A8312025", info.BlockSHA1[0]);
             Assert.AreEqual("8A863B145DC6E4ED7AC41C08F7536C476EBAC7509E028ED2B49F8BD5A3562B9F",
                 info.BlockSHA256[0]);
-            Assert.IsNull(info.SliceMD5);
+            Assert.AreEqual("E1B849F9631FFC1829B2E31402373E3C", info.SliceMD5);
             Assert.AreEqual("05AF01D2", info.Adler32);
 
             s = new MemoryStream(encoding.GetBytes("中文长度不一样"));
@@ -45,7 +45,7 @@ namespace PimixTest.Storage {
             Assert.AreEqual("99EA7BF70F6E69AD71659995677B43F8A8312025", info.SHA1);
             Assert.AreEqual("8A863B145DC6E4ED7AC41C08F7536C476EBAC7509E028ED2B49F8BD5A3562B9F",
                 info.SHA256);
-            Assert.IsNull(info.SliceMD5);
+            Assert.AreEqual("E1B849F9631FFC1829B2E31402373E3C", info.SliceMD5);
             Assert.AreEqual("05AF01D2", info.Adler32);
         }
 
