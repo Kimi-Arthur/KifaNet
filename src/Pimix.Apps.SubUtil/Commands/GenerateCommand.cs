314fe5add0eb5221575e06334468c216c75ba43b
Kimi@mac <kimi.ribery@gmail.com>
2018-09-19 02:40:01 +0200

    Calculate collision.

--- src/Pimix.Apps.SubUtil/Commands/GenerateCommand.cs
+++ src/Pimix.Apps.SubUtil/Commands/GenerateCommand.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using CommandLine;
+using NLog;
 using Pimix.Api.Files;
 using Pimix.Bilibili;
 using Pimix.Subtitle.Ass;
@@ -12,6 +13,8 @@ using Pimix.Subtitle.Srt;
 namespace Pimix.Apps.SubUtil.Commands {
     [Verb("generate", HelpText = "Generate subtitle.")]
     class GenerateCommand : SubUtilCommand {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         [Value(0, Required = true, HelpText = "Target file to generate subtitle for.")]
         public string FileUri { get; set; }
 
@@ -37,8 +40,12 @@ namespace Pimix.Apps.SubUtil.Commands {
             var events = new AssEventsSection();
 
             events.Events.AddRange(srts.Values.First().Lines.Select(x => x.ToAss()));
-            events.Events.AddRange(chats.Values.First().Comments
-                .Select(x => x.GenerateAssDialogue()));
+
+            var bilibiliComments = chats.Values.First().Comments
+                .Select(x => x.GenerateAssDialogue()).ToList();
+            AddMove(bilibiliComments);
+
+            events.Events.AddRange(bilibiliComments);
 
             document.Sections.Add(events);
 
@@ -57,6 +64,74 @@ namespace Pimix.Apps.SubUtil.Commands {
 
 
             return 0;
+        }
+
+        void AddMove(List<AssDialogue> comments) {
+            var screenWidth = 1920;
+
+            var sizes = comments
+                .Select(x => x.Text.ToString().Length * 50).ToList();
+
+            var speeds = sizes.Zip(comments,
+                    (s, c) => (screenWidth + s) / (c.End - c.Start).TotalSeconds)
+                .ToList();
+
+            var rows = new List<SortedList<TimeSpan, int>>();
+            var maxRows = 14;
+            for (int i = 0; i < maxRows; i++) {
+                rows.Add(new SortedList<TimeSpan, int>());
+            }
+
+            var collide = new Func<int, int, bool>((a, b) => {
+                if ((comments[b].Start - comments[a].Start).TotalSeconds * speeds[a] <
+                    sizes[a]) {
+                    return true;
+                }
+
+                if ((comments[a].End - comments[b].Start).TotalSeconds * speeds[b] >
+                    screenWidth) {
+                    return true;
+                }
+
+                return false;
+            });
+
+            var notAdded = 0;
+
+            for (int i = 0; i < comments.Count; i++) {
+                var added = false;
+                foreach (var row in rows) {
+                    if (row.ContainsKey(comments[i].Start)) {
+                        continue;
+                    }
+
+                    row.Add(comments[i].Start, i);
+                    var index = row.IndexOfKey(comments[i].Start);
+                    if (index > 0 && collide(row.ElementAt(index - 1).Value, i)) {
+                        row.RemoveAt(index);
+                        continue;
+                    }
+
+                    if (index < row.Count - 1 && collide(i, row.ElementAt(index + 1).Value)) {
+                        row.RemoveAt(index);
+                        continue;
+                    }
+
+                    added = true;
+                    break;
+                }
+
+                if (!added) {
+                    logger.Warn("Comment {} not added.", comments[i].Text);
+                    notAdded++;
+                }
+            }
+
+            foreach (var row in rows) {
+                logger.Info("Row has {} comments", row.Count);
+            }
+
+            logger.Info("{} comments not added.", notAdded);
         }
 
         void ConfigureStyles() {
