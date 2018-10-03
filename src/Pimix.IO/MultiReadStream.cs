7879fba710cdd39ef903fad496fbd275212d91fd
Kimi@mac3 <kimi.ribery@gmail.com>
2018-10-03 21:01:25 +0200

    Added dummy MultiReadStream.

---
+++ src/Pimix.IO/MultiReadStream.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Pimix.IO {
+    public class MultiReadStream : Stream {
+        public MultiReadStream(List<Stream> streams) {
+            Streams = streams;
+            Length = Streams.Sum(s => s.Length);
+        }
+
+        public List<Stream> Streams { get; set; }
+
+        public override int Read(byte[] buffer, int offset, int count) => throw new System.NotImplementedException();
+
+        public override void Flush() {
+            // Intentionally doing nothing.
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            switch (origin) {
+                case SeekOrigin.Begin:
+                    Position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    Position += offset;
+                    break;
+                case SeekOrigin.End:
+                    Position = Length + offset;
+                    break;
+            }
+
+            if (Position < 0) {
+                throw new ArgumentException(nameof(offset));
+            }
+
+            return Position;
+        }
+
+        public override void SetLength(long value) =>
+            throw new NotSupportedException($"{nameof(SeekableReadStream)} is not writable.");
+
+        public override void Write(byte[] buffer, int offset, int count) =>
+            throw new NotSupportedException($"{nameof(SeekableReadStream)} is not writable.");
+
+        public override bool CanRead => true;
+        public override bool CanSeek => true;
+        public override bool CanWrite => false;
+        public override long Length { get; }
+        public override long Position { get; set; }
+    }
+}
