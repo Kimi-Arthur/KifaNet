972825da5f0a66ff70fdb49095f1cecdc9afec6d
Kimi@mac3 <kimi.ribery@gmail.com>
2018-10-26 17:03:18 -0700

    Update PimixTest.Service to xUnit.

--- tests/PimixTest.Service/DataModelTests.cs
+++ tests/PimixTest.Service/DataModelTests.cs
@@ -1,66 +1,57 @@
 ﻿using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pimix.Service;
+using Xunit;
 
 namespace PimixTest.Service {
-    [TestClass]
     public class DataModelTests {
-        public string PimixServerApiAddress => "http://www.pimix.tk/api";
 
-        [TestMethod]
+        public DataModelTests() {
+            PimixService.PimixServerApiAddress = "http://www.pimix.tk/api";
+            FakeDataModel.Reset();
+        }
+
+        [Fact]
         public void DataModelGetBasicTest() {
             var data = FakeDataModel.Get("item0");
-            Assert.AreEqual("item0", data.Id);
-            Assert.AreEqual(1225, data.IntProp);
-            Assert.AreEqual("str prop value", data.StrProp);
-            CollectionAssert.AreEqual(
+            Assert.Equal("item0", data.Id);
+            Assert.Equal(1225, data.IntProp);
+            Assert.Equal("str prop value", data.StrProp);
+            Assert.Equal(
                 new List<string> {
                     "list prop value 1",
                     "list prop value 2",
                     ""
                 },
                 data.ListProp);
-            CollectionAssert.AreEqual(
+            Assert.Equal(
                 new Dictionary<string, string> {
                     ["dict prop key 1"] = "dict prop value 1",
                     ["dict prop key 2"] = "dict prop value 2"
                 },
                 data.DictProp);
-            Assert.AreEqual("sub prop 1 value", data.SubProp.SubProp1);
-            CollectionAssert.AreEqual(
+            Assert.Equal("sub prop 1 value", data.SubProp.SubProp1);
+            Assert.Equal(
                 new List<string> {
                     "sub prop 2 value 1",
                     "sub prop 2 value 2"
                 }, data.SubProp.SubProp2);
         }
 
-        [TestMethod]
+        [Fact]
         public void DataModelCallBasicTest() {
             FakeDataModel.Reset();
         }
 
-        [TestMethod]
+        [Fact]
         public void DataModelPatchBasicTest() {
             var data = FakeDataModel.Get("item0");
-            Assert.AreEqual(1225, data.IntProp);
-            Assert.AreEqual("str prop value", data.StrProp);
+            Assert.Equal(1225, data.IntProp);
+            Assert.Equal("str prop value", data.StrProp);
             FakeDataModel.Patch(new FakeDataModel {IntProp = 19910123}, "item0");
             FakeDataModel.Patch(new FakeDataModel {StrProp = "new str", Id = "item0"});
             data = FakeDataModel.Get("item0");
-            Assert.AreEqual(19910123, data.IntProp);
-            Assert.AreEqual("new str", data.StrProp);
-        }
-
-        [TestInitialize]
-        public void Initialize() {
-            PimixService.PimixServerApiAddress = PimixServerApiAddress;
-
-            FakeDataModel.Reset();
-        }
-
-        [TestCleanup]
-        public void Cleanup() {
-            FakeDataModel.Reset();
+            Assert.Equal(19910123, data.IntProp);
+            Assert.Equal("new str", data.StrProp);
         }
     }
 }
