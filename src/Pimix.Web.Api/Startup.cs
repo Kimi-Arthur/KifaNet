17f0288237876cccb470110bb4e0e1545db3768e
Kimi@mac <kimi.ribery@gmail.com>
2018-11-02 20:29:02 +0100

    Output with yaml or pretty json based on accept.

--- src/Pimix.Web.Api/Startup.cs
+++ src/Pimix.Web.Api/Startup.cs
@@ -1,10 +1,14 @@
+using System.Buffers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using WebApiContrib.Core.Formatter.Yaml;
 
 namespace Pimix.Web.Api {
     public class Startup {
@@ -16,17 +20,37 @@ namespace Pimix.Web.Api {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-                .AddJsonOptions(opt => {
-                    opt.SerializerSettings.ContractResolver = new DefaultContractResolver {
+            var prettyJsonFormatter = new JsonOutputFormatter(
+                new JsonSerializerSettings {
+                    Formatting = Formatting.Indented,
+                    ContractResolver = new DefaultContractResolver {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                    },
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MetadataPropertyHandling = MetadataPropertyHandling.Ignore
+                }, ArrayPool<char>.Shared);
+            prettyJsonFormatter.SupportedMediaTypes.Clear();
+            prettyJsonFormatter.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("*/*"));
+
+            services.AddMvc(options => {
+                    options.InputFormatters.Add(new YamlInputFormatter(new YamlFormatterOptions()));
+                    options.OutputFormatters.Add(
+                        new YamlOutputFormatter(new YamlFormatterOptions()));
+                    options.OutputFormatters.Insert(0, prettyJsonFormatter);
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options => {
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver {
                         NamingStrategy = new SnakeCaseNamingStrategy()
                     };
-                    opt.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                    opt.SerializerSettings.MetadataPropertyHandling = MetadataPropertyHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.MetadataPropertyHandling =
+                        MetadataPropertyHandling.Ignore;
                 });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
