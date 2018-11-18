628e5d0849a88c4b40c2440f42cef77b954e34f7
Kimi@mac <kimi.ribery@gmail.com>
2018-11-18 21:20:22 +0100

    Add RoundUp, RoundDown for double.

--- src/Pimix/Extensions/MathExtensions.cs
+++ src/Pimix/Extensions/MathExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Pimix {
+﻿using System;
+
+namespace Pimix {
     public static class MathExtensions {
         public static long RoundUp(this long value, long period)
             => value % period == 0 ? value : value - value % period + period;
@@ -6,10 +8,16 @@
         public static int RoundUp(this int value, int period)
             => value % period == 0 ? value : value - value % period + period;
 
+        public static int RoundUp(this double value, int period)
+            => (int) Math.Ceiling(value / period) * period;
+
         public static long RoundDown(this long value, long period)
             => value % period == 0 ? value : value - value % period;
 
         public static int RoundDown(this int value, int period)
             => value % period == 0 ? value : value - value % period;
+
+        public static int RoundDown(this double value, int period)
+            => (int) Math.Floor(value / period) * period;
     }
 }
