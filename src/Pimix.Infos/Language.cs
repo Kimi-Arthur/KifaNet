a38b588feb8bc165bb7eeff52f6bc7b67526e911
Kimi Arthur <kimi.ribery@gmail.com>
2019-04-18 13:54:51 +0200

    Make Language and Region an actual class.

---
+++ src/Pimix.Infos/Language.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pimix.Infos {
+    public class Language : JsonSerializable {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public string ToJson() => Code;
+        public override string ToString() => Name;
+
+        public void FromJson(string data) {
+            var lang = All[data];
+            Name = lang.Name;
+            Code = lang.Code;
+        }
+
+        public static readonly Language Chinese = new Language {
+            Name = "Chinese",
+            Code = "zh"
+        };
+
+        public static readonly Language English = new Language {
+            Name = "English",
+            Code = "en"
+        };
+
+        public static readonly Language Japanese = new Language {
+            Name = "Japanese",
+            Code = "ja"
+        };
+
+        public static readonly Dictionary<string, Language> All = new List<Language> {
+            Chinese,
+            English,
+            Japanese
+        }.SelectMany(r => new List<(string key, Language value)> {
+            (r.Code, r),
+            (r.Name, r)
+        }).ToDictionary(tuple => tuple.key, tuple => tuple.value);
+    }
+}
---
+++ src/Pimix.Infos/Region.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pimix.Infos {
+    public class Region : JsonSerializable {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public string ToJson() => Name;
+
+        public override string ToString() => Name;
+
+        public void FromJson(string data) {
+            var region = All[data];
+            Name = region.Name;
+            Code = region.Code;
+        }
+
+        public static readonly Region UnitedStates = new Region {
+            Name = "United States",
+            Code = "US"
+        };
+
+        public static readonly Region UnitedKingdom = new Region {
+            Name = "United Kingdom",
+            Code = "UK"
+        };
+
+        public static readonly Region Japan = new Region {
+            Name = "Japan",
+            Code = "JP"
+        };
+
+        public static readonly Region China = new Region {
+            Name = "China",
+            Code = "CN"
+        };
+
+        public static readonly Dictionary<string, Region> All = new List<Region> {
+            UnitedStates,
+            UnitedKingdom,
+            Japan,
+            China
+        }.SelectMany(r => new List<(string key, Region value)> {
+            (r.Code, r),
+            (r.Name, r)
+        }).ToDictionary(tuple => tuple.key, tuple => tuple.value);
+    }
+}
