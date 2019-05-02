4da8549261949dc9b694a7c657475969b429e8c8
Kimi Arthur <kimi.ribery@gmail.com>
2019-05-02 19:52:53 +0200

    Rename TEntity to TDataModel.

--- src/Pimix.Web.Api/Controllers/PimixController.cs
+++ src/Pimix.Web.Api/Controllers/PimixController.cs
@@ -6,37 +6,37 @@ using Pimix.Service;
 namespace Pimix.Web.Api.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public abstract class PimixController<TEntity> : ControllerBase {
+    public abstract class PimixController<TDataModel> : ControllerBase {
         protected PimixServiceClient client = new PimixServiceRestClient();
 
         // GET api/values
         [HttpGet]
-        public ActionResult<Dictionary<string, TEntity>> Get() {
-            return new Dictionary<string, TEntity> {
+        public ActionResult<Dictionary<string, TDataModel>> Get() {
+            return new Dictionary<string, TDataModel> {
                 ["/Downloads/Anime/DA01/[数码兽大冒险].[加七][Digimon_Adventure][01][GB].rmvb"] =
-                    client.Get<TEntity>("/Downloads/Anime/DA01/[数码兽大冒险].[加七][Digimon_Adventure][01][GB].rmvb")
+                    client.Get<TDataModel>("/Downloads/Anime/DA01/[数码兽大冒险].[加七][Digimon_Adventure][01][GB].rmvb")
             };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<TEntity> Get(string id) {
+        public ActionResult<TDataModel> Get(string id) {
             id = Uri.UnescapeDataString(id);
-            return client.Get<TEntity>(id);
+            return client.Get<TDataModel>(id);
         }
 
         // POST api/values
         [HttpPost("{id}")]
-        public void Post(string id, [FromBody] TEntity value) {
+        public void Post(string id, [FromBody] TDataModel value) {
             id = Uri.UnescapeDataString(id);
-            client.Get<TEntity>(id);
+            client.Get<TDataModel>(id);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(string id) {
             id = Uri.UnescapeDataString(id);
-            client.Delete<TEntity>(id);
+            client.Delete<TDataModel>(id);
         }
     }
 }
