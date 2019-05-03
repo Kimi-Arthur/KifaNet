44d42cd6087b9d7419d4029f42988bbde225dd88
Kimi Arthur <kimi.ribery@gmail.com>
2019-05-03 11:51:11 +0200

    Basic grouping support in PimixServiceJsonClient.

--- src/Pimix.Service/PimixServiceJsonClient.cs
+++ src/Pimix.Service/PimixServiceJsonClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace Pimix.Service {
@@ -9,10 +10,19 @@ namespace Pimix.Service {
     }
 
     public class PimixServiceJsonClient<TDataModel> : PimixServiceClient<TDataModel> {
+        Dictionary<string, List<string>> Groups { get; set; } = new Dictionary<string, List<string>>();
+
         public override TDataModel Get(string id) {
-            return JsonConvert.DeserializeObject<TDataModel>(
-                File.ReadAllText($"{PimixServiceJsonClient.DataFolder}/{modelId}/{id.Trim('/')}.json"),
-                Defaults.JsonSerializerSettings);
+            LoadGroup();
+
+            if (Groups.ContainsKey(id)) {
+                var obj = JsonConvert.DeserializeObject<TDataModel>(Read(Groups[id].First().Trim('/')),
+                    Defaults.JsonSerializerSettings);
+                JsonConvert.PopulateObject(Read(id), obj);
+                return obj;
+            }
+
+            return JsonConvert.DeserializeObject<TDataModel>(Read(id), Defaults.JsonSerializerSettings);
         }
 
         public override void Set(TDataModel data, string id = null) {
@@ -41,5 +51,21 @@ namespace Pimix.Service {
 
         public override TResponse Call<TResponse>(string action, string id = null,
             Dictionary<string, object> parameters = null) => throw new NotImplementedException();
+
+        string Read(string id) =>
+            File.ReadAllText($"{PimixServiceJsonClient.DataFolder}/{modelId}/{id.Trim('/')}.json");
+
+        void LoadGroup() {
+            var rawGroups = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(
+                File.ReadAllText($"{PimixServiceJsonClient.DataFolder}/metadata/{modelId}/groups.json"));
+            Groups.Clear();
+            foreach (var rawGroup in rawGroups) {
+                var group = rawGroup.Value;
+                group.Insert(0, rawGroup.Key);
+                foreach (var i in group) {
+                    Groups[i] = group;
+                }
+            }
+        }
     }
 }
