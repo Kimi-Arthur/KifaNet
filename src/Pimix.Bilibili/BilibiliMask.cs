825b945d3f395bce34c5640f13270acbe21e189a
Kimi Arthur <kimi.ribery@gmail.com>
2019-05-10 18:21:46 +0200

    Dummy BilibiliMask.

---
+++ src/Pimix.Bilibili/BilibiliMask.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pimix.Subtitle.Ass;
+using Svg;
+
+namespace Pimix.Bilibili {
+    public class BilibiliMask {
+        List<(TimeSpan start, SvgImage mask)> Masks = new List<(TimeSpan start, SvgImage mask)>(); 
+        public void ProcessDialogues(List<AssDialogue> dialogs) {
+            var sortedDialogs = dialogs.OrderBy(d => d.Start).ToList();
+            var currentMask = 0;
+            foreach (var dialog in sortedDialogs) {
+                currentMask = AddMask(dialog, currentMask);
+            }
+        }
+
+        int AddMask(AssDialogue dialog, int currentMask) {
+            while (currentMask + 1 < Masks.Count && Masks[currentMask + 1].start < dialog.Start) {
+                currentMask++;
+            }
+
+            var clipElements = new AssDialogueControlTextElement();
+            
+            if (Masks[currentMask].start < dialog.Start) {
+                clipElements.Elements.Add(GetDrawing(Masks[currentMask].mask));
+            }
+            
+            foreach (var mask in Masks.Skip(currentMask)) {
+                
+            }
+
+            dialog.Text.TextElements.Insert(0, clipElements);
+
+            return currentMask;
+        }
+
+        DrawingClipFunction GetDrawing(SvgImage image) {
+            return new DrawingClipFunction();
+        }
+    }
+}
