85be491ce65c7bb6c661d284a5a8e0fdd20fa64c
Kimi Arthur <kimi.ribery@gmail.com>
2019-11-18 18:37:42 +0100

    Remove old WebResponse related code.

---
+++ src/Pimix/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace Pimix {
+    public static class HttpResponseMessageExtensions {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static string GetString(this HttpResponseMessage response) {
+            using var sr = new StreamReader(response.Content.ReadAsStreamAsync().Result,
+                Encoding.GetEncoding("UTF-8"));
+            var data = sr.ReadToEnd();
+            logger.Trace("Response ({0:D}): {1}", response.StatusCode, data);
+            return data;
+        }
+
+        public static JToken GetJToken(this HttpResponseMessage response)
+            => JToken.Parse(GetString(response));
+
+        public static T GetObject<T>(this HttpResponseMessage response)
+            => JsonConvert.DeserializeObject<T>(GetString(response), Defaults.JsonSerializerSettings);
+    }
+}
