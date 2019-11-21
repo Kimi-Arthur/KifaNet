54fc281bd5ce629edd1698bc8b6773f03609a7d5
Kimi Arthur <kimi.ribery@gmail.com>
2019-11-21 16:51:34 +0100

    Dummy SendWithRetry implementation.

--- src/Pimix/Extensions/HttpExtensions.cs
+++ src/Pimix/Extensions/HttpExtensions.cs
@@ -6,8 +6,11 @@ using Newtonsoft.Json.Linq;
 using NLog;
 
 namespace Pimix {
-    public static class HttpResponseMessageExtensions {
+    public static class HttpExtensions {
         static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static HttpResponseMessage SendWithRetry(this HttpClient client, HttpRequestMessage request) =>
+            client.SendAsync(request).Result;
 
         public static string GetString(this HttpResponseMessage response) {
             using var sr = new StreamReader(response.Content.ReadAsStreamAsync().Result,
