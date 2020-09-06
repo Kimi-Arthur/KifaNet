6ec81c14efeda4fdd8be5cef822d4bacc82fbb62
Kimi@ZRH <kimi.ribery@gmail.com>
2020-09-06 13:09:51 +0200

    Update encoding of bat file to make it work properly.

--- tools/publish.bat
+++ tools/publish.bat
@@ -1,3 +1,3 @@
-﻿rmdir /s /q publish
+rmdir /s /q publish
 dotnet pack -o publish -c Release --include-symbols %1
 dotnet nuget push publish\*.symbols.nupkg -s https://api.nuget.org/v3/index.json -k oy2jqpkcvxsj2eudjriaxwmimioty73u7atgnf3wpukr3u
