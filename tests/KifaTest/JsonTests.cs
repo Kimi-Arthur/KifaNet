8ed851a016b0e669f8853c25e17c86912a6b1803
Kimi Arthur <kimi.ribery@gmail.com>
2020-12-23 15:56:24 +0800

    Add failing test for Enum key json.

---
+++ tests/KifaTest/JsonTests.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Pimix;
+using Xunit;
+
+namespace KifaTest {
+    public enum MyEnum {
+        Value1,
+        TextValue
+    }
+
+    public class DataClass {
+        public Dictionary<MyEnum, string> Dict { get; set; }
+    }
+
+    public class JsonTests {
+        static readonly DataClass Decoded = new DataClass {
+            Dict = new Dictionary<MyEnum, string> {{MyEnum.Value1, "a"}, {MyEnum.TextValue, "b"}}
+        };
+
+        const string Encoded = "{\"dict\":{\"value1\":\"a\",\"text_value\":\"b\"}}";
+
+        [Fact]
+        public void EnumDictionaryKeySerializeTest() {
+            var v = JsonConvert.SerializeObject(Decoded, Defaults.JsonSerializerSettings);
+            Assert.Equal(Encoded, v);
+        }
+
+        [Fact]
+        public void EnumDictionaryKeyDeserializeTest() {
+            var v = JsonConvert.DeserializeObject<DataClass>(Encoded, Defaults.JsonSerializerSettings);
+            Assert.Equal(2, v.Dict.Count);
+        }
+    }
+}
