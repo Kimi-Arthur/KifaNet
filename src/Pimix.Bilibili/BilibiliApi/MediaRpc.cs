9845e5b9fb86f7b68f084b23e9dc032d526e202b
Kimi@mac2 <kimi.ribery@gmail.com>
2021-01-06 19:34:32 +0800

    Add MediaRpc.

---
+++ src/Pimix.Bilibili/BilibiliApi/MediaRpc.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Pimix.Bilibili.BilibiliApi {
+    public class MediaRpc : JsonRpc<string, MediaRpc.MediaResponse> {
+        public class MediaResponse {
+            public long Code { get; set; }
+            public string Message { get; set; }
+            public Result Result { get; set; }
+        }
+
+        public class Result {
+            public Media Media { get; set; }
+        }
+
+        public class Media {
+            public List<Area> Areas { get; set; }
+            public Uri Cover { get; set; }
+            public long MediaId { get; set; }
+            public NewEp NewEp { get; set; }
+            public Rating Rating { get; set; }
+            public long SeasonId { get; set; }
+            public Uri ShareUrl { get; set; }
+            public string Title { get; set; }
+            public string TypeName { get; set; }
+        }
+
+        public class Area {
+            public long Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class NewEp {
+            public long Id { get; set; }
+            public string Index { get; set; }
+            public string IndexShow { get; set; }
+        }
+
+        public class Rating {
+            public long Count { get; set; }
+            public double Score { get; set; }
+        }
+
+        const string UrlPattern = "https://api.bilibili.com/pgc/review/user?media_id={id}";
+
+        static HttpClient client = BilibiliVideo.GetBilibiliClient();
+
+        public override MediaResponse Call(string mediaId) {
+            var url = UrlPattern.Format(new Dictionary<string, string> {{"id", mediaId.Substring(2)}});
+            return client.GetAsync(url).Result.GetObject<MediaResponse>();
+        }
+    }
+}
---
+++ tests/KifaTest.Bilibili/BilibiliMediaTests.cs
@@ -0,0 +1,17 @@
+using Pimix.Bilibili.BilibiliApi;
+using Xunit;
+
+namespace KifaTest.Bilibili {
+    public class BilibiliMediaTests {
+        [Theory]
+        [InlineData("md28222837", "深夜食堂 第三季", "电视剧", 28671)]
+        [InlineData("md28231812", "关于我转生变成史莱姆这档事 第二季", "番剧", 36170)]
+        [InlineData("md59632", "深夜食堂", "电影", 12075)]
+        public void MediaRpcTest(string mediaId, string title, string typeName, long seasonId) {
+            var result = new MediaRpc().Call(mediaId).Result.Media;
+            Assert.Equal(title, result.Title);
+            Assert.Equal(typeName, result.TypeName);
+            Assert.Equal(seasonId, result.SeasonId);
+        }
+    }
+}
