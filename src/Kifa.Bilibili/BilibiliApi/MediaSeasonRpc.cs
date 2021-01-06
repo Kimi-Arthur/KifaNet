31ef892d7d1729e81b020939801074037d114c48
Kimi@mac2 <kimi.ribery@gmail.com>
2021-01-06 20:16:56 +0800

    Add MediaSeasonRpc.

---
+++ src/Kifa.Bilibili/BilibiliApi/MediaSeasonRpc.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using Pimix;
+
+namespace Kifa.Bilibili.BilibiliApi {
+    public class MediaSeasonRpc : JsonRpc<string, MediaSeasonRpc.MediaSeasonResponse> {
+        public class MediaSeasonResponse {
+            public long Code { get; set; }
+            public string Message { get; set; }
+            public Result Result { get; set; }
+        }
+
+        public class Result {
+            public SectionInfo MainSection { get; set; }
+            public List<SectionInfo> Section { get; set; }
+        }
+
+        public class SectionInfo {
+            public List<SectionEpisode> Episodes { get; set; }
+            public long Id { get; set; }
+            public string Title { get; set; }
+            public long Type { get; set; }
+        }
+
+        public class SectionEpisode {
+            public long Aid { get; set; }
+            public string Badge { get; set; }
+            public BadgeInfo BadgeInfo { get; set; }
+            public long BadgeType { get; set; }
+            public long Cid { get; set; }
+            public Uri Cover { get; set; }
+            public string From { get; set; }
+            public long Id { get; set; }
+            public long IsPremiere { get; set; }
+            public string LongTitle { get; set; }
+            public Uri ShareUrl { get; set; }
+            public long Status { get; set; }
+            public string Title { get; set; }
+            public string Vid { get; set; }
+        }
+
+        public class BadgeInfo {
+            public Color BgColor { get; set; }
+            public Color BgColorNight { get; set; }
+            public string Text { get; set; }
+        }
+
+        const string UrlPattern = "https://api.bilibili.com/pgc/web/season/section?season_id={id}";
+
+        static HttpClient client = BilibiliVideo.GetBilibiliClient();
+
+        public override MediaSeasonResponse Call(string seasonId) {
+            var url = UrlPattern.Format(new Dictionary<string, string> {{"id", seasonId.Substring(2)}});
+            return client.GetAsync(url).Result.GetObject<MediaSeasonResponse>();
+        }
+    }
+}
--- tests/Kifa.Bilibili.Tests/BilibiliMediaTests.cs
+++ tests/Kifa.Bilibili.Tests/BilibiliMediaTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Kifa.Bilibili.BilibiliApi;
 using Xunit;
 
@@ -12,6 +13,27 @@ namespace Kifa.Bilibili.Tests {
             Assert.Equal(title, result.Title);
             Assert.Equal(typeName, result.TypeName);
             Assert.Equal(seasonId, result.SeasonId);
+        }
+
+        [Theory]
+        [InlineData("ss28671", "正片", 70710330, "第二十一话", "炸肉饼", "相关视频", 69501035, "预告1", "")]
+        [InlineData("ss36170", "正片", 373619156, "24.9", "闲话：日向·坂口", "PV", 755557176, "PV1", "")]
+        [InlineData("ss12075", "正片", 14681109, "正片", "")]
+        public void MediaSeasonRpcTest(string seasonId, string title, long episode1Id, string episode1Title,
+            string episode1LongTitle, string otherTitle = null, long otherEpisode1Id = 0,
+            string otherEpisode1Title = null, string otherEpisode1LongTitle = null) {
+            var result = new MediaSeasonRpc().Call(seasonId).Result;
+            Assert.Equal(title, result.MainSection.Title);
+            Assert.Equal(episode1Id, result.MainSection.Episodes.First().Aid);
+            Assert.Equal(episode1Title, result.MainSection.Episodes.First().Title);
+            Assert.Equal(episode1LongTitle, result.MainSection.Episodes.First().LongTitle);
+            if (otherTitle != null) {
+                var otherSection = result.Section.First();
+                Assert.Equal(otherTitle, otherSection.Title);
+                Assert.Equal(otherEpisode1Id, otherSection.Episodes.First().Aid);
+                Assert.Equal(otherEpisode1Title, otherSection.Episodes.First().Title);
+                Assert.Equal(otherEpisode1LongTitle, otherSection.Episodes.First().LongTitle);
+            }
         }
     }
 }
