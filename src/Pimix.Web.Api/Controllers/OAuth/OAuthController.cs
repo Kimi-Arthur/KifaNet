ceb138c2daf504c4a3d127ef60854d6539e7c916
Kimi@mac2 <kimi.ribery@gmail.com>
2021-01-10 20:09:00 +0800

    Minor fix.

--- src/Pimix.Web.Api/Controllers/OAuth/OAuthController.cs
+++ src/Pimix.Web.Api/Controllers/OAuth/OAuthController.cs
@@ -19,9 +19,9 @@ namespace Pimix.Web.Api.Controllers.OAuth {
         const string RefreshTokenUrlPattern =
             "https://oauth2.googleapis.com/token?grant_type=refresh_token&refresh_token={refresh_token}&client_id={client_id}&client_secret={client_secret}";
 
-        static HttpClient httpClient = new();
+        static readonly HttpClient HttpClient = new();
 
-        static PimixServiceJsonClient<GoogleAccount> serviceClient = new();
+        static readonly PimixServiceJsonClient<GoogleAccount> ServiceClient = new();
 
         [HttpGet("add")]
         public RedirectResult AccountAdd() {
@@ -43,7 +43,7 @@ namespace Pimix.Web.Api.Controllers.OAuth {
                 {"redirect_url", HttpUtility.UrlEncode(this.ForAction(nameof(AccountRedirect)))}
             });
 
-            var response = httpClient.PostAsync(tokenUrl, null).Result.GetJToken();
+            var response = HttpClient.PostAsync(tokenUrl, null).Result.GetJToken();
             var account = new GoogleAccount {
                 AccessToken = (string) response["access_token"],
                 RefreshToken = (string) response["refresh_token"],
@@ -52,29 +52,29 @@ namespace Pimix.Web.Api.Controllers.OAuth {
 
             var userInfoUrl =
                 UserInfoUrlPattern.Format(new Dictionary<string, string> {{"access_token", account.AccessToken}});
-            var info = httpClient.GetAsync(userInfoUrl).Result.GetJToken();
+            var info = HttpClient.GetAsync(userInfoUrl).Result.GetJToken();
             account.Id = account.UserName = (string) info["email"];
             account.UserId = (string) info["id"];
-            serviceClient.Set(account);
+            ServiceClient.Set(account);
             return Content(account.ToString());
         }
 
         [HttpGet("refresh")]
         public ContentResult AccountRefresh([FromQuery] string id) {
-            var account = serviceClient.Get(id);
+            var account = ServiceClient.Get(id);
             var refreshTokenUrl = RefreshTokenUrlPattern.Format(new Dictionary<string, string> {
                 {"client_id", GoogleCloudConfigs.ClientId},
                 {"client_secret", GoogleCloudConfigs.ClientSecret},
                 {"refresh_token", account.RefreshToken}
             });
 
-            var response = httpClient.PostAsync(refreshTokenUrl, null).Result.GetJToken();
+            var response = HttpClient.PostAsync(refreshTokenUrl, null).Result.GetJToken();
 
 
             account.RefreshToken = (string) response["refresh_token"];
             account.AccessToken = (string) response["access_token"];
 
-            serviceClient.Set(account);
+            ServiceClient.Set(account);
             return Content(account.ToString());
         }
     }
