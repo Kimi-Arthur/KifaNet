b0741d546cbcd21623079864edf513ba3801dc06
Kimi@mac2 <kimi.ribery@gmail.com>
2021-01-29 16:25:45 +0800

    Copy over everything API has.

--- src/Pimix/JsonRpc.cs
+++ src/Pimix/JsonRpc.cs
@@ -1,14 +1,42 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
+using NLog;
 
 namespace Pimix {
     public abstract class JsonRpc<TResponse> {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public virtual HttpClient HttpClient { get; } = new();
+
         public abstract string UrlPattern { get; }
-        public abstract HttpClient HttpClient { get; }
+
+        public virtual HttpMethod Method { get; } = HttpMethod.Get;
+
+        public virtual Dictionary<string, string> Headers { get; } = new();
+
+        public virtual string Data { get; set; }
 
         public TResponse Call(Dictionary<string, string> parameters) {
-            var url = UrlPattern.Format(parameters);
-            return HttpClient.GetAsync(url).Result.GetObject<TResponse>();
+            var address = UrlPattern.Format(parameters);
+            logger.Trace($"{Method} {address}");
+
+            var request = new HttpRequestMessage(Method, address);
+
+            foreach (var (headerName, value) in Headers.Where(h => !h.Key.StartsWith("Content-"))) {
+                request.Headers.Add(headerName, value.Format(parameters));
+            }
+
+            if (Data != null) {
+                request.Content = new ByteArrayContent(Encoding.UTF8.GetBytes(Data.Format(parameters)));
+
+                foreach (var (headerName, value) in Headers.Where(h => h.Key.StartsWith("Content-"))) {
+                    request.Content.Headers.Add(headerName, value.Format(parameters));
+                }
+            }
+
+            return HttpClient.Send(request).GetObject<TResponse>();
         }
     }
 }
