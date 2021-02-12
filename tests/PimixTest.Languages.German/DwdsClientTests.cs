68cfe68c2438e8c43014b2aa971fdd250f893798
Kimi@mac2 <kimi.ribery@gmail.com>
2021-02-12 12:45:05 +0800

    Multi pronunciation case.

--- tests/PimixTest.Languages.German/DwdsClientTests.cs
+++ tests/PimixTest.Languages.German/DwdsClientTests.cs
@@ -6,6 +6,7 @@ namespace PimixTest.Languages.German {
         [Theory]
         [InlineData("malen", "https://media.dwds.de/dwds2/audio/019/mahlen.mp3")]
         [InlineData("Ankunft", "https://media.dwds.de/dwds2/audio/004/die_Ankunft.mp3")]
+        [InlineData("Laptop", "https://media.dwds.de/dwds2/audio/100/der_Laptop.mp3")]
         public void AudioLinkTest(string wordId, string link) {
             var client = new DwdsClient();
             var word = client.GetWord(wordId);
