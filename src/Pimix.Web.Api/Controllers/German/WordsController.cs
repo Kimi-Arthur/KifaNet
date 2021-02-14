34eb46fc74e2484c0904a6b1281c4e8905b3dc6f
Kimi@mac2 <kimi.ribery@gmail.com>
2021-02-14 10:05:09 +0800

    Word is normally stable.

--- src/Pimix.Web.Api/Controllers/German/WordsController.cs
+++ src/Pimix.Web.Api/Controllers/German/WordsController.cs
@@ -4,6 +4,7 @@ using Pimix.Languages.German;
 namespace Pimix.Web.Api.Controllers {
     [Route("api/" + Word.ModelId)]
     public class WordsController : KifaDataController<Word, WordJsonServiceClient> {
+        protected override bool ShouldAutoRefresh => false;
     }
 
     public class WordJsonServiceClient : KifaServiceJsonClient<Word>, WordServiceClient {
