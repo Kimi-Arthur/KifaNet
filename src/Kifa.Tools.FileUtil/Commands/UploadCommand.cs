b3479b3d05866dcae42b830c7586f583468f940c
Kimi@mac2 <kimi.ribery@gmail.com>
2021-02-15 23:35:43 +0800

    No diff move of functionality.

--- src/Kifa.Tools.FileUtil/Commands/UploadCommand.cs
+++ src/Kifa.Tools.FileUtil/Commands/UploadCommand.cs
@@ -48,128 +48,13 @@ namespace Kifa.Tools.FileUtil.Commands {
                 Console.ReadLine();
             }
 
-            var results = files.Select(f => (f.ToString(), UploadFile(new KifaFile(f.ToString()), true))).ToList();
-            return results.Select(r => r.Item2)
-                .Concat(results.Where(r => r.Item2 == -1).Select(r => UploadFile(new KifaFile(r.Item1)))).Max();
-        }
-
-        int UploadFile(KifaFile source, bool skipRegistered = false) {
-            source.UseCache = UseCache | DownloadLocal;
-            // TODO: Better catching.
-            try {
-                var destinationLocation = source.CreateLocation(ServiceType, FormatType);
-                if (!DeleteSource && skipRegistered) {
-                    if (destinationLocation != null && new KifaFile(destinationLocation).Registered) {
-                        logger.Info($"Skipped uploading of {source} to {destinationLocation} for now " +
-                                    "as it's supposed to be already uploaded...");
-                        return -1;
-                    }
-                }
-
-                source.Register();
-                if (DownloadLocal) {
-                    // We will download it anyway.
-                    source.CacheFileToLocal();
-                }
-
-                logger.Info("Checking source {0}...", source);
-                try {
-                    var sourceCheckResult = source.Add();
-
-                    if (sourceCheckResult != FileProperties.None) {
-                        logger.Error($"Source is wrong! The following fields differ: {sourceCheckResult}");
-                        return 1;
-                    }
-                } catch (FileNotFoundException ex) {
-                    source.Unregister();
-                    logger.Error(ex, "Source file not found.");
-                    return 1;
-                }
-
-                destinationLocation ??= source.CreateLocation(ServiceType, FormatType);
-                var destination = new KifaFile(destinationLocation);
-
-                if (destination.Exists()) {
-                    destination.Register();
-                    if (QuickMode) {
-                        Console.WriteLine($"Skipped verifying of {destination} as quick mode is enabled.");
-                        return 0;
-                    }
-
-                    var destinationCheckResult = destination.Add();
-
-                    if (destinationCheckResult == FileProperties.None) {
-                        logger.Info("Already uploaded!");
-
-                        if (DeleteSource) {
-                            if (source.IsCloud) {
-                                logger.Info("Source {0} is not removed as it's in cloud.", source);
-                                source.Register(true);
-                                return 0;
-                            }
-
-                            source.Delete();
-                            FileInformation.Client.RemoveLocation(source.Id, source.ToString());
-                            logger.Info("Source {0} removed since upload is successful.", source);
-                        }
-
-                        return 0;
-                    }
-
-                    logger.Warn("Destination exists, but doesn't match.");
-                    return 2;
-                }
-
-                destination.Unregister();
-                destination.Register();
-
-                logger.Info("Copying {0} to {1}...", source, destination);
-                source.Copy(destination);
-
-                if (destination.Exists()) {
-                    destination.Register();
-                    if (QuickMode) {
-                        Console.WriteLine($"Skipped verifying of {destination} as quick mode is enabled.");
-                        return 0;
-                    }
-
-                    logger.Info("Checking {0}...", destination);
-                    var destinationCheckResult = destination.Add();
-                    if (destinationCheckResult == FileProperties.None) {
-                        logger.Info("Successfully uploaded {0} to {1}!", source, destination);
-
-                        if (!DownloadLocal) {
-                            source.RemoveLocalCacheFile();
-                        }
-
-                        if (DeleteSource) {
-                            if (source.IsCloud) {
-                                logger.Info("Source {0} is not removed as it's in cloud.", source);
-                                source.Register(true);
-                                return 0;
-                            }
-
-                            source.Delete();
-                            FileInformation.Client.RemoveLocation(source.Id, source.ToString());
-                            logger.Info("Source {0} removed since upload is successful.", source);
-                        } else {
-                            source.Register(true);
-                        }
-
-                        return 0;
-                    }
-
-                    destination.Delete();
-                    logger.Fatal("Upload failed! The following fields differ (removed): {0}", destinationCheckResult);
-                    return 2;
-                }
-
-                logger.Fatal("Destination doesn't exist unexpectedly!");
-                return 2;
-            } catch (Exception ex) {
-                logger.Fatal(ex, "Unexpected error");
-                return 127;
-            }
+            var results = files.Select(f => (f.ToString(),
+                    new KifaFile(f.ToString()).Upload(ServiceType, FormatType, deleteSource: DeleteSource,
+                        useCache: UseCache, downloadLocal: DownloadLocal, skipVerify: QuickMode, skipRegistered: true)))
+                .ToList();
+            return results.Select(r => r.Item2).Concat(results.Where(r => r.Item2 == -1).Select(r =>
+                new KifaFile(r.Item1).Upload(ServiceType, FormatType, deleteSource: DeleteSource, useCache: UseCache,
+                    downloadLocal: DownloadLocal, skipVerify: QuickMode, skipRegistered: false))).Max();
         }
     }
 }
