c2fffb0afe4478260ee1125826523cd0a2b38d9f
Kimi@mac2 <kimi.ribery@gmail.com>
2021-02-16 10:01:06 +0800

    Order seems to matter.

--- src/Kifa.Memrise/MemriseGermanWord.cs
+++ src/Kifa.Memrise/MemriseGermanWord.cs
@@ -13,13 +13,14 @@ namespace Kifa.Memrise {
         public string Level { get; set; }
         public string Form { get; set; }
         public string Meaning { get; set; }
-        public List<string> Examples { get; set; }
+
+        // A synonym text like: (CH) = (D, A) Hausmeister
+        public string Synonym { get; set; }
 
         // Only Word, Level, Form are included.
         public MemriseGermanWord Feminine { get; set; }
 
-        // A synonym text like: (CH) = (D, A) Hausmeister
-        public string Synonym { get; set; }
+        public List<string> Examples { get; set; }
     }
 
     public interface MemriseGermanWordServiceClient : KifaServiceClient<MemriseGermanWord> {
