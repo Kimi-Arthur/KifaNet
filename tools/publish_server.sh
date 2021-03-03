de17aeb079e7ef5a2478e87d9889af9245a40eac
Kimi@mac2 <kimi.ribery@gmail.com>
2021-03-03 19:03:52 +0100

    Update server deploy scripts.

--- tools/publish_server.sh
+++ tools/publish_server.sh
@@ -1,4 +1,6 @@
-rm -rf publish
-dotnet publish -o publish/ServerCore -c Release src/Pimix.Web.Api/Pimix.Web.Api.csproj
-scp -r publish/ServerCore kimi@www.kifa.ga:~/
-ssh -t kimi@www.kifa.ga "sudo systemctl restart server-core.service"
+rm -rf web_api
+dotnet publish -c Release -o web_api -r linux-x64 -p:PublishSingleFile=true --self-contained true src/Pimix.Web.Api/Pimix.Web.Api.csproj
+
+ssh -t kimi@www.kifa.ga "sudo systemctl stop web_api.service"
+scp -r web_api/Pimix.Web.Api kimi@www.kifa.ga:~/
+ssh -t kimi@www.kifa.ga "sudo systemctl restart web_api.service"
