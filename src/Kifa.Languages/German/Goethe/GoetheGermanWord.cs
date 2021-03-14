c4ac0cab4fc9d252a033497165e435c66699795e
Kimi@Dell <kimi.ribery@gmail.com>
2021-03-14 16:42:08 +0100

    Root word fix.

--- src/Kifa.Languages/German/Goethe/GoetheGermanWord.cs
+++ src/Kifa.Languages/German/Goethe/GoetheGermanWord.cs
@@ -7,7 +7,7 @@ namespace Kifa.Languages.German.Goethe {
     public class GoetheGermanWord : DataModel {
         public const string ModelId = "goethe/words";
 
-        static readonly Regex RootWordPattern = new(@"^(das |der |die |\(.*\) |sich)?(.+?)(-$| \(.*\)| sein)?$");
+        static readonly Regex RootWordPattern = new(@"^(das |der |die |\(.*\) |sich )?(.+?)(-$| \(.*\)| sein)?$");
 
         public string Word {
             get => Id;
