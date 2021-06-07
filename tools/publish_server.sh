0d7189a9c8866ce42c3d6e77dcfd572759344f58
Kimi@mac2 <kimi.ribery@gmail.com>
2021-06-07 21:09:49 +0200

    Update server deployment script.

--- tools/publish_server.sh
+++ tools/publish_server.sh
@@ -1,6 +1,6 @@
 rm -rf web_api
 dotnet publish -c Release -o web_api -r linux-x64 -p:PublishSingleFile=true --self-contained true src/Kifa.Web.Api/Kifa.Web.Api.csproj
 
-ssh -t kimi@www.kifa.ga "sudo systemctl stop web_api.service"
-scp -r web_api/Kifa.Web.Api kimi@www.kifa.ga:~/
-ssh -t kimi@www.kifa.ga "sudo systemctl restart web_api.service"
+ssh -p 2222 -t kimi@www.kifa.ga "sudo systemctl stop web_api.service"
+scp -P 2222 -r web_api/Kifa.Web.Api kimi@www.kifa.ga:/var/www/
+ssh -p 2222 -t kimi@www.kifa.ga "sudo systemctl restart web_api.service"
