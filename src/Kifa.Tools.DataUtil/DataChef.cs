b5a22c67ca985d2561bbb84f9dd8d3fd7b85c629
Kimi@mac2 <kimi.ribery@gmail.com>
2021-06-29 15:09:32 +0200

    More efficient client.

--- src/Kifa.Tools.DataUtil/DataChef.cs
+++ src/Kifa.Tools.DataUtil/DataChef.cs
@@ -10,21 +10,24 @@ namespace Kifa.Tools.DataUtil {
         where TClient : KifaServiceClient<TDataModel>, new() {
         static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
+        static KifaServiceClient<TDataModel> client;
+        
+        static KifaServiceClient<TDataModel> Client => client ??= new TClient();
+
         public KifaActionResult Import(KifaFile dataFile) {
             using var reader = new StreamReader(dataFile.OpenRead());
             var items = new Deserializer().Deserialize<List<TDataModel>>(reader.ReadToEnd());
-
-            var client = new TClient();
+            
             var results = new KifaBatchActionResult();
             foreach (var item in items) {
-                results.Add(logger.LogResult(client.Update(item), $"Update ({client.ModelId}/{item.Id})"));
+                results.Add(logger.LogResult(Client.Update(item), $"Update ({Client.ModelId}/{item.Id})"));
             }
 
             return results;
         }
 
         public KifaActionResult Refresh(string id) {
-            return new TClient().Refresh(id);
+            return Client.Refresh(id);
         }
     }
 }
