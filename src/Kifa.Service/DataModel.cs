9ba903e26c461fdc17225648e5799e4ae6299bb2
Kimi@mac2 <kimi.ribery@gmail.com>
2021-06-29 16:29:17 +0200

    Some unfinished code.

--- src/Kifa.Service/DataModel.cs
+++ src/Kifa.Service/DataModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NLog;
 
 namespace Kifa.Service {
@@ -13,10 +14,48 @@ namespace Kifa.Service {
     public abstract class DataModel {
         public string Id { get; set; }
 
-        [JsonProperty("$metadata")]
-        public DataMetadata Metadata { get; set; }
+        [JsonProperty("$metadata")] public DataMetadata Metadata { get; set; }
 
         public virtual bool? Fill() => null;
+
+        // Not finished
+        public string Compare<TDataModel>(TDataModel other) {
+            if (!(this is TDataModel model)) {
+                return "<Different type>";
+            }
+
+            var myJson = JToken.Parse(ToString());
+            var otherJson = JToken.Parse(ToString());
+            var diffToken = CompareJToken(myJson, otherJson);
+            return diffToken.ToString();
+        }
+
+        JToken CompareJToken(JToken myJson, JToken otherJson) {
+            var result = new JArray();
+            if (myJson.Type != otherJson.Type) {
+                var myToken = new JObject();
+                myToken["-"] = myJson;
+                result.Add(myToken);
+                var otherToken = new JObject();
+                otherToken["+"] = otherJson;
+                result.Add(otherToken);
+                return result;
+            }
+
+            if (myJson.Type == JTokenType.Array) {
+                foreach (var childPair in myJson.Children().Zip(otherJson.Children())) {
+                    if (childPair.First != childPair.Second) {
+                        if (childPair.First != null) {
+                            var myToken = new JObject();
+                            myToken["-"] = myJson;
+                            result.Add(myToken);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
 
         public override string ToString() => JsonConvert.SerializeObject(this, Defaults.PrettyJsonSerializerSettings);
     }
