6dd0dc7dfb7f31bdb9f3b655bb6f42ed2bed4768
Kimi@mac2 <kimi.ribery@gmail.com>
2021-07-04 22:14:24 +0200

    Keep existing order when exporting.

--- src/Kifa.Tools.DataUtil/DataChef.cs
+++ src/Kifa.Tools.DataUtil/DataChef.cs
@@ -52,13 +52,17 @@ namespace Kifa.Tools.DataUtil {
         public KifaActionResult<string> Export(string data, bool getAll) {
             var items = new Deserializer().Deserialize<List<TDataModel>>(data).Select(item => item.Id).ToList();
 
-            var updatedItems = getAll ? Client.List().Values.ToList() : Client.Get(items);
+            var updatedItems = getAll ? GetItemsWithExistingOrder(items, Client.List()) : Client.Get(items);
 
             var serializer = new SerializerBuilder().WithIndentedSequences()
                 .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull).Build();
             return new KifaActionResult<string>(
                 $"# {ModelId}\n{string.Join("\n", updatedItems.Select(item => serializer.Serialize(new List<TDataModel> {item})))}");
         }
+
+        static List<TDataModel> GetItemsWithExistingOrder(IEnumerable<string> items,
+            SortedDictionary<string, TDataModel> list) =>
+            items.Select(list.Pop).Concat(list.Values).ToList();
 
         public KifaActionResult Refresh(string id) {
             return Client.Refresh(id);
