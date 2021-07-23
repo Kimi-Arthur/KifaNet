6812b63a8a384f07ee0c1d86aaf502f97e50dd35
Kimi@mac2 <kimi.ribery@gmail.com>
2021-07-23 02:23:58 +0200

    Ignore parsing error.

--- src/Kifa/Json/Defaults.cs
+++ src/Kifa/Json/Defaults.cs
@@ -7,43 +7,42 @@ using Newtonsoft.Json.Serialization;
 
 namespace Kifa {
     public static class Defaults {
-        public static readonly JsonSerializerSettings JsonSerializerSettings =
-            new JsonSerializerSettings {
-                DateFormatString = "yyyy-MM-dd HH:mm:ss.ffffffzzz",
-                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
-                ContractResolver = new OrderedContractResolver {
-                    NamingStrategy = new SnakeCaseNamingStrategy()
-                },
-                Converters = new List<JsonConverter> {
-                    new StringEnumConverter(new SnakeCaseNamingStrategy()),
-                    new GenericJsonConverter()
-                },
-                DefaultValueHandling = DefaultValueHandling.Ignore,
-                NullValueHandling = NullValueHandling.Ignore,
-                MetadataPropertyHandling = MetadataPropertyHandling.Ignore
-            };
+        public static readonly JsonSerializerSettings JsonSerializerSettings = new() {
+            DateFormatString = "yyyy-MM-dd HH:mm:ss.ffffffzzz",
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            ContractResolver = new OrderedContractResolver {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            },
+            Converters = new List<JsonConverter> {
+                new StringEnumConverter(new SnakeCaseNamingStrategy()),
+                new GenericJsonConverter()
+            },
+            Error = (sender, ev) => ev.ErrorContext.Handled = true,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            NullValueHandling = NullValueHandling.Ignore,
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore
+        };
 
-        public static readonly JsonSerializerSettings PrettyJsonSerializerSettings =
-            new JsonSerializerSettings {
-                DateFormatString = "yyyy-MM-dd HH:mm:ss.ffffffzzz",
-                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
-                Formatting = Formatting.Indented,
-                ContractResolver = new OrderedContractResolver {
-                    NamingStrategy = new SnakeCaseNamingStrategy()
-                },
-                Converters = new List<JsonConverter> {
-                    new StringEnumConverter(new SnakeCaseNamingStrategy()),
-                    new GenericJsonConverter()
-                },
-                DefaultValueHandling = DefaultValueHandling.Ignore,
-                NullValueHandling = NullValueHandling.Ignore,
-                MetadataPropertyHandling = MetadataPropertyHandling.Ignore
-            };
+        public static readonly JsonSerializerSettings PrettyJsonSerializerSettings = new() {
+            DateFormatString = "yyyy-MM-dd HH:mm:ss.ffffffzzz",
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            Formatting = Formatting.Indented,
+            ContractResolver = new OrderedContractResolver {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            },
+            Converters = new List<JsonConverter> {
+                new StringEnumConverter(new SnakeCaseNamingStrategy()),
+                new GenericJsonConverter()
+            },
+            Error = (sender, ev) => ev.ErrorContext.Handled = true,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            NullValueHandling = NullValueHandling.Ignore,
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore
+        };
     }
 
     public class OrderedContractResolver : DefaultContractResolver {
-        protected override IList<JsonProperty> CreateProperties(Type type,
-            MemberSerialization memberSerialization) {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization) {
             return base.CreateProperties(type, memberSerialization).OrderBy(p => p.PropertyName).ToList();
         }
     }
