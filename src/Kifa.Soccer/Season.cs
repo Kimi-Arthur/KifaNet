81da0c60653a0133c22081826cd66ad811f3d356
Kimi@MBP <kimi.ribery@gmail.com>
2021-09-26 22:32:06 +0200

    Refactor Soccer data.

---
+++ src/Kifa.Soccer/Season.cs
@@ -0,0 +1,12 @@
+namespace Kifa.Soccer {
+    public class Season {
+        public int Year { get; set; }
+        public bool SingleYear { get; set; } = false;
+
+        public static Season Regular(int year) => new Season {Year = year};
+
+        public static Season SingleYearSeason(int year) => new Season {Year = year, SingleYear = true};
+
+        public override string ToString() => SingleYear ? $"Season {Year}" : $"Season {Year}-{(Year + 1) % 100}";
+    }
+}
---
+++ src/Kifa.Soccer/Team.cs
@@ -0,0 +1,12 @@
+using Kifa.Service;
+
+namespace Kifa.Soccer {
+    public class Team : DataModel<Team> {
+        // Full names used in English wiki, like "FC Bayern Munich" or "Borussia Mönchengladbach"
+        public string Name { get; set; }
+        
+        // Short id, normally 3 capitalized characters, found in Twitter tag or scoreboard, like "BAY" for bayern and
+        // "BMG" for M'gladbach.
+        public string Short { get; set; }
+    }
+}
