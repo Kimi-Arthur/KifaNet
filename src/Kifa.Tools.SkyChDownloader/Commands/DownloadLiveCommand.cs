edf2725eeac621080e940172898f24978d83dba4
Kimi@MBP <kimi.ribery@gmail.com>
2021-11-02 14:33:13 +0100

    DownloadLiveCommand that shows the actual video link.

---
+++ src/Kifa.Tools.SkyChDownloader/Commands/DownloadLiveCommand.cs
@@ -0,0 +1,23 @@
+using CommandLine;
+using Kifa.SkyCh;
+using NLog;
+
+namespace Kifa.Tools.SkyChDownloader.Commands {
+    [Verb("live", HelpText = "Download program with id from Live TV page.")]
+    public class DownloadLiveCommand : KifaCommand {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        [Value(0, Required = true, HelpText = "Live TV ID.")]
+        public string LiveId { get; set; }
+
+        public override int Execute() {
+            var skyProgram = new SkyProgram {
+                Id = LiveId
+            };
+            skyProgram.Fill();
+
+            logger.Info(skyProgram.GetVideoLink());
+            return 0;
+        }
+    }
+}
