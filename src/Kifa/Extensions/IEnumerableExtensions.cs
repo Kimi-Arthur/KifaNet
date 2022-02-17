7c0b044d1c851f73bfb8e30cbec4ce5c9f6b76a7
Kimi@MBP <kimi.ribery@gmail.com>
2022-02-17 18:49:57 +0100

    Add IEnumerable.ForEach.

--- src/Kifa/Extensions/IEnumerableExtensions.cs
+++ src/Kifa/Extensions/IEnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,5 +6,11 @@ namespace Kifa {
     public static class IEnumerableExtensions {
         public static IEnumerable<T> ExceptNull<T>(this IEnumerable<T?> enumerable) where T : class =>
             enumerable.Where(e => e != null).Select(e => e!);
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+            foreach (var item in source) {
+                action(item);
+            }
+        }
     }
 }
