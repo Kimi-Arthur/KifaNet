879340780e8428606fca6659836e5eec242a7775
Kimi@ZRH <kimi.ribery@gmail.com>
2022-02-20 12:35:30 +0100

    format

--- src/Kifa.Tools.FileUtil/Commands/ImportCommand.cs
+++ src/Kifa.Tools.FileUtil/Commands/ImportCommand.cs
@@ -40,27 +40,32 @@ namespace Kifa.Tools.FileUtil.Commands {
                 case "tv_shows":
                     var tvShow = TvShow.Client.Get(id);
                     series = tvShow;
-                    episodes = tvShow.Seasons.Where(season => seasonId <= 0 || season.Id == seasonId)
-                        .SelectMany(season => season.Episodes, (season, episode) => (season, episode))
-                        .Where(item => episodeId <= 0 || episodeId == item.episode.Id).ToList();
+                    episodes = tvShow.Seasons
+                        .Where(season => seasonId <= 0 || season.Id == seasonId)
+                        .SelectMany(season => season.Episodes,
+                            (season, episode) => (season, episode)).Where(item =>
+                            episodeId <= 0 || episodeId == item.episode.Id).ToList();
                     break;
                 case "animes":
                     var anime = Anime.Client.Get(id);
                     series = anime;
                     episodes = anime.Seasons.Where(season => seasonId <= 0 || season.Id == seasonId)
-                        .SelectMany(season => season.Episodes, (season, episode) => ((Season) season, episode))
-                        .Where(item => episodeId <= 0 || episodeId == item.episode.Id).ToList();
+                        .SelectMany(season => season.Episodes,
+                            (season, episode) => ((Season) season, episode)).Where(item =>
+                            episodeId <= 0 || episodeId == item.episode.Id).ToList();
                     break;
                 case "soccer":
                     foreach (var file in FileNames.SelectMany(path =>
-                        FileInformation.Client.ListFolder(ById ? path : new KifaFile(path).Id, Recursive)
-                            .DefaultIfEmpty(ById ? path : new KifaFile(path).Id))) {
+                                 FileInformation.Client
+                                     .ListFolder(ById ? path : new KifaFile(path).Id, Recursive)
+                                     .DefaultIfEmpty(ById ? path : new KifaFile(path).Id))) {
                         var ext = file.Substring(file.LastIndexOf(".") + 1);
                         var targetFileName = $"{SoccerShow.FromFileName(file)}.{ext}";
                         targetFileName = Confirm($"Confirm importing {file} as ", targetFileName);
                         FileInformation.Client.Link(file, targetFileName);
                         logger.Info($"Successfully linked {file} to {targetFileName}");
                     }
+
                     return 0;
                 default:
                     Console.WriteLine($"Cannot find resource type {type}");
@@ -68,10 +73,12 @@ namespace Kifa.Tools.FileUtil.Commands {
             }
 
             foreach (var file in FileNames.SelectMany(path =>
-                FileInformation.Client.ListFolder(ById ? path : new KifaFile(path).Id, Recursive))) {
-                var suffix = file.Substring(file.LastIndexOf('.'));
+                         FileInformation.Client.ListFolder(ById ? path : new KifaFile(path).Id,
+                             Recursive))) {
+                var suffix = file[file.LastIndexOf('.')..];
                 var ((season, episode), index) = SelectOne(episodes,
-                    e => $"{file} => {series.Format(e.season, e.episode)}{suffix}", "mapping", (null, null));
+                    e => $"{file} => {series.Format(e.season, e.episode)}{suffix}", "mapping",
+                    (null, null));
                 if (index >= 0) {
                     FileInformation.Client.Link(file, series.Format(season, episode) + suffix);
                     episodes.RemoveAt(index);
