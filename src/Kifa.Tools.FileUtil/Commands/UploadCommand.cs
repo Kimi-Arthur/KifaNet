fb36cddd05cbb7f2471ee3be45ebf098b40a8dfd
Kimi@MBP <kimi.ribery@gmail.com>
2022-04-29 15:05:37 -0700

    Allow multiple upload targets to be specified.
    Upload and remove is broken.

--- src/Kifa.Tools.FileUtil/Commands/UploadCommand.cs
+++ src/Kifa.Tools.FileUtil/Commands/UploadCommand.cs
@@ -1,17 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using CommandLine;
-using NLog;
 using Kifa.Api.Files;
-using Kifa.IO;
+using NLog;
 
 namespace Kifa.Tools.FileUtil.Commands;
 
 [Verb("upload", HelpText = "Upload file to a cloud location.")]
 class UploadCommand : KifaCommand {
     static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    public static List<string> DefaultTargets { get; set; }
 
     [Value(0, Required = true, HelpText = "Target file(s) to upload.")]
     public IEnumerable<string> FileNames { get; set; }
@@ -23,14 +23,10 @@ class UploadCommand : KifaCommand {
     [Option('q', "quick", HelpText = "Finish quickly by not verifying validity of destination.")]
     public bool QuickMode { get; set; } = false;
 
-    [Option('s', "service",
+    [Option('t', "targets",
         HelpText =
-            "Type of service to upload to. Default is google. Allowed values: [google, baidu, mega, swiss]")]
-    public CloudServiceType ServiceType { get; set; } = CloudServiceType.Google;
-
-    [Option('f', "format",
-        HelpText = "Format used to upload file. Default is v1. Allowed values: [v1, v2]")]
-    public CloudFormatType FormatType { get; set; } = CloudFormatType.V1;
+            "Targets to upload to, in the format of 'google.v1', 'swiss.v2' or combined 'google.v1,swiss.v2' etc.")]
+    public string Targets { get; set; } = "";
 
     [Option('c', "use-cache", HelpText = "Use cache to help upload.")]
     public bool UseCache { get; set; } = false;
@@ -52,11 +48,22 @@ class UploadCommand : KifaCommand {
             Console.ReadLine();
         }
 
-        var results = files.Select(f => (f.ToString(),
-            new KifaFile(f.ToString()).Upload(ServiceType, FormatType, DeleteSource, UseCache,
-                DownloadLocal, QuickMode, true))).ToList();
-        return results.Select(r => r.Item2).Concat(results.Where(r => r.Item2 == -1).Select(r
-            => new KifaFile(r.Item1).Upload(ServiceType, FormatType, DeleteSource, UseCache,
-                DownloadLocal, QuickMode, false))).Max();
+        var targetsFromFlag = Targets.Split(",").ToList();
+
+        var targets = (targetsFromFlag.Count == 0 ? DefaultTargets : targetsFromFlag)
+            .Select(ParseDestination).ToList();
+
+        var results = files.SelectMany(f => targets.Select(d => (f.ToString(), d,
+            new KifaFile(f.ToString()).Upload(d.serviceType, d.formatType, DeleteSource, UseCache,
+                DownloadLocal, QuickMode, true)))).ToList();
+        return results.Select(r => r.Item3).Concat(results.Where(r => r.Item3 != 0).Select(r
+            => new KifaFile(r.Item1).Upload(r.Item2.serviceType, r.Item2.formatType, DeleteSource,
+                UseCache, DownloadLocal, QuickMode, false))).Max();
+    }
+
+    (CloudServiceType serviceType, CloudFormatType formatType) ParseDestination(string s) {
+        var segments = s.Split(".");
+        return (Enum.Parse<CloudServiceType>(segments[0], true),
+            Enum.Parse<CloudFormatType>(segments[1], true));
     }
 }
