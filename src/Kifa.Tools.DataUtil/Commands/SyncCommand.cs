a6ca2477003cb01afc2ac07ea6fa5258aea9aa9a
Kimi@M1 <kimi.ribery@gmail.com>
2022-05-08 23:46:39 +0200

    Add `datax sync` to combine `datax import` and `datax export`.

---
+++ src/Kifa.Tools.DataUtil/Commands/SyncCommand.cs
@@ -0,0 +1,55 @@
+using CommandLine;
+using Kifa.Api.Files;
+using Kifa.Service;
+using NLog;
+
+namespace Kifa.Tools.DataUtil.Commands;
+
+[Verb("sync", HelpText = "Sync data in a specific file.")]
+public class SyncCommand : KifaCommand {
+    static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    [Option('t', "type", HelpText = "Type of data. Allowed values: goethe/words, goethe/lists")]
+    public string Type { get; set; }
+
+    [Option('a', "get-all",
+        HelpText = "Whether to get all items that don't even appear in the file.")]
+    public bool GetAll { get; set; }
+
+    [Option('c', "compact", HelpText = "Whether to put leaf list into one line.")]
+    public bool Compact { get; set; }
+
+    [Value(0, Required = true, HelpText = "File to export data from.")]
+    public string File { get; set; }
+
+    public override int Execute() {
+        var file = new KifaFile(File);
+        var content = file.ReadAsString();
+
+        var chef = DataChef.GetChef(Type, content);
+
+        if (chef == null) {
+            logger.Error($"Unknown type name: {Type}.\n{content}");
+            return 1;
+        }
+
+        var importResult = logger.LogResult(chef.Import(content), "Summary");
+        if (importResult.Status != KifaActionStatus.OK) {
+            logger.Error($"Failed to import data for {chef.ModelId}.");
+            return (int) importResult.Status;
+        }
+        
+        var result = logger.LogResult(chef.Export(content, GetAll, Compact), "Summary");
+        if (result.Status != KifaActionStatus.OK) {
+            logger.Error($"Failed to export data for {chef.ModelId}.");
+            return (int) result.Status;
+        }
+
+        if (result.Response != null) {
+            file.Delete();
+            file.Write(result.Response);
+        }
+
+        return 0;
+    }
+}
--- src/Kifa.Tools.DataUtil/Program.cs
+++ src/Kifa.Tools.DataUtil/Program.cs
@@ -7,5 +7,5 @@ class Program {
     static int Main(string[] args)
         => KifaCommand.Run(
             parameters => Parser.Default.ParseArguments(parameters, typeof(ImportCommand),
-                typeof(ExportCommand), typeof(LinkCommand), typeof(AddCommand)), args);
+                typeof(ExportCommand), typeof(LinkCommand), typeof(AddCommand), typeof(SyncCommand)), args);
 }
