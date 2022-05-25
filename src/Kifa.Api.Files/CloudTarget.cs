b0a1c0a2e26ed4fc7f01c6fedd7461f3f6d2b830
Kimi@MBP <kimi.ribery@gmail.com>
2022-05-25 14:11:20 +0200

    Add CloudTarget parsing logic.

--- src/Kifa.Api.Files/CloudTarget.cs
+++ src/Kifa.Api.Files/CloudTarget.cs
@@ -1,6 +1,19 @@
+using System;
+
 namespace Kifa.Api.Files;
 
 public class CloudTarget {
     public CloudServiceType ServiceType { get; set; }
     public CloudFormatType FormatType { get; set; }
+
+    public static CloudTarget Parse(string targetSpec) {
+        var segments = targetSpec.Split(".");
+        return new CloudTarget {
+            ServiceType = Enum.Parse<CloudServiceType>(segments[0], true),
+            FormatType = Enum.Parse<CloudFormatType>(segments[1], true)
+        };
+    }
+
+    public override string ToString()
+        => $"{ServiceType.ToString().ToLowerInvariant()}.{FormatType.ToString().ToLowerInvariant()}";
 }
