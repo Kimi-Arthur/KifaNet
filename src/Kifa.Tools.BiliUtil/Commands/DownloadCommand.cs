7a7e40c0b4dce1f4dd6b78f81761ac2cc80fa16f
Kimi@M1 <kimi.ribery@gmail.com>
2022-06-14 19:50:36 +0200

    Add OutputAudio option.

--- src/Kifa.Tools.BiliUtil/Commands/DownloadCommand.cs
+++ src/Kifa.Tools.BiliUtil/Commands/DownloadCommand.cs
@@ -8,9 +8,6 @@ public abstract class DownloadCommand : KifaCommand {
     [Option('d', "prefix-date", HelpText = "Prefix file name with the upload date.")]
     public bool PrefixDate { get; set; } = false;
 
-    [Option('a', "all-staff", HelpText = "Put video in all folders owned by any staff.")]
-    public bool AllStaff { get; set; } = false;
-
     [Option('s', "source", HelpText = "Override default source choice.")]
     public int SourceChoice { get; set; } = BilibiliVideo.DefaultBiliplusSourceChoice;
 
@@ -18,6 +15,9 @@ public abstract class DownloadCommand : KifaCommand {
         HelpText = "Folder to output video files to. Defaults to current folder.")]
     public string? OutputFolder { get; set; }
 
+    [Option('a', "output-audio", HelpText = "Also generate audio file in destination.")]
+    public bool OutputAudio { get; set; } = false;
+    
     public DownloadOptions DownloadOptions
         => new() {
             PrefixDate = PrefixDate,
