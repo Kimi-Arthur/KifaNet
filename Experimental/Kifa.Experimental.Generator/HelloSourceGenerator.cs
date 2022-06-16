d4a368df3995074c27a4e1a8467b41c1f3366208
Kimi@M1 <kimi.ribery@gmail.com>
2022-06-16 23:00:15 +0200

    Incomplete HelloSourceGenerator.

---
+++ Experimental/Kifa.Experimental.Generator/HelloSourceGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Kifa.Experimental.Generator
+{
+    [Generator]
+    public class HelloSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Code generation goes here
+        }
+        public void Initialize(GeneratorInitializationContext
+            context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
