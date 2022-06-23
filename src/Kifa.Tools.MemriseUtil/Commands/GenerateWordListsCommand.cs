143aa7883f4570f3bb38d81e75b0076be529460a
Kimi@M1 <kimi.ribery@gmail.com>
2022-06-22 18:09:14 -0700

    Remove reference code.

--- src/Kifa.Tools.MemriseUtil/Commands/GenerateWordListsCommand.cs
+++ src/Kifa.Tools.MemriseUtil/Commands/GenerateWordListsCommand.cs
@@ -9,25 +9,6 @@ namespace Kifa.Tools.MemriseUtil.Commands;
 
 [Verb("levels", HelpText = "Generate word lists.")]
 public class GenerateWordListsCommand : KifaCommand {
-    /*
-with codecs.open('Data/goethe_word_list.yaml', 'r', 'utf-8') as f:
-    words = yaml.load(f)
-
-lists = {}
-
-for word in words:
-    if word['Examples'][0].startswith('example'):
-        continue
-    lists[word['Level']] = lists.get(word['Level'], [])
-    lists[word['Level']] += [word['Id']]
-
-output = [{'Id':k,'Words':v} for k,v in lists.items()]
-
-with codecs.open('Data/goethe_lists.yaml', 'w', 'utf-8') as f:
-    yaml.dump(output, f, allow_unicode=True, default_flow_style=False)
-
-     */
-
     #region public late string WordsFile { get; set; }
 
     string? wordsFile;
