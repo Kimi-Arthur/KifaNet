bc0394da5950c7608d972776c46c087eb54d797b
Kimi@M1 <kimi.ribery@gmail.com>
2022-06-27 02:49:07 -0700

    Won't turn server before needed to. Shortened downtime.

--- tools/publish_server.sh
+++ tools/publish_server.sh
@@ -1,6 +1,10 @@
 rm -rf web_api
 dotnet publish -c Release -o web_api -r linux-x64 -p:PublishSingleFile=true --self-contained true src/Kifa.Web.Api/Kifa.Web.Api.csproj
 
+version=$(date -u +%Y%m%d%H%M%S)
+
+ssh -p 2222 -t kimi@www.kimily.ga "mkdir /var/www/$version/"
+scp -P 2222 -r web_api/Kifa.Web.Api* kimi@www.kimily.ga:/var/www/$version/
 ssh -p 2222 -t kimi@www.kimily.ga "sudo systemctl stop web_api.service"
-scp -P 2222 -r web_api/Kifa.Web.Api* kimi@www.kimily.ga:/var/www/
+ssh -p 2222 -t kimi@www.kimily.ga "cp /var/www/$version/* /var/www/"
 ssh -p 2222 -t kimi@www.kimily.ga "sudo systemctl restart web_api.service"
