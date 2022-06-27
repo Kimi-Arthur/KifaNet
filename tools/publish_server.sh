bfb8136c7efa09fef30a15628b0d6d82787113a8
Kimi@M1 <kimi.ribery@gmail.com>
2022-06-27 07:06:41 -0700

    Reduce login times for deploying server.

--- tools/publish_server.sh
+++ tools/publish_server.sh
@@ -5,6 +5,4 @@ version=$(date -u +%Y%m%d%H%M%S)
 
 ssh -p 2222 -t kimi@www.kimily.ga "mkdir /var/www/$version/"
 scp -P 2222 -r web_api/Kifa.Web.Api* kimi@www.kimily.ga:/var/www/$version/
-ssh -p 2222 -t kimi@www.kimily.ga "sudo systemctl stop web_api.service"
-ssh -p 2222 -t kimi@www.kimily.ga "cp /var/www/$version/* /var/www/"
-ssh -p 2222 -t kimi@www.kimily.ga "sudo systemctl restart web_api.service"
+ssh -p 2222 -t kimi@www.kimily.ga "sudo systemctl stop web_api.service; cp /var/www/$version/* /var/www/; sudo systemctl restart web_api.service"
