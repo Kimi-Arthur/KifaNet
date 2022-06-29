4edd6150aff8362d43422f021c381013859f31e5
Kimi@M1 <kimi.ribery@gmail.com>
2022-06-29 07:08:44 -0700

    Test Link<TDdataModel>.Clone().

--- tests/Kifa.Service.Tests/LinkTests.cs
+++ tests/Kifa.Service.Tests/LinkTests.cs
@@ -5,26 +5,39 @@ using Xunit;
 namespace Kifa.Service.Tests;
 
 public class LinkTests {
+    string serialized =
+        "{\"a_dict\":{\"very\":\"good\"},\"a_item\":\"15\",\"a_list\":[\"10\",\"12\"],\"id\":\"id\"}";
+
+    DataB data = new DataB {
+        Id = "id",
+        AList = new List<Link<DataA>> {
+            new(new DataA {
+                Id = "10",
+                MyValue = "b"
+            }),
+            new("12")
+        },
+        AItem = "15",
+        ADict = new Dictionary<string, Link<DataA>> {
+            { "very", new Link<DataA>("good") }
+        }
+    };
+
     [Fact]
     public void LinkSerializationTest() {
-        var b = new DataB {
-            Id = "id",
-            AList = new List<Link<DataA>> {
-                new(new DataA {
-                    Id = "10",
-                    MyValue = "b"
-                }),
-                new("12")
-            },
-            AItem = "15"
-        };
+        Assert.Equal(serialized,
+            JsonConvert.SerializeObject(data, Defaults.JsonSerializerSettings));
+    }
 
-        b.ADict = new Dictionary<string, Link<DataA>>();
-        b.ADict["very"] = new Link<DataA>("good");
+    [Fact]
+    public void LinkDeserializationTest() {
+        Assert.Equal(data,
+            JsonConvert.DeserializeObject<DataB>(serialized, Defaults.JsonSerializerSettings));
+    }
 
-        Assert.Equal(
-            "{\"a_dict\":{\"very\":\"good\"},\"a_item\":\"15\",\"a_list\":[\"10\",\"12\"],\"id\":\"id\"}",
-            JsonConvert.SerializeObject(b, Defaults.JsonSerializerSettings));
+    [Fact]
+    public void LinkCloneTest() {
+        Assert.Equal(data, data.Clone());
     }
 }
 
