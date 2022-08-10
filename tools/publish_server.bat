686a1f5e0fc5d188fa3c1be967c8f32115fd4272
Kimi@M1 <kimi.ribery@gmail.com>
2022-08-10 14:02:35 +0200

    Update server.

--- tools/publish_server.bat
+++
@@ -1,6 +0,0 @@
-rmdir /s /q web_api
-dotnet publish -c Release -o web_api -r linux-x64 -p:PublishSingleFile=true --self-contained true src/Kifa.Web.Api/Kifa.Web.Api.csproj
-
-ssh -p 2222 -t kimi@www.kifa.ga "sudo systemctl stop web_api.service"
-scp -P 2222 web_api/Kifa.Web.Api "kimi@www.kifa.ga:/var/www/"
-ssh -p 2222 -t kimi@www.kifa.ga "sudo systemctl restart web_api.service"
--- tools/publish_server.sh
+++ tools/publish_server.sh
@@ -2,7 +2,8 @@ rm -rf web_api
 dotnet publish -c Release -o web_api -r linux-x64 -p:PublishSingleFile=true --self-contained true src/Kifa.Web.Api/Kifa.Web.Api.csproj
 
 version=$(date -u +%Y%m%d%H%M%S)
+server="kimi@kimily.ch"
 
-ssh -p 2222 -t kimi@www.kimily.ga "mkdir /var/www/$version/"
-scp -P 2222 -r web_api/Kifa.Web.Api* kimi@www.kimily.ga:/var/www/$version/
-ssh -p 2222 -t kimi@www.kimily.ga "sudo systemctl stop web_api.service; cp /var/www/$version/* /var/www/; sudo systemctl restart web_api.service"
+ssh -p 2222 -t $server "mkdir /var/www/$version/"
+scp -P 2222 -r web_api/Kifa.Web.Api* $server:/var/www/$version/
+ssh -p 2222 -t $server "sudo systemctl stop web_api.service; cp /var/www/$version/* /var/www/; sudo systemctl restart web_api.service"
