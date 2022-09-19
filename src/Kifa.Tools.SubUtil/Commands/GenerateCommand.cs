1359654d37f450e196970b973b64286aa0ba4e32
Kimi@M1 <kimi.ribery@gmail.com>
2022-09-19 22:08:48 +0200

    Revert last change.

--- src/Kifa.Tools.SubUtil/Commands/GenerateCommand.cs
+++ src/Kifa.Tools.SubUtil/Commands/GenerateCommand.cs
@@ -158,10 +158,9 @@ class GenerateCommand : KifaFileCommand {
     static void PositionNormalComments(List<AssDialogue> comments) {
         var screenWidth = 1920;
 
-        // var sizes = comments.Select(x
-        //     => x.Text.TextElements.Where(e => e is AssDialogueRawTextElement)
-        //         .Sum(e => ((AssDialogueRawTextElement) e).Content.Length) * 50F).ToList();
-        var sizes = comments.Select(x => 500F).ToList();
+        var sizes = comments.Select(x
+            => x.Text.TextElements.Where(e => e is AssDialogueRawTextElement)
+                .Sum(e => ((AssDialogueRawTextElement) e).Content.Length) * 50F).ToList();
 
         var speeds = sizes.Zip(comments,
             (s, c) => (screenWidth + s) / (c.End - c.Start).TotalSeconds).ToList();
