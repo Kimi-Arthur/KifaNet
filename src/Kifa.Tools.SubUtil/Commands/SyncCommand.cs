15cbd35e6cc137942ed9bc558da19455faad5ef9
Kimi@M1 <kimi.ribery@gmail.com>
2022-09-25 12:02:29 +0200

    Use content as a second hand info.

--- src/Kifa.Tools.SubUtil/Commands/SyncCommand.cs
+++ src/Kifa.Tools.SubUtil/Commands/SyncCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using CommandLine;
 using Kifa.Api.Files;
 using Kifa.Subtitle.Ass;
@@ -19,6 +20,11 @@ public class SyncCommand : KifaCommand {
     [Option('s', "source", Required = true,
         HelpText = "Reference source subtitle to sync time from.")]
     public string Source { get; set; }
+
+    [Option('c', "content", Required = false,
+        HelpText =
+            "Whether two subtitle files should have the same content. Supported values: 'full', 'partial' (default), 'no'.")]
+    public string ContentType { get; set; } = "partial";
 
     public override int Execute() {
         var file = new KifaFile(FileName, simpleMode: true);
@@ -48,7 +54,8 @@ public class SyncCommand : KifaCommand {
             while (hasValue && !IsMatch(line, referenceEnumerator.Current) &&
                    referenceEnumerator.Current.Start < line.End) {
                 if (matchedLines.Count > 0 && matchedLines[^1].sourceLines.Count > 0 &&
-                    matchedLines[^1].sourceLines[^1] != referenceEnumerator.Current) {
+                    matchedLines[^1].sourceLines[^1] != referenceEnumerator.Current &&
+                    matchedLines[^1].targetLines.Count == 1) {
                     if (IsMatch(matchedLines[^1].targetLines[0], referenceEnumerator.Current)) {
                         matchedLines[^1].sourceLines.Add(referenceEnumerator.Current);
                     } else {
@@ -115,13 +122,63 @@ public class SyncCommand : KifaCommand {
         return 0;
     }
 
-    const double TimeThreshold = 0.2;
+    const double MinTimeThreshold = 0.2;
+    const double Threshold = 1;
 
-    static bool IsMatch(SubtitleLine line, SubtitleLine reference)
-        => (Math.Min(line.End.TotalMilliseconds, reference.End.TotalMilliseconds) -
-            Math.Max(line.Start.TotalMilliseconds, reference.Start.TotalMilliseconds)) / Math.Min(
-            line.End.TotalMilliseconds - line.Start.TotalMilliseconds,
-            reference.End.TotalMilliseconds - reference.Start.TotalMilliseconds) > TimeThreshold;
+    bool IsMatch(SubtitleLine line, SubtitleLine reference) {
+        var score =
+            (Math.Min(line.End.TotalMilliseconds, reference.End.TotalMilliseconds) -
+             Math.Max(line.Start.TotalMilliseconds, reference.Start.TotalMilliseconds)) / Math.Min(
+                line.End.TotalMilliseconds - line.Start.TotalMilliseconds,
+                reference.End.TotalMilliseconds - reference.Start.TotalMilliseconds);
+        return score switch {
+            > Threshold => true,
+            < MinTimeThreshold => false,
+            _ => GetContentScore(line.Content, reference.Content) > Threshold
+        };
+    }
+
+    static readonly Regex SplitterPattern = new(@"<[^>]*>| |\n|\\N|{[^}]*}|\W|(?=[^a-zA-Z0-9])");
+
+    const double DefaultContentScore = 0.5;
+    const double DefaultPartialScale = 1.5;
+
+    double GetContentScore(string lineContent, string referenceContent) {
+        if (ContentType == "no") {
+            return DefaultContentScore;
+        }
+
+        var lineWords = SplitterPattern.Split(lineContent).Where(w => !string.IsNullOrEmpty(w))
+            .ToList();
+        var referenceWords = SplitterPattern.Split(referenceContent)
+            .Where(w => !string.IsNullOrEmpty(w)).ToList();
+        var matchedWords = MatchWords(lineWords, referenceWords);
+        return matchedWords * (ContentType == "partial" ? DefaultPartialScale : 1.0) /
+               Math.Min(lineWords.Count, referenceWords.Count);
+    }
+
+    static int MatchWords(List<string> lineWords, List<string> referenceWords) {
+        var n = lineWords.Count;
+        var m = referenceWords.Count;
+        var f = new int[n + 1, m + 1];
+        for (var i = 0; i <= n; i++) {
+            for (var j = 0; j <= m; j++) {
+                if (i > 0) {
+                    f[i, j] = Math.Max(f[i, j], f[i - 1, j]);
+                }
+
+                if (j > 0) {
+                    f[i, j] = Math.Max(f[i, j], f[i, j - 1]);
+                }
+
+                if (i > 0 && j > 0 && lineWords[i - 1] == referenceWords[j - 1]) {
+                    f[i, j] = Math.Max(f[i, j], f[i - 1, j - 1] + 1);
+                }
+            }
+        }
+
+        return f[n, m];
+    }
 
     static List<SubtitleLine> GetAssLines(AssDocument assDocument) {
         return assDocument.Sections.First(section => section is AssEventsSection).AssLines
