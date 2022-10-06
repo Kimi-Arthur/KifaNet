e41a84dd13621c5d48c603a9b77ee54250a0b38b
Kimi@M1 <kimi.ribery@gmail.com>
2022-10-06 03:00:49 +0200

    fix?

--- src/Kifa.Subtitle/Ass/AssDialogueEffect.cs
+++ src/Kifa.Subtitle/Ass/AssDialogueEffect.cs
@@ -6,7 +6,8 @@ namespace Kifa.Subtitle.Ass;
 public class AssDialogueEffect {
     static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-    public virtual string EffectType { get; set; }
+    public virtual string EffectType => null;
+
     public virtual IEnumerable<string> EffectParameters => null;
 
     public static AssDialogueEffect Parse(string content) {
@@ -20,9 +21,7 @@ public class AssDialogueEffect {
                 return new AssDialogueBannerEffect();
             default:
                 Logger.Warn("Unexpected dialogue effect type: {0}", content);
-                return new AssDialogueEffect {
-                    EffectType = segments[0]
-                };
+                return new AssDialogueEffect();
         }
     }
 
