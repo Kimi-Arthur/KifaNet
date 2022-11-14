baafa4009d1c2cc3185a1ec0a3f5d96427337f72
Kimi@M1 <kimi.ribery@gmail.com>
2022-11-14 16:24:31 +0100

    rename

--- src/Kifa.Web.Api/Controllers/German/GermanWordsController.cs
+++ src/Kifa.Web.Api/Controllers/German/GermanWordsController.cs
@@ -1,10 +1,10 @@
 using Kifa.Languages.German;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Kifa.Web.Api.Controllers;
+namespace Kifa.Web.Api.Controllers.German;
 
 [Route("api/" + GermanWord.ModelId)]
-public class WordsController : KifaDataController<GermanWord, GermanWordJsonServiceClient> {
+public class GermanWordsController : KifaDataController<GermanWord, GermanWordJsonServiceClient> {
 }
 
 public class GermanWordJsonServiceClient : KifaServiceJsonClient<GermanWord>,
