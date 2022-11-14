6cb5e39b4a59dafb141a4f30e41d49d75a0e4f5a
Kimi@M1 <kimi.ribery@gmail.com>
2022-11-14 21:43:05 +0100

    A quick and naive implementation for `bookx manga`.

--- src/Kifa.Tools.BookUtil/Commands/CreateMangaCommand.cs
+++ src/Kifa.Tools.BookUtil/Commands/CreateMangaCommand.cs
@@ -1,30 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using CommandLine;
-using Kifa.Bilibili;
+using Kifa.Api.Files;
 using QuickEPUB;
 
 namespace Kifa.Tools.BookUtil.Commands;
 
 [Verb("manga", HelpText = "Generate Epub book based on Manga chapters.")]
 public class CreateMangaCommand : KifaCommand {
-    #region public late string MangaId { get; set; }
-
-    string? mangaId;
-
-    [Value(0, Required = true, HelpText = "Manga id to generate Epub book for.")]
-    public string MangaId {
-        get => Late.Get(mangaId);
-        set => Late.Set(ref mangaId, value);
-    }
-
-    #endregion
+    [Value(0, Required = true, HelpText = "Target files to link.")]
+    public IEnumerable<string> FileNames { get; set; }
 
     public override int Execute() {
-        var manga = BilibiliManga.Client.Get(MangaId);
-        var episode = manga.Episodes[0];
-        var epub = new Epub($"{manga.Title} {episode.Title}", manga.Authors[0]);
-        foreach (var page in episode.Pages) {
-            epub.AddResource(page.Id + ".jpg", EpubResourceType.JPEG, null);
+        var epub = new Epub($"天才麻将少女 第1卷", "小林立");
+
+        var sb = new StringBuilder();
+
+        var section = "";
+        var right = true;
+
+        foreach (var file in KifaFile.FindAllFiles(FileNames).files.Skip(1)) {
+            var newSection =
+                file.Parent.ToString().Split("/", StringSplitOptions.RemoveEmptyEntries)[^1]
+                    .Split(" ", 2)[^1];
+            if (newSection != section) {
+                if (sb.Length > 0) {
+                    epub.AddSection(section, sb.ToString());
+                    sb.Clear();
+                }
+
+                section = newSection;
+            }
+
+            var name = $"{newSection}/{file.Name}";
+            epub.AddResource(name, EpubResourceType.JPEG, file.OpenRead());
+            sb.Append($"<img src=\"{name}\" />");
         }
+
+        if (sb.Length > 0) {
+            epub.AddSection(section, sb.ToString());
+            sb.Clear();
+        }
+
+        using var memoryStream = new MemoryStream();
+        epub.Export(memoryStream);
+
+        var output = new KifaFile("a.epub");
+        output.Delete();
+        output.Write(memoryStream.ToArray());
 
         return 0;
     }
--- src/Kifa.Tools.BookUtil/Program.cs
+++ src/Kifa.Tools.BookUtil/Program.cs
@@ -6,5 +6,6 @@ namespace Kifa.Tools.BookUtil;
 class Program {
     static int Main(string[] args)
         => KifaCommand.Run(
-            parameters => Parser.Default.ParseArguments(parameters, typeof(ReorderCommand)), args);
+            parameters => Parser.Default.ParseArguments(parameters, typeof(ReorderCommand),
+                typeof(CreateMangaCommand)), args);
 }
