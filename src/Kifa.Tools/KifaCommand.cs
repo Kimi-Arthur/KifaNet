c8cd04837e46c61122b206a559482f151e28271e
Kimi@M1 <kimi.ribery@gmail.com>
2022-11-15 20:41:00 +0100

    Replaced usage of NLog config with coding. Should probably apply to all binaries.

--- src/Kifa.Tools/KifaCommand.cs
+++ src/Kifa.Tools/KifaCommand.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using CommandLine;
 using Kifa.Api.Files;
 using Kifa.Configs;
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace Kifa.Tools;
 
 public abstract partial class KifaCommand {
     static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-    public static HashSet<string> LoggingTargets { get; set; }
+    public static string LogPath { get; set; } = "/tmp";
 
     public static KifaFile CurrentFolder => new(".");
 
@@ -31,6 +32,8 @@ public abstract partial class KifaCommand {
             ConfigureLogger(true);
         } else if (command.NonVerbose) {
             ConfigureLogger(false);
+        } else {
+            ConfigureLogger();
         }
 
         try {
@@ -53,26 +56,114 @@ public abstract partial class KifaCommand {
             => KifaConfigs.LoadFromSystemConfigs(eventArgs.LoadedAssembly);
 
         KifaConfigs.LoadFromSystemConfigs();
-
-        if (LoggingTargets != null) {
-            ConfigureLogger();
-        }
     }
 
-    static void ConfigureLogger(bool? fullConsole = null) {
-        LogManager.Configuration.LoggingRules.Clear();
-
-        if (fullConsole == true) {
-            LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "console_full");
-            LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "file_full");
-        } else if (fullConsole == false) {
-            LogManager.Configuration.AddRule(LogLevel.Info, LogLevel.Fatal, "console");
-            LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "file_full");
-        } else {
-            foreach (var target in LoggingTargets) {
-                var minLevel = target.EndsWith("_full") ? LogLevel.Trace : LogLevel.Debug;
-                LogManager.Configuration.AddRule(minLevel, LogLevel.Fatal, target);
+    static Target ConsoleTarget = new ColoredConsoleTarget("console") {
+        ErrorStream = true,
+        Layout =
+            @"${date:format=yyyy-MM-ddTHH\:mm\:ss.ffffff} ${message}${onexception:inner=${newline}Exception\:${newline}}${exception:format=toString,Data}",
+        RowHighlightingRules = {
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Trace",
+                ForegroundColor = ConsoleOutputColor.Gray
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Debug",
+                ForegroundColor = ConsoleOutputColor.White
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Info",
+                ForegroundColor = ConsoleOutputColor.Green
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Warn",
+                ForegroundColor = ConsoleOutputColor.Yellow
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Error",
+                ForegroundColor = ConsoleOutputColor.Magenta
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Fatal",
+                ForegroundColor = ConsoleOutputColor.Red
             }
+        },
+        WordHighlightingRules = {
+            new ConsoleWordHighlightingRule {
+                Regex = "^[^ ]* ",
+                CompileRegex = true,
+                ForegroundColor = ConsoleOutputColor.DarkGray
+            }
+        }
+    };
+
+    static Target ConsoleFullTarget = new ColoredConsoleTarget("console_full") {
+        ErrorStream = true,
+        Layout = @"${pad:padding=1:fixedLength=true:inner=${level}} " +
+                 @"${date:format=yyyy-MM-ddTHH\:mm\:ss.ffffff} ${callsite}:${callsite-linenumber} " +
+                 @"${message}${onexception:inner=${newline}Exception\:${newline}}${exception:format=toString,Data}",
+        RowHighlightingRules = {
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Trace",
+                ForegroundColor = ConsoleOutputColor.Gray
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Debug",
+                ForegroundColor = ConsoleOutputColor.White
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Info",
+                ForegroundColor = ConsoleOutputColor.Green
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Warn",
+                ForegroundColor = ConsoleOutputColor.Yellow
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Error",
+                ForegroundColor = ConsoleOutputColor.Magenta
+            },
+            new ConsoleRowHighlightingRule {
+                Condition = "level == LogLevel.Fatal",
+                ForegroundColor = ConsoleOutputColor.Red
+            }
+        },
+        WordHighlightingRules = {
+            new ConsoleWordHighlightingRule {
+                Regex = "^([^ ]* ){3}",
+                CompileRegex = true,
+                ForegroundColor = ConsoleOutputColor.DarkGray
+            }
+        }
+    };
+
+    static Target FileFullTarget = new FileTarget("file_full") {
+        FileName =
+            LogPath + @"/${appdomain:format={1\}}.${date:format=yyyyMMddHHmmss:cached=true}.log",
+        Layout =
+            @"${pad:padding=1:fixedLength=true:inner=${level}} ${date:format=yyyy-MM-ddTHH\:mm\:ss.ffffff} ${callsite}:${callsite-linenumber} ${message}${onexception:inner=${newline}Exception\:${newline}}${exception:format=toString,Data}"
+    };
+
+    static void ConfigureLogger(bool? fullConsole = null) {
+        LogManager.Configuration = new LoggingConfiguration();
+
+        LogManager.Configuration.AddTarget(ConsoleTarget);
+        LogManager.Configuration.AddTarget(ConsoleFullTarget);
+        LogManager.Configuration.AddTarget(FileFullTarget);
+
+        switch (fullConsole) {
+            case true:
+                LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "console_full");
+                LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "file_full");
+                break;
+            case false:
+                LogManager.Configuration.AddRule(LogLevel.Info, LogLevel.Fatal, "console");
+                LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "file_full");
+                break;
+            default:
+                LogManager.Configuration.AddRule(LogLevel.Debug, LogLevel.Fatal, "console");
+                LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, "file_full");
+                break;
         }
 
         LogManager.ReconfigExistingLoggers();
