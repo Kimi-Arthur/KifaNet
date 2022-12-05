b5be1fea421f5610172cb120376c3f5bfc9e7954
Kimi@M1 <kimi.ribery@gmail.com>
2022-12-05 23:48:23 +0100

    Add MojiGetWordRpc.

---
+++ src/Kifa.Languages/Moji/Rpcs/MojiGetWordRpc.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Kifa.Rpc;
+using Newtonsoft.Json;
+
+namespace Kifa.Languages.Moji.Rpcs;
+
+public sealed class MojiGetWordRpc : KifaJsonParameterizedRpc<MojiGetWordRpc.Response> {
+    #region MojiGetWordRpc.Response
+
+    public partial class Response {
+        public ResponseResult Result { get; set; }
+    }
+
+    public partial class ResponseResult {
+        public List<object> The1 { get; set; }
+        public List<ResultElement> Result { get; set; }
+        public long Code { get; set; }
+    }
+
+    public partial class ResultElement {
+        public Word Word { get; set; }
+        public List<Detail> Details { get; set; }
+        public List<Detail> Subdetails { get; set; }
+        public List<Example> Examples { get; set; }
+    }
+
+    public partial class Detail {
+        public string Title { get; set; }
+        public long Index { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string WordId { get; set; }
+        public string UpdatedBy { get; set; }
+        public string ObjectId { get; set; }
+        public string DetailsId { get; set; }
+    }
+
+    public partial class Example {
+        public string Title { get; set; }
+        public long Index { get; set; }
+        public string Trans { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string WordId { get; set; }
+        public string SubdetailsId { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsFree { get; set; }
+        public long Quality { get; set; }
+        public bool IsChecked { get; set; }
+        public long ViewedNum { get; set; }
+        public string ObjectId { get; set; }
+        public string NotationTitle { get; set; }
+    }
+
+    public partial class Word {
+        public string Excerpt { get; set; }
+        public string Spell { get; set; }
+        public string Accent { get; set; }
+        public string Pron { get; set; }
+        public string Romaji { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public string Tags { get; set; }
+        public long OutSharedNum { get; set; }
+        public long VTag { get; set; }
+        public bool IsFree { get; set; }
+        public long Quality { get; set; }
+        public long ViewedNum { get; set; }
+        public string ObjectId { get; set; }
+    }
+
+    #endregion
+
+    public override string UrlPattern { get; } =
+        "https://api.mojidict.com/parse/functions/nlt-fetchManyLatestWords";
+
+    public override HttpMethod Method { get; } = HttpMethod.Post;
+
+    public override string? JsonContent { get; } = JsonConvert.SerializeObject(
+        new Dictionary<string, object> {
+            {
+                "itemsJson", new List<Dictionary<string, string>> {
+                    new() {
+                        { "objectId", "{word_id}" }
+                    }
+                }
+            },
+            { "skipAccessories", false },
+            { "_SessionToken", Configs.SessionToken },
+            { "_ApplicationId", Configs.ApplicationId }
+        });
+
+    public override bool CamelCase { get; set; } = true;
+
+    public MojiGetWordRpc(string wordId) {
+        parameters = new Dictionary<string, string> {
+            { "word_id", wordId }
+        };
+    }
+}
--- tests/Kifa.Languages.Tests/MojiTests.cs
+++ tests/Kifa.Languages.Tests/MojiTests.cs
@@ -22,4 +22,19 @@ public class MojiTests {
         Assert.Equal(firstTitle, results[0].Title);
         Assert.Equal(firstTarId, results[0].TarId);
     }
+
+    [Theory]
+    [InlineData("198934326", "鉛筆", "名詞", "えんぴつ", "◎",
+        "铅笔。（筆記具の一種。黒鉛と粘土との粉末の混合物を高熱で焼いて芯を造り、木の軸にはめて造る。1565年、イギリスで考案。江戸初期にオランダ人から輸入。）", 8)]
+    [InlineData("198962739", "車", "名", "くるま", "◎", "车，小汽车。（自動車・人力車。）", 8)]
+    public void MojiGetWordRpcTests(string wordId, string word, string type, string pronunciation,
+        string accent, string meaning, int exampleCount) {
+        var result = HttpClient.Call(new MojiGetWordRpc(wordId))!.Result.Result[0];
+        Assert.Equal(wordId, result.Word.ObjectId);
+        Assert.Equal(word, result.Word.Spell);
+        Assert.Equal(type, result.Details[0].Title);
+        Assert.Equal(pronunciation, result.Word.Pron);
+        Assert.Equal(accent, result.Word.Accent);
+        result.Examples.Should().HaveCount(exampleCount);
+    }
 }
