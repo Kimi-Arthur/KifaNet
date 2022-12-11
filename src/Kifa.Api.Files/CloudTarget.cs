626c5b5d090aa04f4aeb43245f663424e98f4864
Kimi@M1 <kimi.ribery@gmail.com>
2022-12-11 22:11:58 +0100

    doc

--- src/Kifa.Api.Files/CloudTarget.cs
+++ src/Kifa.Api.Files/CloudTarget.cs
@@ -6,6 +6,7 @@ public class CloudTarget {
     public CloudServiceType ServiceType { get; set; }
     public CloudFormatType FormatType { get; set; }
 
+    // Parses targetSpec like google.v1, swiss.v2 etc.
     public static CloudTarget Parse(string targetSpec) {
         var segments = targetSpec.Split(".");
         return new CloudTarget {
