92a73711dc4348b161da677a454eb034b60c7b73
Kimi@M1 <kimi.ribery@gmail.com>
2022-12-14 01:37:43 +0100

    More efficient.

--- Experimental/LockExperiments/Program.cs
+++ Experimental/LockExperiments/Program.cs
@@ -13,32 +13,55 @@ public class Program {
     static ConcurrentDictionary<string, Link<TestModel>> Locks = new();
 
     static Link<TestModel> GetLock(string id) => Locks.GetOrAdd(id, key => key);
+    static void DeleteLock(string id) => Locks.Remove(id, out _);
 
     static void Work1(string id) {
         lock (GetLock(id)) {
-            Console.WriteLine($"Start Work1: {id}");
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-            Console.WriteLine($"End Work1: {id}");
+            Console.WriteLine($"Start Work1 ({DateTime.Now}): {id}");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"End Work1 ({DateTime.Now}): {id}");
         }
+
+        DeleteLock(id);
     }
 
     static void Work2(string id) {
         lock (GetLock(id)) {
-            Console.WriteLine($"Start Work2: {id}");
-            Thread.Sleep(TimeSpan.FromSeconds(5));
-            Console.WriteLine($"End Work2: {id}");
+            Console.WriteLine($"Start Work2 ({DateTime.Now}): {id}");
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            Console.WriteLine($"End Work2 ({DateTime.Now}): {id}");
         }
+
+        DeleteLock(id);
+    }
+
+    static void Try1() {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        Enumerable.Repeat("", 10000).Select<string, string>(_
+                => new string(Enumerable.Repeat(chars, 100).Select(s => s[random.Next(s.Length)])
+                    .ToArray())).SelectMany(id => new List<Action> {
+                () => Work1(id),
+                () => Work2(id)
+            }).OrderBy(order => random.Next()).AsParallel().WithDegreeOfParallelism(100)
+            .ForAll(f => f.Invoke());
+    }
+
+    static void Try2() {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        Enumerable.Repeat("", 10000).Select<string, string>(_
+                => new string(Enumerable.Repeat(chars, 100).Select(s => s[random.Next(s.Length)])
+                    .ToArray())).SelectMany(id => new List<Action> {
+                () => Work1(id),
+                () => Work2(id)
+            }).OrderBy(order => random.Next()).AsParallel().WithDegreeOfParallelism(100)
+            .ForAll(f => f.Invoke());
     }
 
     public static void Main(string[] args) {
-        var random = new Random();
-        new List<string> {
-            "abc",
-            "bcd",
-            "cde"
-        }.SelectMany(id => new List<Action> {
-            () => Work1(id),
-            () => Work2(id)
-        }).OrderBy(order => random.Next()).AsParallel().ForAll(f => f.Invoke());
+        Try1();
+        Console.ReadLine();
+        Try2();
     }
 }
