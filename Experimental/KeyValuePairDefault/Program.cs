60b18f703b74e092849353077f527f0b9399a050
Kimi@M1 <kimi.ribery@gmail.com>
2022-12-16 18:50:23 +0100

    Experiment with Nullable.

---
+++ Experimental/KeyValuePairDefault/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var x = new Dictionary<string, string> {
+    // { "", "ab" }
+};
+
+Console.WriteLine(x.Select(kv => (KeyValuePair<string, string>?) (kv))
+    .MaxBy(kv => kv.Value.Key.Length) == null);
