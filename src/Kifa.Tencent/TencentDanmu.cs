520159c317e80c314aa9743b0ab01c2bd76c5e69
Kimi@M1 <kimi.ribery@gmail.com>
2023-01-22 22:37:39 +0100

    Add dummy Kifa.Tencent project.

---
+++ src/Kifa.Tencent/TencentDanmu.cs
@@ -0,0 +1,7 @@
+﻿namespace Kifa.Tencent;
+
+public class TencentDanmu {
+    // APIs:
+    //   https://dm.video.qq.com/barrage/base/i0045u918s5
+    //   https://dm.video.qq.com/barrage/segment/d0045caapwc/t/v1/0/30000
+}
