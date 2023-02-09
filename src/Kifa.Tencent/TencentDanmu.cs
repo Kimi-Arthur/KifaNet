7209d04bfec40296cb626ee09d90505f44af8cc8
Kimi@M1 <kimi.ribery@gmail.com>
2023-02-09 23:34:23 +0100

    Add generation logic.

--- src/Kifa.Tencent/TencentDanmu.cs
+++ src/Kifa.Tencent/TencentDanmu.cs
@@ -1,11 +1,14 @@
 ﻿using System.Drawing;
 using System.Globalization;
+using Kifa.Subtitle.Ass;
 using Kifa.Tencent.Rpcs;
 using Newtonsoft.Json;
 
 namespace Kifa.Tencent;
 
 public class TencentDanmu {
+    static readonly TimeSpan DefaultDuration = TimeSpan.FromSeconds(8);
+
     public required string Text { get; set; }
 
     public Color? TextColor { get; set; }
@@ -13,19 +16,50 @@ public class TencentDanmu {
     public required TimeSpan VideoTime { get; set; }
 
     internal static TencentDanmu Parse(SegmentDanmuRpc.Barrage barrage) {
-        var color = string.IsNullOrEmpty(barrage.ContentStyle)
+        var color = !string.IsNullOrEmpty(barrage.ContentStyle)
             ? JsonConvert.DeserializeObject<ContentStyle>(barrage.ContentStyle,
-                KifaJsonSerializerSettings.Default)?.GradientColors[0]
+                KifaJsonSerializerSettings.Default)?.GradientColors?[0]
             : null;
+        if (color == "ffffff") {
+            color = null;
+        }
 
         return new TencentDanmu {
             Text = barrage.Content,
             VideoTime = TimeSpan.FromMilliseconds(barrage.TimeOffset),
             TextColor = color == null
                 ? null
-                : Color.FromArgb(int.Parse(color[..2], NumberStyles.HexNumber),
+                : Color.FromArgb(AssStyle.DefaultSemiAlpha,
+                    int.Parse(color[..2], NumberStyles.HexNumber),
                     int.Parse(color[2..4], NumberStyles.HexNumber),
                     int.Parse(color[4..], NumberStyles.HexNumber))
+        };
+    }
+
+    public AssDialogue GenerateAssDialogue() {
+        var textElements = new List<AssDialogueTextElement>();
+        if (TextColor.HasValue) {
+            textElements.Add(new AssDialogueControlTextElement {
+                Elements = new List<AssControlElement> {
+                    new PrimaryColourStyle {
+                        Value = TextColor.Value
+                    }
+                }
+            });
+        }
+
+        textElements.Add(new AssDialogueRawTextElement {
+            Content = Text.Trim()
+        });
+
+        return new AssDialogue {
+            Start = VideoTime,
+            End = VideoTime + DefaultDuration,
+            Layer = 0,
+            Text = new AssDialogueText {
+                TextElements = textElements
+            },
+            Style = AssStyle.NormalCommentStyle
         };
     }
 }
@@ -33,6 +67,6 @@ public class TencentDanmu {
 class ContentStyle {
     // {"color":"ffffff","gradient_colors":["CD87FF","CD87FF"],"position":1}
     public string Color { get; set; }
-    public List<string> GradientColors { get; set; }
+    public List<string>? GradientColors { get; set; }
     public int Position { get; set; }
 }
--- tests/Kifa.Tencent.Tests/DanmuTests.cs
+++ tests/Kifa.Tencent.Tests/DanmuTests.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net.Http;
+using FluentAssertions;
+using Kifa.Subtitle.Ass;
 using Kifa.Tencent.Rpcs;
 using Xunit;
 
@@ -22,7 +27,7 @@ public class DanmuTests {
         Assert.Equal("把心脏起搏器按停了",
             response.BarrageList.First(b => b.Id == "76561198061395137").Content);
         Assert.Equal(
-            "{\"color\":\"ffffff\",\"gradient_colors\":[\"FDA742\",\"FBF076\"],\"position\":1}",
+            "{\"color\":\"ffffff\",\"gradient_colors\":[\"FF1964\",\"FF1964\"],\"position\":1}",
             response.BarrageList.First(b => b.Id == "76561198061432944").ContentStyle);
     }
 
@@ -31,11 +36,64 @@ public class DanmuTests {
         var danmuList = httpClient.Call(new SegmentDanmuRpc("i0045u918s5", "t/v1/210000/240000"))
             .BarrageList.Select(TencentDanmu.Parse).ToList();
         Assert.Equal(600, danmuList.Count);
+        var danmu1 = danmuList.First(d => d.Text == "世界属于三体");
+        Assert.Equal("世界属于三体", danmu1.Text);
+        Assert.Equal(TimeSpan.FromSeconds(210), danmu1.VideoTime);
+        Assert.Null(danmu1.TextColor);
+
+        var danmu2 = danmuList.First(d => d.Text == "人类的命运，就在这纤细的两指之上");
+        Assert.Equal(Color.FromArgb(0xCD, 0x87, 0xFF), danmu2.TextColor);
     }
 
     [Fact]
     public void GetDanmuListTest() {
         var danmuList = TencentVideo.GetDanmuList("i0045u918s5");
-        Assert.Equal(52241, danmuList.Count());
+        danmuList.Should().HaveCountGreaterThan(52200);
+    }
+
+    [Fact]
+    public void GenerateAssDialogTest() {
+        var danmu1 = new TencentDanmu {
+            Text = "世界属于三体",
+            VideoTime = TimeSpan.FromSeconds(210),
+        };
+
+        danmu1.GenerateAssDialogue().Should().BeEquivalentTo(new AssDialogue {
+            Start = TimeSpan.FromSeconds(210),
+            End = TimeSpan.FromSeconds(218),
+            Style = AssStyle.NormalCommentStyle,
+            Text = new AssDialogueText {
+                TextElements = new List<AssDialogueTextElement> {
+                    new AssDialogueRawTextElement {
+                        Content = "世界属于三体"
+                    }
+                }
+            }
+        });
+        var danmu2 = new TencentDanmu {
+            Text = "人类的命运，就在这纤细的两指之上",
+            VideoTime = TimeSpan.FromSeconds(211),
+            TextColor = Color.FromArgb(0xD0, 0xCD, 0x87, 0xFF)
+        };
+
+        danmu2.GenerateAssDialogue().Should().BeEquivalentTo(new AssDialogue {
+            Start = TimeSpan.FromSeconds(211),
+            End = TimeSpan.FromSeconds(219),
+            Style = AssStyle.NormalCommentStyle,
+            Text = new AssDialogueText {
+                TextElements = new List<AssDialogueTextElement> {
+                    new AssDialogueControlTextElement {
+                        Elements = new List<AssControlElement> {
+                            new PrimaryColourStyle {
+                                Value = Color.FromArgb(0xD0, 0xCD, 0x87, 0xFF)
+                            }
+                        }
+                    },
+                    new AssDialogueRawTextElement {
+                        Content = "人类的命运，就在这纤细的两指之上"
+                    }
+                }
+            }
+        });
     }
 }
