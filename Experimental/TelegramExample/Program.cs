54adb225eb45781749d936e44219122230876ebd
Kimi@M1 <kimi.ribery@gmail.com>
2023-03-31 12:15:45 +0200

    Looks like it's due to caching (new messages may not appear fast enough).
    In which case, Message_GetHistory can be a good replacement. We may need to determine the timeframe needed for Message_GetHistory.
    
    A solution with two steps is coming!

--- Experimental/TelegramExample/Program.cs
+++ Experimental/TelegramExample/Program.cs
@@ -17,11 +17,21 @@ var channel = client.Messages_GetAllChats().Result.chats[chatId];
 // Using InputPeer.Self will make the last line pass even if the breaking line is not commented.
 // var channel = InputPeer.Self;
 
-var text = Random.Shared.NextInt64().ToString();
+// var text = Random.Shared.NextInt64().ToString();
+var text = "abcdefghijklmn";
 
 // Commenting this line will make the last line pass. (breaking line)
-Console.WriteLine($"Before (should be 0): {client.Messages_Search(channel, text).Result.Count}");
+Console.WriteLine(
+    $"Before (should be 0): {client.Messages_Search(channel, text, min_date: DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(6))).Result.Count}");
+
+foreach (var m in client.Messages_GetHistory(channel).Result.Messages) {
+    Console.WriteLine($"History: {m.Date}");
+}
 
 Console.WriteLine($"Send: {client.SendMessageAsync(channel, text).Result}");
 
 Console.WriteLine($"After (should be 1): {client.Messages_Search(channel, text).Result.Count}");
+
+foreach (var m in client.Messages_GetHistory(channel).Result.Messages) {
+    Console.WriteLine($"History: {m.Date}");
+}
