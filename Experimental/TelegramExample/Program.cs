e7d02fe3370cecebd030c80c7fc7d5cdd9081283
Kimi@M1 <kimi.ribery@gmail.com>
2023-03-31 22:24:27 +0200

    Update the test to verify the exact behavior.

--- Experimental/TelegramExample/Program.cs
+++ Experimental/TelegramExample/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using TL;
 using WTelegram;
 
@@ -14,24 +16,20 @@ Console.WriteLine($"Login: {client.Login(phoneNumber).Result}");
 
 var channel = client.Messages_GetAllChats().Result.chats[chatId];
 
-// Using InputPeer.Self will make the last line pass even if the breaking line is not commented.
-// var channel = InputPeer.Self;
+var text = "whywhywhy";
 
-// var text = Random.Shared.NextInt64().ToString();
-var text = "abcdefghijklmn";
+for (int i = 0; i < 100000; i++) {
+    var history = client.Messages_GetHistory(channel).Result.Messages.Select(m => m as Message)
+        .Where(m => m?.message == text).ToList();
+    foreach (var m in history) {
+        Console.WriteLine($"History: {m.Date}, {m.ID}");
+    }
 
-// Commenting this line will make the last line pass. (breaking line)
-Console.WriteLine(
-    $"Before (should be 0): {client.Messages_Search(channel, text, min_date: DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(6))).Result.Count}");
+    Console.WriteLine($"History (should be {i}): {history.Count}");
 
-foreach (var m in client.Messages_GetHistory(channel).Result.Messages) {
-    Console.WriteLine($"History: {m.Date}");
-}
+    Console.WriteLine(
+        $"Search (should be {i}): {client.Messages_Search(channel, text).Result.Count}");
 
-Console.WriteLine($"Send: {client.SendMessageAsync(channel, text).Result}");
-
-Console.WriteLine($"After (should be 1): {client.Messages_Search(channel, text).Result.Count}");
-
-foreach (var m in client.Messages_GetHistory(channel).Result.Messages) {
-    Console.WriteLine($"History: {m.Date}");
+    Console.WriteLine($"Send: {client.SendMessageAsync(channel, text).Result}");
+    Thread.Sleep(TimeSpan.FromSeconds(10));
 }
