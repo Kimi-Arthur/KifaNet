ec4a8bde0c494cbb3a277ad3f481a67d5b1fa36d
Kimi@M1 <kimi.ribery@gmail.com>
2023-04-08 01:09:02 +0200

    Support multiple bilibili ids.

--- src/Kifa.Tools.BiliUtil/Commands/DownloadVideoCommand.cs
+++ src/Kifa.Tools.BiliUtil/Commands/DownloadVideoCommand.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using CommandLine;
 using Kifa.Bilibili;
@@ -10,29 +11,31 @@ public class DownloadVideoCommand : DownloadCommand {
     static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
     [Value(0, Required = true,
-        HelpText = "The video id from Bilibili. With possible p{n} as a suffix.")]
-    public string Aid { get; set; }
+        HelpText = "Video ids from Bilibili, like av12345, with possible p{n} as a suffix.")]
+    public IEnumerable<string> Aids { get; set; }
 
     public override int Execute() {
-        var segments = Aid.Split('p');
-        var aid = segments.First();
+        foreach (var aidWithPage in Aids) {
+            var segments = aidWithPage.Split('p');
+            var aid = segments.First();
 
-        var video = BilibiliVideo.Client.Get(aid);
-        if (video == null) {
-            Logger.Fatal($"Cannot find video ({aid}). Exiting.");
-            return 1;
-        }
+            var video = BilibiliVideo.Client.Get(aid);
+            if (video == null) {
+                Logger.Fatal($"Cannot find video ({aid}). Exiting.");
+                continue;
+            }
 
-        if (segments.Length == 2) {
-            var pid = int.Parse(segments.Last());
-            Logger.Info($"Downloading part {pid} of video {video.Id}...");
-            Download(video, pid);
-            return 0;
-        }
+            if (segments.Length == 2) {
+                var pid = int.Parse(segments.Last());
+                Logger.Info($"Downloading part {pid} of video {video.Id}...");
+                Download(video, pid);
+                continue;
+            }
 
-        Logger.Info($"Downloading all parts of video {video.Id}...");
-        foreach (var page in video.Pages) {
-            Download(video, page.Id);
+            Logger.Info($"Downloading all parts of video {video.Id}...");
+            foreach (var page in video.Pages) {
+                Download(video, page.Id);
+            }
         }
 
         return 0;
