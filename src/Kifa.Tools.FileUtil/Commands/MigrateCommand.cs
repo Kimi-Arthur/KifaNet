bf7b2cc3b3f4049b40a21cefe552f158e375e155
Kimi@MBP <kimi.ribery@gmail.com>
2023-07-20 18:07:07 +0200

    Better logging and mode control.

--- src/Kifa.Tools.FileUtil/Commands/MigrateCommand.cs
+++ src/Kifa.Tools.FileUtil/Commands/MigrateCommand.cs
@@ -8,17 +8,23 @@ namespace Kifa.Tools.FileUtil.Commands;
 
 [Verb("migrate", HelpText = "Migrate Google Drive files to a new cell and quick check on it.")]
 public class MigrateCommand : KifaCommand {
+    [Option('d', "dry-run", HelpText = "Only dry run, not actually migrate.")]
+    public bool DryRun { get; set; } = false;
+
+    [Option('q', "quiet", HelpText = "Quiet mode, no confirmation is requested.")]
+    public bool QuietMode { get; set; } = false;
+
     public override int Execute() {
         var files = FileInformation.Client.List();
-        var processed = new HashSet<string>();
+        var processed = new Dictionary<string, string>();
         foreach (var (file, info) in files) {
             if (info.Sha256 == null) {
-                Console.WriteLine($"{file}: no sha256");
+                Console.WriteLine($"{file}:(3) no sha256");
                 continue;
             }
 
-            if (processed.Contains(info.Sha256)) {
-                Console.WriteLine($"{file}: sha256 already processed.");
+            if (processed.TryGetValue(info.Sha256, out var processedFile)) {
+                Console.WriteLine($"{file}:(2) sha256 already processed by {processedFile}.");
                 continue;
             }
 
@@ -38,20 +44,27 @@ public class MigrateCommand : KifaCommand {
             }
 
             if (foundTarget == true && foundSource == null) {
-                Console.WriteLine($"{file}: already moved.");
-                processed.Add(info.Sha256);
+                Console.WriteLine($"{file}:(1) already moved.");
+                processed.Add(info.Sha256, info.Id);
                 continue;
             }
 
             if (foundSource == true && foundTarget == null) {
-                Console.WriteLine($"{file}: to move.");
-                processed.Add(info.Sha256);
+                if (DryRun) {
+                    Console.WriteLine($"{file}:(0) to move.");
+                } else if (QuietMode || Confirm($"Confirm migrating {source} to {target}")) {
+                    Console.WriteLine($"{file}:(+) moved.");
+                } else {
+                    Console.WriteLine($"{file}:(-) skipped.");
+                }
+
+                processed.Add(info.Sha256, info.Id);
                 continue;
             }
 
             Console.WriteLine(
-                $"{file}: error as source is {foundSource} and target is {foundTarget}");
-            processed.Add(info.Sha256);
+                $"{file}:(!) error as source is {foundSource} and target is {foundTarget}");
+            processed.Add(info.Sha256, info.Id);
         }
 
         return 0;
