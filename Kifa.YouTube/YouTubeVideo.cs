1c711f9996ca7f56064f8f8ae1b6611c1c4153f2
Kimi@M1 <kimi.ribery@gmail.com>
2023-11-20 21:01:22 +0100

    Add basic YouTubeVideo class.

---
+++ Kifa.YouTube/YouTubeVideo.cs
@@ -0,0 +1,27 @@
+﻿using Kifa.Service;
+
+namespace Kifa.YouTube;
+
+public class YouTubeVideo : DataModel, WithModelId<YouTubeVideo> {
+    public static string ModelId => "youtube/videos";
+
+    public static KifaServiceClient<YouTubeVideo> Client { get; set; } =
+        new KifaServiceRestClient<YouTubeVideo>();
+
+    public string? Author { get; set; }
+    public List<string> Categories { get; set; } = new();
+    public string? Description { get; set; }
+    public TimeSpan Duration { get; set; }
+    public long Fps { get; set; }
+    public long Height { get; set; }
+    public List<string> Tags { get; set; } = new();
+    public string? Thumbnail { get; set; }
+    public string? Title { get; set; }
+    public Date? UploadDate { get; set; }
+    public long Width { get; set; }
+
+    public override DateTimeOffset? Fill() {
+        // Set refresh date and change version when we have modifiable data.
+        return null;
+    }
+}
