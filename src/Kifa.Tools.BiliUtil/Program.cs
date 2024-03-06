c96fc7d0a8fa23f3986ccc7a62f91a3c12a58c7d
Kimi@MBP <kimi.ribery@gmail.com>
2024-03-06 15:11:12 +0100

    Update for bilix.

--- src/Kifa.Tools.BiliUtil/Program.cs
+++ src/Kifa.Tools.BiliUtil/Program.cs
@@ -1,13 +1,15 @@
-﻿using CommandLine;
+﻿using System;
+using CommandLine;
 using Kifa.Tools.BiliUtil.Commands;
 
 namespace Kifa.Tools.BiliUtil;
 
 class Program {
     static int Main(string[] args)
-        => KifaCommand.Run(
-            new Parser(settings => { settings.CaseInsensitiveEnumValues = true; })
-                .ParseArguments<GetChatCommand, DownloadVideoCommand, DownloadUploaderCommand,
-                    DownloadBangumiCommand, GetCoverCommand, LinkCommand, DownloadMangaCommand,
-                    GetTencentChatCommand, DownloadArchiveCommand, ShadowCommand>, args);
+        => KifaCommand.Run(new Parser(settings => {
+            settings.CaseInsensitiveEnumValues = true;
+            settings.HelpWriter = Console.Error;
+        }).ParseArguments<GetChatCommand, DownloadVideoCommand, DownloadUploaderCommand,
+            DownloadBangumiCommand, GetCoverCommand, LinkCommand, DownloadMangaCommand,
+            GetTencentChatCommand, DownloadArchiveCommand, ShadowCommand>, args);
 }
