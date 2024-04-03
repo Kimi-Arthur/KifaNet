103276a49b814fa95a21edc00a72f21eac7753b9
Kimi@MBP <kimi.ribery@gmail.com>
2024-04-03 16:43:24 +0200

    Allow putting videos into the folder with video title instead of uploader name.

--- src/Kifa.Tools.BiliUtil/Commands/DownloadVideoCommand.cs
+++ src/Kifa.Tools.BiliUtil/Commands/DownloadVideoCommand.cs
@@ -14,6 +14,11 @@ public class DownloadVideoCommand : DownloadCommand {
         HelpText = "Video ids from Bilibili, like av12345, with possible p{n} as a suffix.")]
     public IEnumerable<string> Aids { get; set; }
 
+    [Option('n', "use-video-name",
+        HelpText =
+            "Use video name (and id) as folder name instead of uploader name. This is best for a collection of videos with one id.")]
+    public bool UseVideoNameFolder { get; set; } = false;
+
     public override int Execute() {
         foreach (var aidWithPage in Aids) {
             var segments = aidWithPage.Split('p');
@@ -28,13 +33,15 @@ public class DownloadVideoCommand : DownloadCommand {
             if (segments.Length == 2) {
                 var pid = int.Parse(segments.Last());
                 Logger.Info($"Downloading part {pid} of video {video.Id}...");
-                Download(video, pid);
+                Download(video, pid,
+                    alternativeFolder: UseVideoNameFolder ? $"{video.Title}-{video.Id}" : null);
                 continue;
             }
 
             Logger.Info($"Downloading all parts of video {video.Id}...");
             foreach (var page in video.Pages) {
-                Download(video, page.Id);
+                Download(video, page.Id,
+                    alternativeFolder: UseVideoNameFolder ? $"{video.Title}-{video.Id}" : null);
             }
         }
 
