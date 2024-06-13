94e85637b3b68734faa7ba8b3ab24cc89e7b1204
Kimi@MBP <kimi.ribery@gmail.com>
2024-06-13 16:12:08 +0200

    Debugging changes that seem to suggest OAuthAccountController.Get was not called.

--- src/Kifa.Web.Api/Controllers/OAuthAccountController.cs
+++ src/Kifa.Web.Api/Controllers/OAuthAccountController.cs
@@ -4,6 +4,7 @@ using Kifa.Service;
 using Kifa.Web.Api.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using NLog;
 
 namespace Kifa.Web.Api.Controllers;
 
@@ -11,11 +12,14 @@ public abstract class
     OAuthAccountController<TAccount> : KifaDataController<TAccount, OAuthAccountClient<TAccount>>
     where TAccount : OAuthAccount, WithModelId<TAccount>, new() {
     static readonly HttpClient HttpClient = new();
+    static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
     static readonly KifaServiceJsonClient<TAccount> ServiceClient = new();
 
     public override ActionResult<TAccount?> Get(string id, bool refresh = false) {
+        Logger.Trace("Before Get");
         var result = base.Get(id, refresh);
+        Logger.Trace($"Got OAuth account: {result.Value}");
         return result.Value != null ? result : AccountAdd(id);
     }
 
@@ -30,9 +34,9 @@ public abstract class
             HttpClient.FetchJToken(() => new HttpRequestMessage(HttpMethod.Post, tokenUrl));
         var account = new TAccount {
             Id = state,
-            AccessToken = (string) response["access_token"],
-            RefreshToken = (string) response["refresh_token"],
-            Scope = (string) response["scope"]
+            AccessToken = ((string?) response["access_token"]).Checked(),
+            RefreshToken = ((string?) response["refresh_token"]).Checked(),
+            Scope = ((string?) response["scope"]).Checked()
         };
 
         return account.FillUserInfo().And(() => ServiceClient.Set(account)).And(Redirect(
