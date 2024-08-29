e0bc23348871c678669ee1014b742a5ec83715fc
Kimi@M1 <kimi.ribery@gmail.com>
2024-08-30 00:03:18 +0200

    No need to have task related stuff in KifaCommand.

--- src/Kifa.Tools/KifaCommand.Job.cs
+++ src/Kifa.Tools/KifaCommand.Job.cs
@@ -7,22 +7,17 @@ namespace Kifa.Tools;
 public abstract partial class KifaCommand {
     public string? RunId { get; set; }
 
-    // Indexes of current task ids in each level.
-    public List<string> CurrentTaskIds { get; set; } = [];
-
     protected void CreateRun(List<string> args) {
         RunId = "generate a sortable run id";
-        KifaRun.Client.Set(new KifaRun {
-            Arguments = args
-        });
-        throw new NotImplementedException();
-    }
-
-    protected void StartJob() {
-        throw new NotImplementedException();
-    }
-
-    protected void AddSubTasks() {
+        var run = new KifaRun {
+            Tool = null,
+            Version = null,
+            Arguments = args,
+            Host = null,
+            ProcessId = null,
+            StartTime = DateTimeOffset.UtcNow
+        };
+        KifaRun.Client.Set(run);
         throw new NotImplementedException();
     }
 }
