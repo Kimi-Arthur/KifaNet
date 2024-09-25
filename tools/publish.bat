1cf2a95af0bb505968cbde56fa4ff34fe065c7d6
Kimi@ZRH <kimi.ribery@gmail.com>
2024-09-25 02:23:09 +0200

    backslash helps.

--- tools/publish.bat
+++ tools/publish.bat
@@ -1,3 +1,3 @@
 rmdir publish
 dotnet pack -o publish -c Release --include-symbols "%1"
-dotnet nuget push publish/*.symbols.nupkg -s https://api.nuget.org/v3/index.json -k oy2matfsxzjesq6zvqsq7g4m62wafpsebh4lnncf6aac2a
+dotnet nuget push publish\*.symbols.nupkg -s https://api.nuget.org/v3/index.json -k oy2matfsxzjesq6zvqsq7g4m62wafpsebh4lnncf6aac2a
