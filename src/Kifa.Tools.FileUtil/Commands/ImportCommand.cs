3982ac6a8720e2a23cf10fe25ca7b894ba3ef6e4
Kimi@M1 <kimi.ribery@gmail.com>
2024-10-29 22:27:09 +0100

    Supposedly a noop change that separates Type and SourceId.

--- src/Kifa.Tools.FileUtil/Commands/ImportCommand.cs
+++ src/Kifa.Tools.FileUtil/Commands/ImportCommand.cs
@@ -25,26 +25,57 @@ class ImportCommand : KifaCommand {
     [Option('i', "id", HelpText = "Treat input files as logical ids.")]
     public bool ById { get; set; } = false;
 
-    [Option('s', "source-id", HelpText = "ID for the source, like tv_shows/Westworld/1")]
-    public string SourceId { get; set; }
+    [Option('t', "type",
+        HelpText = "Type for the source, in case it cannot be inferred from the path")]
+    public string? Type { get; set; }
+
+    [Option('s', "source-id", HelpText = "ID for the source, like Westworld/1")]
+    public string? SourceId { get; set; }
 
     [Option('r', "recursive", HelpText = "Whether to list folder recursively.")]
     public bool Recursive { get; set; }
 
     public override int Execute(KifaTask? task = null) {
-        var segments = SourceId.Trim('/').Split('/');
-        var type = segments[0];
+        var files = KifaFile.FindExistingFiles(FileNames);
+        files = FilterRegisteredFiles(files);
+        if (files.Count == 0) {
+            Logger.Error("No files found. Action canceled.");
+            return 1;
+        }
 
+        foreach (var file in files) {
+            Console.WriteLine(file);
+        }
+
+        if (!Confirm($"Confirm processing the {files.Count} files above?")) {
+            Logger.Error("Action canceled.");
+            return 1;
+        }
+
+        // All source files are assumed to be in a path like
+        //     /Downloads/<Type>/<Title>/<Subpaths>
+        // For example,
+        //     /Downloads/TV Shows/DARK.S01.2160p.NF.WEBRip.DDP5.1.x264-NTb[rartv]/DARK.S01E01.Secrets.2160p.NF.WEBRip.DDP5.1.x264-NTb.mkv
+        //     /Downloads/Gaming/黑桐谷歌-合集·【黑神话:悟空】.43536-3659146.bilibili/4K超清【黑神话:悟空】全妖降伏攻略解说 11 第三回 夜生白露-小雷音寺.av113073067262848p1.c25765217836.120-hevc.mp4
+        //     /Downloads/Movies/Death Note Trilogy/1. Death Note (2006).mkv
+
+        Type ??= files[0].PathSegments[1];
+        foreach (var file in files) {
+            if (file.PathSegments[1] != Type) {
+                Logger.Error("Files don't share a common type. Exit.");
+                return 1;
+            }
+        }
+
+        SourceId ??= InferSourceId(files[0].Path);
         List<(Season Season, Episode Episode, bool Matched)> episodes;
         Formattable series;
-
-        switch (type) {
+        switch (Type) {
             case "Gaming": {
                 // Example:
                 // Gaming/黑桐谷歌/漫威蜘蛛侠2 -> /Gaming/黑桐谷歌/漫威蜘蛛侠2/漫威蜘蛛侠2 EP01 表面张力.mp4
-                var id = segments[1];
                 series = new Gaming {
-                    Id = segments[1..].JoinBy("/")
+                    Id = SourceId
                 };
                 episodes = Enumerable.Range(1, 100).Select(i => (new Season {
                     Id = 1
@@ -54,9 +85,10 @@ class ImportCommand : KifaCommand {
                 break;
             }
             case "TV Shows": {
-                var id = segments.Length > 1 ? segments[1] : "";
-                var seasonId = segments.Length > 2 ? int.Parse(segments[2]) : 0;
-                var episodeId = segments.Length > 3 ? int.Parse(segments[3]) : 0;
+                var segments = SourceId.Split('/');
+                var id = segments.Length > 0 ? segments[0] : "";
+                var seasonId = segments.Length > 1 ? int.Parse(segments[1]) : 0;
+                var episodeId = segments.Length > 2 ? int.Parse(segments[2]) : 0;
                 var tvShow = TvShow.Client.Get(id);
                 series = tvShow;
                 episodes = tvShow.Seasons.Where(season => seasonId <= 0 || season.Id == seasonId)
@@ -66,9 +98,10 @@ class ImportCommand : KifaCommand {
                 break;
             }
             case "Anime": {
-                var id = segments.Length > 1 ? segments[1] : "";
-                var seasonId = segments.Length > 2 ? int.Parse(segments[2]) : 0;
-                var episodeId = segments.Length > 3 ? int.Parse(segments[3]) : 0;
+                var segments = SourceId.Split('/');
+                var id = segments.Length > 0 ? segments[0] : "";
+                var seasonId = segments.Length > 1 ? int.Parse(segments[1]) : 0;
+                var episodeId = segments.Length > 2 ? int.Parse(segments[2]) : 0;
                 var anime = Anime.Client.Get(id);
                 series = anime;
                 episodes = anime.Seasons.Where(season => seasonId <= 0 || season.Id == seasonId)
@@ -91,15 +124,9 @@ class ImportCommand : KifaCommand {
 
                 return 0;
             default:
-                SourceId = SourceId.TrimEnd('/');
-                if (!SourceId.StartsWith('/')) {
-                    SourceId = $"/{SourceId}";
-                }
-
-                Console.WriteLine(
-                    $"{SourceId} doesn't have a known type. Will treat it like base folder");
+                Console.WriteLine($"{Type} is not known. Will treat it like base folder");
                 var lastSlash = SourceId.LastIndexOf("/");
-                var folder = SourceId[..lastSlash];
+                var folder = $"/{Type}/{SourceId[..lastSlash]}";
                 var baseName = SourceId[(lastSlash + 1)..];
                 string? fileVersion = null;
 
@@ -141,25 +168,25 @@ class ImportCommand : KifaCommand {
                 return 0;
         }
 
-        var files = FileNames.SelectMany(path
+        var fileMatches = FileNames.SelectMany(path
                 => FileInformation.Client.ListFolder(ById ? path : new KifaFile(path).Id,
                     Recursive))
             .Select(f => (File: f, Matched: false)).ToList();
 
-        for (int i = 0; i < files.Count; i++) {
-            var info = FileInformation.Client.Get(files[i].File);
+        for (int i = 0; i < fileMatches.Count; i++) {
+            var info = FileInformation.Client.Get(fileMatches[i].File);
             var existingMatch = info.GetAllLinks().Select(l => (Link: l, Episode: series.Parse(l)))
                 .FirstOrDefault(e => e.Episode != null, ("", null));
 
             if (existingMatch.Episode.HasValue) {
                 var match = existingMatch.Episode.Value;
-                Logger.Info($"{files[i].File} already matched to {existingMatch.Link}");
+                Logger.Info($"{fileMatches[i].File} already matched to {existingMatch.Link}");
                 MarkMatched(episodes, match.Season, match.Episode);
-                files[i] = (files[i].File, true);
+                fileMatches[i] = (fileMatches[i].File, true);
             }
         }
 
-        foreach (var (file, matched) in files) {
+        foreach (var (file, matched) in fileMatches) {
             if (matched) {
                 continue;
             }
@@ -196,6 +223,23 @@ class ImportCommand : KifaCommand {
         }
 
         return 0;
+    }
+
+    string InferSourceId(string title) {
+        throw new NotImplementedException();
+    }
+
+    List<KifaFile> FilterRegisteredFiles(List<KifaFile> files) {
+        var notRegisteredFiles = files.Where(f => !f.Registered).ToList();
+        if (notRegisteredFiles.Count == 0) {
+            return files;
+        }
+
+        var toRegister = SelectMany(notRegisteredFiles,
+            choiceToString: file => $"{file} ({file.FileInfo?.Size.ToSizeString()})",
+            choiceName: "files to add to the system");
+        toRegister.ForEach(f => ExecuteItem($"register {f}", () => f.Add()));
+        return files.Where(f => f.Registered).ToList();
     }
 
     static void MarkMatched(List<(Season Season, Episode Episode, bool Matched)> episodes,
