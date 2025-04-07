e4e91b372ba821f01628ec8231b0b6b1654e5c34
Kimi@m4p <kimi.ribery@gmail.com>
2025-04-07 23:50:47 +0200

    Supports importing subtitles based on actual media files.

--- src/Kifa.Tools.SubUtil/Commands/ImportCommand.cs
+++ src/Kifa.Tools.SubUtil/Commands/ImportCommand.cs
@@ -13,36 +13,34 @@ namespace Kifa.Tools.SubUtil.Commands;
 class ImportCommand : KifaCommand {
     static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-    [Value(0, Required = true, HelpText = "Target file(s) to import.")]
+    [Value(0, Required = true, HelpText = "Target subtitle file(s) to import.")]
     public IEnumerable<string> FileNames { get; set; }
 
-    [Option('s', "source-id", HelpText = "ID for the source, like tv_shows/Westworld/1.",
+    [Option('t', "targets", Separator = '|', HelpText = "Target media file(s) to import for.",
         Required = true)]
-    public string SourceId { get; set; }
+    public IEnumerable<string> Targets { get; set; }
 
-    [Option('i', "id",
+    [Option('s', "release-id",
         HelpText =
-            "Id to be added before suffix. This can be language code and/or group name, like en, zh-华盟 etc.",
+            "Release Id to be added before suffix. This can be language code and/or group name, like en, zh-华盟 etc.",
         Required = true)]
     public string ReleaseId { get; set; }
 
     public override int Execute(KifaTask? task = null) {
-        var segments = SourceId.Split('/', options: StringSplitOptions.RemoveEmptyEntries);
-        var episodes = (TvShow.GetItems(segments) ?? Anime.GetItems(segments)).Checked()
-            .Select(e => new MatchableItem(e)).ToList();
-
         // Assumed all FileNames are from SubtitlesHost.
-        var files = KifaFile.FindExistingFiles(FileNames);
+        var subtitleFiles = KifaFile.FindExistingFiles(FileNames);
+        var targetFiles = KifaFile.FindExistingFiles(Targets)
+            .Select(file => new MatchableItem(file.PathWithoutSuffix));
 
-        foreach (var file in files) {
+        foreach (var file in subtitleFiles) {
             var suffix = file.Extension.Checked().ToLower();
 
             // TODO: Remove this when we can print better message in SelectOne.
             Console.WriteLine($"Select a location to import {file} to:");
-            var validEpisodes = episodes.Where(e => !e.Matched).ToList();
+            var validEpisodes = targetFiles.Where(e => !e.Matched).ToList();
             try {
-                var selected = SelectOne(validEpisodes, e => e.Item.Path, "mapping",
-                    startingIndex: 1, supportsSpecial: true, reverse: true);
+                var selected = SelectOne(validEpisodes, e => e.Item, "mapping", startingIndex: 1,
+                    supportsSpecial: true, reverse: true);
                 if (selected == null) {
                     Logger.Warn($"Ignored {file}.");
                     continue;
@@ -51,16 +49,15 @@ class ImportCommand : KifaCommand {
                 var (choice, _, special) = selected.Value;
                 if (special) {
                     var newName = Confirm($"Confirm linking {file} to (without suffix):",
-                        choice.Item.Path);
+                        choice.Item);
                     var newFile = new KifaFile($"{file.Host}{newName}.{ReleaseId}.{suffix}");
 
                     file.Copy(newFile, true);
-                    if (Confirm($"Remove info item {choice.Item.Path}?")) {
+                    if (Confirm($"Remove info item {choice.Item}?")) {
                         selected.Value.Choice.Matched = true;
                     }
                 } else {
-                    var newFile =
-                        new KifaFile($"{file.Host}{choice.Item.Path}.{ReleaseId}.{suffix}");
+                    var newFile = new KifaFile($"{file.Host}{choice.Item}.{ReleaseId}.{suffix}");
                     file.Copy(newFile, true);
                     choice.Matched = true;
                 }
@@ -73,7 +70,7 @@ class ImportCommand : KifaCommand {
     }
 }
 
-class MatchableItem(ItemInfo item) {
-    public ItemInfo Item { get; set; } = item;
+class MatchableItem(string item) {
+    public string Item { get; set; } = item;
     public bool Matched { get; set; }
 }
