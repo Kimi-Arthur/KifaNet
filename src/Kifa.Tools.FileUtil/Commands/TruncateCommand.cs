ecbdaa7f509f906d1ba1d9834193dbaa427abc51
Kimi.m4 <kimi.ribery@gmail.com>
2025-10-12 14:44:46 +0200

    Fix opending truncate file.

--- src/Kifa.Tools.FileUtil/Commands/TruncateCommand.cs
+++ src/Kifa.Tools.FileUtil/Commands/TruncateCommand.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using CommandLine;
 using Kifa.Api.Files;
-using Kifa.IO;
 using Kifa.Jobs;
 using Kifa.Service;
-using NLog;
 
 namespace Kifa.Tools.FileUtil.Commands;
 
 [Verb("truncate",
     HelpText = "Special command to truncate the last byte of the files in case it ends with \\0.")]
 class TruncateCommand : KifaCommand {
-    static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
     [Value(0, Required = true, HelpText = "Target file(s) to upload.")]
-    public IEnumerable<string> FileNames { get; set; }
+    public IEnumerable<string> FileNames { get; set; } = [];
 
     public override int Execute(KifaTask? task = null) {
         var selected = SelectMany(KifaFile.FindExistingFiles(FileNames),
@@ -32,7 +26,7 @@ class TruncateCommand : KifaCommand {
     static KifaActionResult TruncateFile(KifaFile file) {
         var truncatedFile = file.GetTruncatedFile();
         file.Copy(truncatedFile, neverLink: true);
-        using (var s = File.OpenWrite(truncatedFile.GetLocalPath())) {
+        using (var s = File.Open(truncatedFile.GetLocalPath(), FileMode.Open)) {
             s.Seek(-1, SeekOrigin.End);
             if (s.ReadByte() != 0) {
                 return new KifaActionResult {
