a20908d5ec758378bc81122a4975141ba644ebc2
Kimi@m4p <kimi.ribery@gmail.com>
2025-10-16 16:46:39 +0200

    Add ArchiveOrg API.

---
+++ Kifa.ArchiveOrg/CdxSearchRpc.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using Kifa.Rpc;
+
+namespace Kifa.ArchiveOrg;
+
+public class CdxSearchRpc : KifaJsonParameterizedRpc<List<List<string>>> {
+    protected override string Url
+        => "http://web.archive.org/cdx/search/cdx?url={encoded_url}&output=json";
+
+    protected override HttpMethod Method => HttpMethod.Get;
+
+    public CdxSearchRpc(string url) {
+        Parameters = new Dictionary<string, FuncOrValue<string>> {
+            ["encoded_url"] = HttpUtility.UrlEncode(url)
+        };
+    }
+}
---
+++ tests/Kifa.ArchiveOrg.Tests/CdxSearchRpcTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+
+namespace Kifa.ArchiveOrg.Tests;
+
+public class CdxSearchRpcTests {
+    static readonly HttpClient Client = new();
+
+    [Fact]
+    public void SearchYouTubeArchive() {
+        var result = Client.Call(new CdxSearchRpc("https://www.youtube.com/watch?v=0iNrY1ixR8I"));
+        result.Should().HaveCountGreaterThan(10);
+        result[1][1].Should().Be("20131127172500");
+    }
+}
