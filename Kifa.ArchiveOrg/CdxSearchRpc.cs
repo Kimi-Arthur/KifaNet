a169cb96069b8d473ccf1437911b11cf169b516f
Kimi.m4 <kimi.ribery@gmail.com>
2025-10-18 23:00:09 +0200

    Use typed response for CdxSearchRpc.

--- Kifa.ArchiveOrg/CdxSearchRpc.cs
+++ Kifa.ArchiveOrg/CdxSearchRpc.cs
@@ -1,10 +1,40 @@
 ﻿using System.Web;
 using Kifa.Rpc;
+using Newtonsoft.Json;
 
 namespace Kifa.ArchiveOrg;
 
 // https://archive.org/developers/wayback-cdx-server.html#basic-usage
-public class CdxSearchRpc : KifaJsonParameterizedRpc<List<List<string>>> {
+public class CdxSearchRpc : KifaParameterizedRpc, KifaRpc<List<CdxSearchRpc.ArchiveEntry>> {
+    #region CdxSearchRpc.Response
+
+    // urlkey
+    // timestamp
+    // original
+    // mimetype
+    // statuscode
+    // digest
+    // length
+    // "com,youtube)/watch?v=0inry1ixr8i"
+    // 20131127172500
+    // http://www.youtube.com/watch?v=0iNrY1ixR8I&gl=US&hl=en
+    // text/html
+    // 200
+    // 5P3KHGI3SVWPJKIA7T4TGL3PQAVUWKVQ
+    // 24444
+    public class ArchiveEntry {
+        public string? UrlKey { get; set; }
+        public string? Timestamp { get; set; }
+        public string? Original { get; set; }
+        public string? MimeType { get; set; }
+        public int? StatusCode { get; set; }
+        public string? Digest { get; set; }
+        public int? Length { get; set; }
+    }
+
+    #endregion
+
+
     protected override string Url
         => "http://web.archive.org/cdx/search/cdx?url={encoded_url}&output=json";
 
@@ -14,5 +44,24 @@ public class CdxSearchRpc : KifaJsonParameterizedRpc<List<List<string>>> {
         Parameters = new Dictionary<string, FuncOrValue<string>> {
             ["encoded_url"] = HttpUtility.UrlEncode(url)
         };
+    }
+
+    public List<ArchiveEntry> ParseResponse(HttpResponseMessage responseMessage) {
+        var response =
+            JsonConvert.DeserializeObject<List<List<string>>>(responseMessage.GetString());
+        var result = new List<ArchiveEntry>();
+        foreach (var item in response.Skip(1)) {
+            result.Add(new ArchiveEntry {
+                UrlKey = item[0],
+                Timestamp = item[1],
+                Original = item[2],
+                MimeType = item[3],
+                StatusCode = int.Parse(item[4]),
+                Digest = item[5],
+                Length = int.Parse(item[6])
+            });
+        }
+
+        return result;
     }
 }
--- Tests/Kifa.ArchiveOrg.Tests/CdxSearchRpcTests.cs
+++ Tests/Kifa.ArchiveOrg.Tests/CdxSearchRpcTests.cs
@@ -10,6 +10,14 @@ public class CdxSearchRpcTests {
     public void SearchYouTubeArchive() {
         var result = Client.Call(new CdxSearchRpc("https://www.youtube.com/watch?v=0iNrY1ixR8I"));
         result.Should().HaveCountGreaterThan(10);
-        result[1][1].Should().Be("20131127172500");
+        result[0].ToJson().Should().Be(new CdxSearchRpc.ArchiveEntry {
+            UrlKey = "com,youtube)/watch?v=0inry1ixr8i",
+            Timestamp = "20131127172500",
+            Original = "http://www.youtube.com/watch?v=0iNrY1ixR8I&gl=US&hl=en",
+            MimeType = "text/html",
+            StatusCode = 200,
+            Digest = "5P3KHGI3SVWPJKIA7T4TGL3PQAVUWKVQ",
+            Length = 24444
+        }.ToJson());
     }
 }
