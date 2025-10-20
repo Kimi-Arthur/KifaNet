3d98fe51491448c3f7ed86ef878ca64545e23e9d
Kimi.m4 <kimi.ribery@gmail.com>
2025-10-21 00:07:13 +0200

    Partisl fill is supported for uploader.

--- src/Kifa.Bilibili/BilibiliUploader.cs
+++ src/Kifa.Bilibili/BilibiliUploader.cs
@@ -13,6 +13,8 @@ public class BilibiliUploader : DataModel, WithModelId<BilibiliUploader> {
 
     public static string ModelId => "bilibili/uploaders";
 
+    public bool ForceFullRefresh { get; set; } = false;
+
     public static KifaServiceClient<BilibiliUploader> Client { get; set; } =
         new KifaServiceRestClient<BilibiliUploader>();
 
@@ -30,7 +32,7 @@ public class BilibiliUploader : DataModel, WithModelId<BilibiliUploader> {
         }
 
         Name = info.Space.Info.Name;
-        var list = GetAllVideos(Id);
+        var list = ForceFullRefresh ? GetAllVideos() : MergeVideos(GetNewVideos(), Aids);
         var removed = RemovedAids.ToHashSet();
         removed.UnionWith(Aids);
         removed.ExceptWith(list);
@@ -42,25 +44,62 @@ public class BilibiliUploader : DataModel, WithModelId<BilibiliUploader> {
         return null;
     }
 
-    List<string> GetAllVideos(string uploaderId) {
-        var data = HttpClients.GetBilibiliClient().Call(new UploaderVideoRpc(uploaderId))?.Data;
+    static List<string> MergeVideos(List<string> newVideos, List<string> oldVideos) {
+        var newHashes = new HashSet<string>(newVideos);
+        return newVideos.Concat(oldVideos.Reverse<string>()
+            .Where(video => !newHashes.Contains(video))).ToList();
+    }
+
+    List<string> GetAllVideos() {
+        var data = HttpClients.GetBilibiliClient().Call(new UploaderVideoRpc(Id))?.Data;
         if (data == null) {
-            throw new DataNotFoundException($"Cannot find videos uploaded by {uploaderId}.");
+            throw new DataNotFoundException($"Cannot find videos uploaded by {Id}.");
         }
 
         var page = 1;
         var list = GetAids(data).ToList();
 
         while (data.HasMore) {
-            data = HttpClients.GetBilibiliClient()
-                .Call(new UploaderVideoRpc(uploaderId, data.Offset))?.Data;
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            data = HttpClients.GetBilibiliClient().Call(new UploaderVideoRpc(Id, data.Offset))
+                ?.Data;
             if (data == null) {
                 throw new DataNotFoundException(
-                    $"Cannot find videos uploaded by {uploaderId} after {list.Count} videos.");
+                    $"Cannot find videos uploaded by {Id} after {list.Count} videos.");
             }
 
             list.AddRange(GetAids(data));
+        }
+
+        return list;
+    }
+
+    // Gets new videos, may contain duplicates.
+    List<string> GetNewVideos() {
+        var data = HttpClients.GetBilibiliClient().Call(new UploaderVideoRpc(Id))?.Data;
+        if (data == null) {
+            throw new DataNotFoundException($"Cannot find videos uploaded by {Id}.");
+        }
+
+        var page = 1;
+        var list = GetAids(data).ToList();
+        if (Aids.Contains(list.Last())) {
+            return list;
+        }
+
+        while (data.HasMore) {
             Thread.Sleep(TimeSpan.FromSeconds(5));
+            data = HttpClients.GetBilibiliClient().Call(new UploaderVideoRpc(Id, data.Offset))
+                ?.Data;
+            if (data == null) {
+                throw new DataNotFoundException(
+                    $"Cannot find videos uploaded by {Id} after {list.Count} videos.");
+            }
+
+            list.AddRange(GetAids(data));
+            if (Aids.Contains(list.Last())) {
+                return list;
+            }
         }
 
         return list;
