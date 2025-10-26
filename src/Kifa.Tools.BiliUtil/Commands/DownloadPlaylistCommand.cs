99fe894193097b422d908152135571e9e8f33f42
Kimi.m4 <kimi.ribery@gmail.com>
2025-10-26 11:38:15 +0100

    Add support to download videos in a bilibili playlist.

---
+++ src/Kifa.Tools.BiliUtil/Commands/DownloadPlaylistCommand.cs
@@ -0,0 +1,45 @@
+using CommandLine;
+using Kifa.Bilibili;
+using Kifa.Jobs;
+using NLog;
+
+namespace Kifa.Tools.BiliUtil.Commands;
+
+[Verb("list", HelpText = "Download all high quality Bilibili videos in a playlist.")]
+public class DownloadPlaylistCommand : DownloadCommand {
+    static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    [Value(0, Required = true, HelpText = "Playlist ID")]
+    public string PlaylistId { get; set; }
+
+    [Option('f', "folder",
+        HelpText =
+            "Alternate folder to use. Playlist Id will be appended as {folder}.p{id}.bilibili")]
+    public string? AlternateFolder { get; set; }
+
+    public override int Execute(KifaTask? task = null) {
+        var playlist = BilibiliPlaylist.Client.Get(PlaylistId);
+        if (playlist == null) {
+            Logger.Fatal($"Cannot find playlist ({PlaylistId}). Exiting.");
+            return 1;
+        }
+
+        foreach (var videoId in playlist.Videos) {
+            var video = BilibiliVideo.Client.Get(videoId);
+            if (video == null) {
+                Logger.Error($"Cannot find video ({videoId}). Skipping.");
+                continue;
+            }
+
+            Logger.Trace($"To download video {video}");
+
+            foreach (var page in video.Pages) {
+                ExecuteItem($"{video.Id}p{page.Id} {video.Title} {page.Title}",
+                    () => Download(video, page.Id,
+                        alternativeFolder: $"{AlternateFolder ?? playlist.Title}.p{PlaylistId}"));
+            }
+        }
+
+        return 0;
+    }
+}
--- src/Kifa.Tools.BiliUtil/Program.cs
+++ src/Kifa.Tools.BiliUtil/Program.cs
@@ -7,9 +7,11 @@ namespace Kifa.Tools.BiliUtil;
 class Program {
     static int Main(string[] args)
         => KifaCommand.Run(new Parser(settings => {
-            settings.CaseInsensitiveEnumValues = true;
-            settings.HelpWriter = Console.Error;
-        }).ParseArguments<GetChatCommand, DownloadVideoCommand, DownloadUploaderCommand,
-            DownloadBangumiCommand, GetCoverCommand, LinkCommand, DownloadMangaCommand,
-            GetTencentChatCommand, DownloadArchiveCommand, ShadowCommand>, args);
+                settings.CaseInsensitiveEnumValues = true;
+                settings.HelpWriter = Console.Error;
+            }).ParseArguments<GetChatCommand, DownloadVideoCommand, DownloadUploaderCommand,
+                DownloadBangumiCommand, GetCoverCommand, LinkCommand, DownloadMangaCommand,
+                GetTencentChatCommand, DownloadArchiveCommand, ShadowCommand,
+                DownloadPlaylistCommand>,
+            args);
 }
