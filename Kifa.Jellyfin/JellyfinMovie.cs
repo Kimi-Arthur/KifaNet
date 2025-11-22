a9b81b1c49b16988bcf9d50612e2eeae7ee17002
Kimi.m4 <kimi.ribery@gmail.com>
2025-11-22 16:49:15 +0100

    Dummy JellyfinMovie.

---
+++ Kifa.Jellyfin/JellyfinMovie.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace Kifa.Jellyfin;
+
+// Currently dummy
+public class JellyfinMovie {
+    public string Title { get; set; }
+    public string Plot { get; set; }
+    public string Studio { get; set; }
+    public string Director { get; set; }
+    public List<string> Actor { get; set; }
+    public Date Year { get; set; }
+    public List<string> Genre { get; set; }
+    public List<string> Tag { get; set; }
+    public string Thumb { get; set; }
+    public string Fanart { get; set; }
+}
