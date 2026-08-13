83f1b1b60fc77310edc2fb25c8107c376d484b46
Kimi@mba24 <kimi.ribery@gmail.com>
2026-08-13 02:32:54 +0200

    refactor(youtube): download to tempFile with overrideOptions

--- Kifa.Tools.YoutubeUtil/Commands/DownloadCommand.cs
+++ Kifa.Tools.YoutubeUtil/Commands/DownloadCommand.cs
@@ -3,11 +3,16 @@ using Kifa.Api.Files;
 using Kifa.YouTube;
 using NLog;
 using YoutubeDLSharp;
+using YoutubeDLSharp.Options;
 
 namespace Kifa.Tools.YoutubeUtil.Commands;
 
 public abstract class DownloadCommand : YoutubeCommand {
     static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    static YoutubeDL YoutubeDL => new() {
+        YoutubeDLPath = YouTubeVideo.YoutubeDownloaderPath
+    };
 
     [Option('p', "prefix", HelpText = "Prefix of file name. Possible values: date, number")]
     public string? Prefix { get; set; }
@@ -44,20 +49,15 @@ public abstract class DownloadCommand : YoutubeCommand {
         }
 
         var canonicalTargetFile = targetFiles[0];
-
-        var ytdl = new YoutubeDL {
-            YoutubeDLPath = YouTubeVideo.YoutubeDownloaderPath,
-            OutputFolder = canonicalTargetFile.Parent.GetLocalPath(),
-            OutputFileTemplate =
-                $"{KifaFile.DefaultIgnoredPrefix}{canonicalTargetFile.BaseName}.%(ext)s"
-        };
+        var tempFile = canonicalTargetFile.GetIgnoredFile();
 
         Logger.Debug($"Downloading video {video.Id} with yt-dlp to {canonicalTargetFile}...");
-        var tempFile = canonicalTargetFile.Parent.GetFile(
-            $"{KifaFile.DefaultIgnoredPrefix}{canonicalTargetFile.BaseName}.mp4");
 
-        var downloadResult = ytdl.RunVideoDownload($"https://www.youtube.com/watch?v={video.Id}")
-            .GetAwaiter().GetResult();
+        var downloadResult = YoutubeDL.RunVideoDownload(video.Id,
+            mergeFormat: DownloadMergeFormat.Mp4,
+            overrideOptions: new OptionSet {
+                Output = tempFile.GetLocalPath()
+            }).GetAwaiter().GetResult();
 
         if (!downloadResult.Success) {
             throw new KifaExecutionException(
